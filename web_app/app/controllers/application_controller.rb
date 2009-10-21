b0982a08930fe1027719b7259004839e899f86e6
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2009-10-21 14:44:07 +0000

    + added page-title generation (generate_page_title) + added page-title guessing (guess_page_title) for CRUD-methods * fixed seeds.rb (to match latest changes by FFI) * gravatar is now w=h=24

--- web_app/app/controllers/application_controller.rb
+++ web_app/app/controllers/application_controller.rb
@@ -12,11 +12,15 @@ class ApplicationController < ActionController::Base
 
   before_filter :fetch_user
   before_filter :require_current_user
+  before_filter :generate_page_title
 
   protected
 
   attr_accessor :current_user
   helper_method :current_user, :logged_in?
+
+  attr_accessor :current_page_title
+  helper_method :current_page_title
 
   def require_current_user
     unless logged_in?
@@ -35,5 +39,39 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def guess_page_title
+    begin
+      clazz = Kernel.const_get controller_name.classify
+    rescue
+      return  nil
+    end
+    
+    singular = clazz.name
+    plural = singular.pluralize
+    if clazz.respond_to? :human_name
+      singular = clazz.human_name 
+      plural = clazz.human_name(:count => 2)
+    end
+
+    case action_name
+    when "edit", "update"
+      I18n.t("titles.generic.edit", :model => singular, :default => nil)
+    when "new", "create"
+      I18n.t("titles.generic.new", :model => singular, :default => nil)
+    when "show"
+      I18n.t("titles.generic.show", :model => singular, :default => nil)
+    when "index"
+      I18n.t("titles.generic.index", :model => plural, :default => nil)
+    else
+      nil
+    end
+  end
+
+  def generate_page_title
+    default = guess_page_title
+    default ||= "#{controller_name} : #{action_name}"
+    @current_page_title = I18n.t("titles.#{controller_name}.#{action_name}", :default => default)
   end
 end
