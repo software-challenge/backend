111920c31cba83d1ffed1608e2af2703db6b9bdf
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2009-12-22 02:00:39 +0000

    fixed capistrano/db config

--- web_app/config/deploy.rb
+++ web_app/config/deploy.rb
@@ -1,8 +1,16 @@
 set :application, "swchallenge"
-set :repository,  "http://samoa.informatik.uni-kiel.de:84/svn/teaching/ss09/swchal/common/web_app"
+
+set :scm_user, Proc.new { Capistrano::CLI.ui.ask "SCM User:" }
+set :scm_password, Proc.new { Capistrano::CLI.password_prompt("Password for #{scm_user}: ") } 
+
+set :repository, Proc.new {
+	"--username #{scm_user} --password #{scm_password} --no-auth-cache http://samoa.informatik.uni-kiel.de:84/svn/teaching/ss09/swchal/common/web_app"
+}
 
 set :scm, :subversion
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :scm_auth_cache, true
 
 set :deploy_to, "/home/scadmin/www/apps/#{application}"
 
