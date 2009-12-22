cfbdce4939ec76ded8e6ed043ab6a5e8a7ef03f7
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2009-12-22 12:45:51 +0000

    set runner/admin_runner for capistrano

--- web_app/config/deploy.rb
+++ web_app/config/deploy.rb
@@ -1,5 +1,8 @@
 set :application, "swchallenge"
 
+set :runner, "scadmin"
+set :admin_runner, "scadmin"
+
 set :scm_user, Proc.new { Capistrano::CLI.ui.ask "SCM User:" }
 set :scm_password, Proc.new { Capistrano::CLI.password_prompt("Password for #{scm_user}: ") } 
 
@@ -16,12 +19,8 @@ set :deploy_to, "/home/scadmin/www/apps/#{application}"
 
 role :web, "134.245.253.5"                          # Your HTTP server, Apache/etc
 role :app, "134.245.253.5"                          # This may be the same as your `Web` server
-role :db,  "134.245.253.5", :primary => true # This is where Rails migrations will run
+role :db,  "134.245.253.5", :primary => true        # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
   task :start do
