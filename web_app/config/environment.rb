7f1271f3b2e2aefebeccf156237f1ecd380108aa
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2009-12-22 13:02:35 +0000

    add Daemons gem

--- web_app/config/environment.rb
+++ web_app/config/environment.rb
@@ -22,7 +22,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "rubyzip", :lib => false
   config.gem "nokogiri"
-  config.gem "cap-recipes", :lib => false, :soruce => "http://gemcutter.org"
+  config.gem "cap-recipes", :lib => false, :source => "http://gemcutter.org"
+  config.gem "daemons", :lib => false
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
