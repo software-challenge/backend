df2cffe4debd94dcb2f301beb515788ebd7d70f8
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2009-12-22 00:32:30 +0000

    * localized views

--- web_app/app/helpers/matches_helper.rb
+++ web_app/app/helpers/matches_helper.rb
@@ -1,2 +1,9 @@
 module MatchesHelper
+  def match_score_fragment_name(game, fragment)
+    t("games.#{game.game_identifier.to_s.underscore}.match_score.#{fragment.name}")
+  end
+
+  def round_score_fragment_name(game, fragment)
+    t("games.#{game.game_identifier.to_s.underscore}.round_score.#{fragment.name}")
+  end
 end
