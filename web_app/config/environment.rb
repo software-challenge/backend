372af44597fa8dabd8f4be0af8af902ea7ddb671
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2009-12-26 00:38:07 +0000

    + add erubis requirement for XSS plugin

--- web_app/config/environment.rb
+++ web_app/config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "cap-recipes", :lib => false, :source => "http://gemcutter.org"
   config.gem "daemons", :lib => false
   config.gem "declarative_authorization", :source => "http://gemcutter.org"
+  config.gem "erubis", :lib => false, :version => ">= 2.6.5"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
