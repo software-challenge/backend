d441a5ebdbb6c9f63a4661bc8de12f804a11817b
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2009-12-28 17:09:01 +0000

    fixed daemon script to respond to TERM

--- web_app/lib/daemons/game_server.rb
+++ web_app/lib/daemons/game_server.rb
@@ -6,8 +6,18 @@ ENV["RAILS_ENV"] ||= "production"
 require File.dirname(__FILE__) + "/../../config/environment"
 
 $running = true
+$game_server_pid = nil
+
 Signal.trap("TERM") do 
   $running = false
+  Process.kill("TERM", $game_server_pid)
+end
+
+# SIGNAL response thread
+Thread.new do
+  while($running) do
+    sleep 1
+  end
 end
 
 while($running) do
@@ -16,8 +26,15 @@ while($running) do
   ActiveRecord::Base.logger.info "This daemon is still running at #{Time.now}.\n"
   
   Dir.chdir Rails.root.join("public", "server") do
-    `java -jar GameServer.jar`
-    process_status = $?
+    #command = "java -Dfile.encoding=UTF-8 -jar GameServer.jar"
+    #io = IO.popen command
+    #io.close
+
+    $game_server_pid = Process.fork do
+      exec('java', '-Dfile.encoding=UTF-8', '-jar', './GameServer.jar')
+    end
+    pid, process_status = Process.wait2($game_server_pid)
+
     ActiveRecord::Base.logger.info "The GameServer exited with exitstatus=#{process_status.exitstatus}.\n"
   end
 
