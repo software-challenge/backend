98290273e77131711f54b80d8b8dd7bb60773ff8
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2009-12-30 16:01:19 +0000

    + added release logback configuration * save server logs

--- web_app/lib/daemons/game_server.rb
+++ web_app/lib/daemons/game_server.rb
@@ -24,15 +24,19 @@ while($running) do
   
   # Replace this with your code
   ActiveRecord::Base.logger.info "This daemon is still running at #{Time.now}.\n"
+  log_directory = File.expand_path(Rails.root.join("log"))
   
   Dir.chdir Rails.root.join("public", "server") do
-    #command = "java -Dfile.encoding=UTF-8 -jar GameServer.jar"
-    #io = IO.popen command
-    #io.close
+
 
     $game_server_pid = Process.fork do
-      exec('java', '-Dfile.encoding=UTF-8', '-jar', './GameServer.jar')
+      exec('java',
+        '-Dfile.encoding=UTF-8',
+        '-Dlogback.configurationFile=logback-release.xml',
+        "-DLOG_DIRECTORY=\"#{log_directory}\"",
+        '-jar', './GameServer.jar')
     end
+
     pid, process_status = Process.wait2($game_server_pid)
 
     ActiveRecord::Base.logger.info "The GameServer exited with exitstatus=#{process_status.exitstatus}.\n"
