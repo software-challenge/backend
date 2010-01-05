f10c4e779791fdf71e15ea18d83aca04d84fbbdc
mja <mja@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-01-05 10:25:21 +0000

    fixed seed

--- web_app/db/seeds.rb
+++ web_app/db/seeds.rb
@@ -2,9 +2,9 @@ ActiveRecord::Base.transaction do
   contest = Contest.new(:name => "Software-Challenge 2010", :active => true)
   contest.save!
 
-  password = "swordfish"
-  salt = "the_salt"
-  encrypted_password = Person.encrypt_password(password, salt)
-  person = Person.new(:nick_name => "The Company Guy", :last_name => "Dude", :first_name => "Der", :email => "root@example.com", :password_hash => encrypted_password, :password_salt => salt, :administrator => true)
-  person.save!
+  Person.create!(:nick_name => "The Company Guy",
+    :last_name => "Dude", :first_name => "Der",
+    :email => "root@example.com",
+    :password => "swordfish",
+    :administrator => true)
 end
