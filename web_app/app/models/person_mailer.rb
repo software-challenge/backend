b6f6621edba430432219dff2bac65149d8561a49
svk <svk@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-01-14 12:18:23 +0000

    it is now possible to send password-reset-notifications for existing people

--- web_app/app/models/person_mailer.rb
+++ web_app/app/models/person_mailer.rb
@@ -8,4 +8,12 @@ class PersonMailer < ActionMailer::Base
     body({ :person => person, :url => "http://#{contest.subdomain}.software-challenge.de", :plain_password => plain_password })
   end
 
+  def password_reset_notification(person, contest, plain_password)
+    recipients "#{person.name} <#{person.email}>"
+    from       "software-challenge@gfxpro.eu"
+    subject    "Ihr Passwort zur Wettbewerbsseite der Software Challenge wurde neu gesetzt"
+    sent_on    Time.now
+    body({ :person => person, :url => "http://#{contest.subdomain}.software-challenge.de", :plain_password => plain_password })
+  end
+
 end
