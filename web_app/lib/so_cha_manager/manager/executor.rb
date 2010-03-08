db94ce84946de60d1c713e9c9688de551fe96ec9
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-03-08 13:48:32 +0000

    Fixed: View logs even if client not tested yet Debug: Deactivated forced game start after 120 seconds

--- web_app/lib/so_cha_manager/manager/executor.rb
+++ web_app/lib/so_cha_manager/manager/executor.rb
@@ -85,7 +85,8 @@ module SoChaManager
             logger.info "Game is already over, start_game_after not necessary."
           elsif !room_handler.received_data_after?(threshold.seconds.ago)
             logger.info "#{SoChaManager.start_game_after} seconds passed. Forcing game to start."
-            @client.step(room_id, true)
+            #@client.step(room_id, true)
+            logger.info "WARNING!: #{SoChaManager.start_game_after} seconds passed, game should be forces to start but isn't!"
           else
             logger.info "Action detected, start_game_after not necessary."
           end
