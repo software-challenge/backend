1a7141f9104762017cab3ea7f203eafefd447947
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-03-10 08:15:35 +0000

    Changing for the standings overview, Ticket #96

--- web_app/app/helpers/matchdays_helper.rb
+++ web_app/app/helpers/matchdays_helper.rb
@@ -1,31 +1,42 @@
-module MatchdaysHelper
-  def matchday_status(matchday)
-    if matchday.running?
-      I18n.t("helpers.playing_in_progress")
-    elsif matchday.played?
-      I18n.t("helpers.played")
-    else
-      I18n.t("helpers.not_played_yet")
-    end
-  end
-  
-  def matchesCount(matchday)
-     matchcount = 0
-     matchday.matches.each do |match|
-       matchcount += match.rounds.count
-     end
-     return matchcount
-  end
-  
-  def matchesDone(matchday)
-    done = 0
-    matchday.matches.each do |match|
-      match.rounds.each do |round|
-        if not round.played?.nil? 
-          done += 1 
-        end
-      end
-    end
-    return done
-  end
+	module MatchdaysHelper
+	  def matchday_status(matchday)
+	    if matchday.running?
+	      I18n.t("helpers.playing_in_progress")
+	    elsif matchday.played?
+	      I18n.t("helpers.played")
+	    else
+	      I18n.t("helpers.not_played_yet")
+	    end
+	  end
+	  
+	  def matchesCount(matchday)
+	     matchcount = 0
+	     matchday.matches.each do |match|
+	       matchcount += match.rounds.count
+	     end
+	     return matchcount
+	  end
+	  
+	  def matchesDone(matchday)
+	    done = 0
+	    matchday.matches.each do |match|
+	      match.rounds.each do |round|
+		if not round.played?.nil? 
+		  done += 1 
+		end
+	      end
+	    end
+	    return done
+	  end
+	  
+	  def count_matches_played_by_contestant(contestant)
+	    all_matches = contestant.matches
+            count = 0
+            all_matches.each do |match|
+              if not match.played.nil? 
+                count+=1 
+              end
+            end
+	    return count
+          end
 end
