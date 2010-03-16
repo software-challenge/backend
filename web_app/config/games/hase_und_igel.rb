ca8dccc1095d7d84d3ddee075a279b69568afaa8
svk <svk@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-03-16 07:46:10 +0000

    rule violations are now shown in point table and are punished by setting the position to 0

--- web_app/config/games/hase_und_igel.rb
+++ web_app/config/games/hase_und_igel.rb
@@ -16,9 +16,21 @@ GameDefinition.create :"HaseUndIgel" do
 
   match_score do
     field :points, :ordering => "DESC", :aggregate => :sum do |my_scores, their_scores|
-      my_victories = my_scores.inject(0) { |sum,x| sum + x.victory }
+      my_victories = my_scores.inject(0) do |sum,x|
+        if x.cause == "REGULAR"
+          sum + x.victory
+        else
+          sum
+        end
+      end
       enemy_scores = their_scores.first
-      enemy_victories = enemy_scores.inject(0) { |sum,x| sum + x.victory }
+      enemy_victories = enemy_scores.inject(0) do |sum,x|
+        if x.cause == "REGULAR"
+          sum + x.victory
+        else
+          sum
+        end
+      end
 
       if my_victories > enemy_victories
         2
@@ -30,7 +42,18 @@ GameDefinition.create :"HaseUndIgel" do
     end
 
     field :victories, :sum => :victory
-    field :average_position, :average => :position, :precision => 2
+    field :average_position, :aggregate => :average do |my_scores, their_scores|
+      # for non-regular game-ends set position to 0
+      sum = my_scores.inject(0) do |sum, score|
+        if score.cause == "REGULAR"
+          sum + score.position
+        else
+          sum
+        end
+      end
+
+      sum / my_scores.count
+    end
     field :average_carrots, :average => :carrots, :precision => 2
     field :average_time, :average => :average_time
 
