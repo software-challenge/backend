54482bdb5a50ce4c9eb91095fea788e2ca155048
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-03-21 21:36:40 +0000

    Timeout set to 2000ms

--- game_plugins/hase_und_igel/server/sc/plugin2010/Game.java
+++ game_plugins/hase_und_igel/server/sc/plugin2010/Game.java
@@ -378,6 +378,6 @@ public class Game extends RoundBasedGameInstance<Player>
 	@Override
 	protected ActionTimeout getTimeoutFor(Player player)
 	{
-		return new ActionTimeout(true, 10000l, 2100l);
+		return new ActionTimeout(true, 10000l, 2000l);
 	}
 }
