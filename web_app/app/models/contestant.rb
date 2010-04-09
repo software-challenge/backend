cb4bf9e422d327ceb51b1694f7f1a142c3d7c529
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-04-09 13:08:18 +0000

    Spielfreie Tage in den Begegnungen anzeigen

--- web_app/app/models/contestant.rb
+++ web_app/app/models/contestant.rb
@@ -28,6 +28,19 @@ class Contestant < ActiveRecord::Base
     contest.matches.with_contestant(self)
   end
 
+  def matches_including_free_days
+    matches = []
+    contest.matchdays.each do |matchday|
+      match = self.matches.to_ary.find{|match| match.matchday == matchday}
+      if !match.blank?
+        matches << match
+      else
+        matches << LeagueMatch.new(:matchday => matchday)
+      end
+    end
+    return matches
+  end
+
   def has_running_tests?
     !clients.running.count.zero?
   end
