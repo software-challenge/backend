0225af8ed2509c9a8c3ac7d016155b4396177960
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-05-10 14:06:49 +0000

    Corrected matchday status for not logged in users for unpublished matchdays

--- web_app/app/helpers/matchdays_helper.rb
+++ web_app/app/helpers/matchdays_helper.rb
@@ -1,8 +1,8 @@
 	module MatchdaysHelper
 	  def matchday_status(matchday)
-	    if matchday.running?
+	    if matchday.running? and (current_user.has_role?(:administrator) or matchday.published?)
 	      I18n.t("helpers.playing_in_progress")
-	    elsif matchday.played?
+	    elsif matchday.played? and (current_user.has_role?(:administrator) or matchday.published?)
 	      I18n.t("helpers.played")
 	    else
 	      I18n.t("helpers.not_played_yet")
