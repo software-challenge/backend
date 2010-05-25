d0ed40a4176c331feebacccd3763073a12f0d73d
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-05-25 14:02:31 +0000

    Now, number of rounds to play (2, 4, 6) in custom games can be specified

--- web_app/app/controllers/custom_matches_controller.rb
+++ web_app/app/controllers/custom_matches_controller.rb
@@ -7,11 +7,17 @@ class CustomMatchesController < ApplicationController
   def create
     con1 = Contestant.find(params[:match][:contestant1])
     con2 = Contestant.find(params[:match][:contestant2])
-    unless con1 == con2
+    rounds = params[:match][:rounds].to_i
+    rounds ||= @contest.game_definition.test_rounds
+    if (con1 != con2 and rounds.even? and rounds > 0)
       mm = @contest.custom_matches.create
-      mm.clients = [con1.current_client, con2.current_client] 
-    else  
+      mm.setup_clients [con1.current_client, con2.current_client], rounds
+    elsif con1 == con2 
       flash[:error] = "Eine Schule kann nicht gegen sich selbst antreten"
+    elsif rounds.odd?
+      flash[:error] = "Nur gerade Anzahl Runden möglich"
+    elsif rounds <= 0
+      flash[:error] = "Spiel mit #{rounds} Runden nicht möglich"
     end
 
     redirect_to hash_for_contest_custom_matches_url
