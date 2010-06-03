57f9f5266e381d0e0325accd1559b1f4866d0b86
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-06-03 13:43:04 +0000

    ...

--- web_app/app/models/person.rb
+++ web_app/app/models/person.rb
@@ -183,11 +183,11 @@ class Person < ActiveRecord::Base
     end
   end
 
-  def has_hidden_friendly_encounters?
-    teams.inject(false) {|val, x| val or x.has_hidden_friendly_encounters?}
+  def has_hidden_friendly_encounters?(contest)
+    teams.visible.without_testers.for_contest(contest).inject(false) {|val, x| val or x.has_hidden_friendly_encounters?}
   end
 
-  def is_member_of_a_team?
-    not teams.nil?
+  def is_member_of_a_team?(contest)
+    not teams.visible.without_testers.for_contest(contest).nil?
   end
 end
