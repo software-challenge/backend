69673d84a2b443430259fd5b77b7d43b4063cf7a
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-06-17 08:18:35 +0000

    - The PrepareGameRequest can now contain a <loadGameInfo class="..."> object to start the game with a given state (board i.e.)

--- game_plugins/minimal/server/sc/plugin_minimal/Game.java
+++ game_plugins/minimal/server/sc/plugin_minimal/Game.java
@@ -205,10 +205,20 @@ public class Game extends RoundBasedGameInstance<Player>
 
 	@Override
 	public void loadFromFile(String file) {
-		GameLoader<GameState> gl = new GameLoader<GameState>(GameState.class);
-		GameState state = gl.loadGame(Configuration.getXStream(), file);
-		if (state != null) {
-			this.board = state.getGame().getBoard();
+		GameLoader gl = new GameLoader(new Class<?>[] {GameState.class, Board.class});
+		Object gameInfo = gl.loadGame(Configuration.getXStream(), file);
+		if (gameInfo != null) {
+			loadGameInfo(gameInfo);
+		}
+	}
+
+	@Override
+	public void loadGameInfo(Object gameInfo) {
+		if (gameInfo instanceof GameState) {
+			this.board = ((GameState)gameInfo).getGame().getBoard();
+		}
+		if (gameInfo instanceof Board) {
+			this.board = (Board)gameInfo;
 		}
 	}
 }
