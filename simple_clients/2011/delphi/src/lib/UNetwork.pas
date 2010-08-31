925b686e4ac50cc171249f970deaa494631e4dde
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-08-31 14:27:26 +0000

    Delphi client

---
+++ simple_clients/2011/delphi/src/lib/UNetwork.pas
@@ -0,0 +1,256 @@
+unit UNetwork;
+
+interface
+
+uses
+  UDefines,
+  USocket,
+  AdomCore_4_3,
+  UBoard,
+  UClient,
+  SysUtils, Classes, UProtocol;
+
+type
+	TNetwork = class
+	private
+    Fsocket   : TSocket;  // Netzwerk-Kommunikationsschnittstelle mit Windows
+		Fhost     : String;   // Host-Adresse
+		Fport     : Integer;  // Host-Port
+    FLog      : Boolean;  // Do we log?
+
+    Fboard    : TBoard;   // Spielbrett
+    FClient   : TClient;  // Simple Client
+    FProtocol : TProtocol; // Protocl layer
+    FFile     : ^TextFile; // Log File
+
+    FReceiveBuffer : String;
+    FReservationID : String;
+
+    FXml : TDomElement;
+    FXmlDoc : TDomDocument;
+    FXmlDomImpl : TDomImplementation;
+
+    Fconnected: Boolean;
+	public
+    constructor Create(const host: String; port: Integer; reservationID: String; board : TBoard; client : TClient);
+    destructor Destroy(); override;
+
+    function connect(): Boolean;
+    procedure disconnect();
+    procedure sendString(const s: String);
+    procedure sendXml(xml : TDomDocument);
+    procedure logFile(var logFile : TextFile);
+    procedure log(text : String);
+
+    procedure emptyReceiveBuffer();
+    function readString() : Boolean;
+    function processMessages() : Boolean;
+
+    property connected: Boolean read Fconnected;
+end;
+
+implementation
+
+constructor TNetwork.Create(const host: String; port: Integer; reservationID: String; board : TBoard; client : TClient);
+begin
+  inherited Create();
+  Fhost := host;
+  Fport := port;
+  Fconnected := False;
+  FLog := false;
+
+  FReservationID := reservationID;
+  Fboard := board;
+  FClient := client;
+
+  Fsocket := TSocket.Create();
+  FProtocol := TProtocol.Create(self, FBoard, FClient);
+  FXmlDomImpl := TDomImplementation.Create(nil);
+
+  WriteLn('> Network/Socket created.');
+end;
+
+destructor TNetwork.Destroy;
+begin
+  if Fsocket <> nil then
+    Fsocket.Free;
+  if(FProtocol <> nil) then FreeAndNil(FProtocol);
+  if(FXmlDomImpl <> nil) then FreeAndNil(FXmlDomImpl);
+  inherited;
+end;
+
+//----------------------------------------------------------------------------------------
+
+(*
+ * baut eine Verbindung auf.
+ *)
+function TNetwork.connect(): Boolean;
+var
+  n: Integer;
+begin
+  WriteLn('> Connecting...');
+  if(FLog) then begin
+    Writeln(FFile^, '> Connecting...');
+    Flush(FFile^);
+  end;
+
+  n := 0;
+  while not Fsocket.Connected do
+  begin
+    try
+      Fsocket.Connect(Fhost, Fport);
+    except
+      WriteLn('Establishing connection (retry: '+IntToStr(n)+') on port '+
+                                    IntToStr(Fport)+' failed!');
+      inc(n);
+    end;
+  end;
+
+  WriteLn('> Connected.');
+  if(FLog) then Writeln(FFile^, '> Connected.');
+  Fconnected := Fsocket.Connected;
+
+  // Connected, start protocol and join the game
+  Self.sendString('<protocol>');
+  // Do we have a reservation ID?
+  if(FReservationID <> '') then begin
+    // Got reservation id, use it
+    FXmlDoc := TDomDocument.Create(FXmlDomImpl);
+    FXml := TDomElement.Create(FXmlDoc, 'joinPrepared');
+    FXml.SetAttribute('reservationCode', FReservationID);
+    FXmlDoc.AppendChild(FXml);
+    Self.sendXml(FXmlDoc);
+    FreeAndNil(FXmlDoc);
+  end
+  else begin
+    // No reservation, join based on gametype
+    FXmlDoc := TDomDocument.Create(FXmlDomImpl);
+    FXml := TDomElement.Create(FXmlDoc, 'join');
+    FXml.SetAttribute('gameType', 'SIT');
+    FXmlDoc.AppendChild(FXml);
+    Self.sendXml(FXmlDoc);
+    FreeAndNil(FXmlDoc);
+  end;
+
+  result := Fconnected;
+end;
+
+(*
+ * Bricht die Verbindung ab, sollte eine existieren
+ *)
+procedure TNetwork.disconnect();
+begin      
+  if (Fsocket.Connected) then
+  begin
+    Fsocket.Free();
+    Fsocket := TSocket.Create; // workaround: wird nämlich in Network.free freigegeben
+    Fconnected := false;
+  end;
+  WriteLn('> Disconnected.');
+end;
+
+(*
+ * readMessages liest den Socket-Buffer aus und prüft, ob eine vollständige
+ * XML-Nachricht empfangen wurde
+ *)
+function TNetwork.readString : Boolean;
+var
+  sockMsg : String;
+begin
+  sockMsg := '';
+  if(not Fsocket.Connected) then exit;
+  try
+    sockMsg := Fsocket.receive();
+  except
+    writeln('Netzwerkfehler. Verbindung beendet');
+    self.sendString('</protocol>');
+    self.disconnect;
+    result := false;
+    exit;
+  end;
+  if(sockMsg <> '') then begin
+    // Auf dem Socket wurde etwas empfangen, es wird in den Buffer kopiert
+    FReceiveBuffer := FReceiveBuffer + sockMsg;
+
+    // <protocol> - Tag entfernen
+    FReceiveBuffer := StringReplace(FReceiveBuffer, '<protocol>', '', [rfReplaceAll, rfIgnoreCase]);
+
+    Writeln('Receive:');
+    Writeln;
+    Writeln(sockMsg);
+    if(FLog) then begin
+      Writeln(FFile^, 'Receive:');
+      Writeln(FFile^);
+      Writeln(FFile^, sockMsg);
+    end;
+
+    // Text verarbeiten
+    FProtocol.processString(FReceiveBuffer);
+  end;
+  Result := true;
+end;
+
+procedure TNetwork.emptyReceiveBuffer();
+begin
+  FReceiveBuffer := '';
+end;
+
+(*
+ * processMessages wird regelmäßig aufgerufen und verarbeitet die empfangenen XML-Nachrichten
+ *)
+function TNetwork.processMessages : Boolean;
+begin
+  if(not Fsocket.Connected) then begin
+    result := false;
+    exit;
+  end;
+
+  result := self.readString;
+end;
+
+(*
+ * sendString schickt eine Nachricht an den Server
+ *)
+procedure TNetwork.sendString(const s: String);
+begin
+  if(FSocket.Connected) then begin
+    FSocket.sendLn(s);
+    writeln('Send:');
+    writeln;
+    writeln(s);
+    if(FLog) then begin
+      writeln(FFile^, 'Send:');
+      writeln(FFile^);
+      writeln(FFile^, s);
+    end;
+  end;
+end;
+
+procedure TNetwork.sendXML(xml : TDomDocument);
+var
+  text : String;
+  FXmlParser : TDomToXmlParser;
+begin
+  FXmlParser := TDomToXmlParser.Create(nil);
+  FXmlParser.DOMImpl := FXmlDomImpl;
+  FXmlParser.IncludeXmlDecl := false;
+  FXmlParser.WriteToString(xml, '', text);
+  self.sendString(text);
+  FreeAndNil(FXmlParser);
+end;
+
+procedure TNetwork.logFile(var logFile : TextFile);
+begin
+  FLog := true;
+  FFile := @logFile;
+end;
+
+procedure TNetwork.log(text : String);
+begin
+  if(Flog) then begin
+    writeln;
+    writeln(FFile^, text);
+  end;
+end;
+
+end.
---
+++ simple_clients/2011/delphi/src/simpleClient/UClient.pas
@@ -0,0 +1,130 @@
+unit UClient;
+
+interface
+  uses UPlayer, UBoard, UDebugHint, UMyBoard, UMove, USheep, SysUtils, Classes, UInteger;
+  type
+    TClient = class
+      protected
+        FMyId : Integer;
+        FPlayers : array [0..1] of TPlayer;
+        FBoard : TMyBoard;
+        FActivePlayer : String;
+        FTurn : Integer;
+        FHaveToEatSalad : Boolean;
+        Me : TPlayer;
+        Opponent : TPlayer;
+      public
+        function getPlayer(playerNum : Integer) : TPlayer; overload;
+        function getPlayer(displayName : String) : TPlayer; overload;
+        function getBoard : TBoard;
+        function zugAngefordert : TMove;
+        procedure setId(playerId : Integer);
+
+        property MyId : Integer read FMyId write FMyId;
+        property CurrentTurn : Integer read FTurn write FTurn;
+        constructor Create(board : TMyBoard);
+        destructor destroy; override;
+    end;
+
+implementation
+
+uses UNetwork;
+  destructor TClient.destroy;
+  begin
+    if(FPlayers[0] <> nil) then FPlayers[0].Free;
+    if(FPlayers[1] <> nil) then FPlayers[1].Free;
+
+    inherited;
+  end;
+
+  function TClient.zugAngefordert : TMove;
+    var
+      mov : TMove;
+      n, m, o : Integer;
+      sheeps : TList;
+      targets : TList;
+      die : Integer;
+      sheep : TSheep;
+    begin
+      // Die beiden Spieler zur Übersicht als Ich und Gegner ordnen
+      if(FPlayers[0].PlayerID = FMyId) then begin
+        Me := FPlayers[0];
+        Opponent := FPlayers[1];
+      end
+      else begin
+        me := FPlayers[1];
+        opponent := FPlayers[0];
+      end;
+
+      mov := TMove.create;
+
+      // Die Wurfel der Reihe nach durchprobieren bis irgendein gultiger Zug gefunden wurde.
+      for n := 0 to Length(FBoard.getDice) - 1 do begin
+        // Den n-ten Wurfel holen
+        die := FBoard.getDice[n];
+        sheeps := TList.create;
+        // Meine Schafe holen
+        FBoard.getSheepsFor(FMyId, sheeps);
+        for m := 0 to sheeps.Count - 1 do begin
+          sheep := TSheep(sheeps[m]);
+          targets := TList.Create;
+          // Die Felder holen, die ich mit dem n-ten Wurfel und dem gewahlten Schaf erreichen kann
+          FBoard.getNeighbourFields(TSheep(sheeps[m]).Node, die, targets);
+          for o := 0 to targets.Count - 1 do begin
+            // Den Zug probeweise vorbereiten
+            mov.moveTo(TSheep(sheeps[m]).SheepIndex, TInteger(targets[o]).Value);
+            // Wenn der Zug tatsachlich gultig ist, bin ich fertig.
+            if(FBoard.isMoveValid(mov)) then begin
+              Result := mov;
+              break;
+            end;
+          end;
+          FreeAndNil(targets);
+          if(Result <> nil) then break;
+        end;
+        FreeAndNil(sheeps);
+        if(Result <> nil) then break;
+      end;
+
+      // Einen Debug-Hinweis mit dem Zug ubertragen. Dieser wird dann im Server auf der GUI
+      // angezeigt.
+      mov.addHint(TDebugHint.create('Hinweis', 'Das ist ein zufalliger Zug!'));
+    end;
+
+  procedure TClient.setId(playerId : Integer);
+    begin
+      FMyId := playerId;
+    end;
+
+  function TClient.getPlayer(playerNum : Integer) : TPlayer;
+    begin
+      Result := FPlayers[playerNum];
+    end;
+
+  function TClient.getPlayer(displayName : String) : TPlayer;
+    begin
+      Result := nil;
+      if(FPlayers[0].DisplayName = displayName) then begin
+        Result := FPlayers[0];
+      end
+      else if(FPlayers[1].DisplayName = displayName) then begin
+        Result := FPlayers[1];
+      end;
+    end;
+
+  function TClient.getBoard : TBoard;
+    begin
+      Result := FBoard;
+    end;
+
+  constructor TClient.Create(board : TMyBoard);
+    begin
+      inherited Create;
+      // Die zwei Spieler werden schonmal erstellt.
+      FPlayers[0] := TPlayer.Create('Spieler 1');
+      FPlayers[1] := TPlayer.Create('Spieler 2');
+
+      FHaveToEatSalad := false;
+      FBoard := board;
+    end;
+end.
