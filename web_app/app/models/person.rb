c8fe11e694be9063caafccfd057d0fb49c5970f4
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2010-09-15 09:51:35 +0000

    email regex fix

--- web_app/app/models/person.rb
+++ web_app/app/models/person.rb
@@ -17,7 +17,7 @@ class Person < ActiveRecord::Base
 
   validates_uniqueness_of :email
   validates_presence_of :email
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "ist keine gültige Adresse"
+  validates_format_of :email, :with => /^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i, :message => "ist keine gültige Adresse"
 
   validates_presence_of :first_name
   validates_presence_of :last_name
