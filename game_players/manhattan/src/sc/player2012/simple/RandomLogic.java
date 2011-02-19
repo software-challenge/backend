81bba7f9fa7dce9c8b5d4c96fc93c43aba4116da
tkra <tkra@d26744b6-cbba-4121-8a74-f02c85b2345c>
2011-02-19 17:28:31 +0000

    MANHATTAN, initial import

---
+++ game_players/manhattan/src/sc/player2012/simple/RandomLogic.java
@@ -0,0 +1,76 @@
+package sc.player2012.simple;
+
+import java.util.List;
+import java.util.Random;
+
+import sc.player2012.Starter;
+import sc.plugin2012.DebugHint;
+import sc.plugin2012.GameState;
+import sc.plugin2012.IGameHandler;
+import sc.plugin2012.Move;
+import sc.plugin2012.Player;
+import sc.plugin2012.PlayerColor;
+import sc.shared.GameResult;
+
+public class RandomLogic implements IGameHandler
+{
+
+	private Starter	client;
+	private GameState		gameState;
+	private Player			currentPlayer;
+
+	public RandomLogic(Starter client)
+	{
+		this.client = client;
+	}
+
+	@Override
+	public void gameEnded(GameResult data, PlayerColor color,
+			String errorMessage)
+	{
+
+		System.out.println("*** das spiel ist beendet");
+	}
+
+	@Override
+	public void onRequestAction()
+	{
+		System.out.println("*** es wurde ein zug angefordert");
+
+	//	List<Move> moves = gameState.getValidMoves();
+		Random rand = new Random(System.currentTimeMillis());
+		//Move move = moves.get(rand.nextInt(moves.size()));
+		//move.addHint(new DebugHint("Strategie", "RANDOM"));
+		//sendAction(move);
+
+	}
+
+	@Override
+	public void onUpdate(Player player, Player otherPlayer)
+	{
+		currentPlayer = player;
+
+		System.out.println("*** spielerwechsel: " + player.getPlayerColor());
+
+	}
+
+	@Override
+	public void onUpdate(GameState gameState)
+	{
+		this.gameState = gameState;
+		currentPlayer = gameState.getCurrentPlayer();
+
+		System.out.println("*** das spiel geht vorran: " + gameState.getTurn()
+				+ " " + currentPlayer.getPlayerColor());
+	}
+
+	@Override
+	public void sendAction(Move move)
+	{
+		client.sendMove(move);
+
+//		System.out.println("*** sende zug: " + move.sheep + " -> "
+//				+ move.target);
+	}
+
+}
