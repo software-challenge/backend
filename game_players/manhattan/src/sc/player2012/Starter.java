6da6f74c81621fbc657e0b7b68debdb1af686b39
tkra <tkra@d26744b6-cbba-4121-8a74-f02c85b2345c>
2011-02-20 03:34:35 +0000

    Spass mit XStream-Konvertern

--- game_players/manhattan/src/sc/player2012/Starter.java
+++ game_players/manhattan/src/sc/player2012/Starter.java
@@ -2,6 +2,7 @@ package sc.player2012;
 
 import java.io.IOException;
 
+import sc.player2012.logic.RandomLogic;
 import sc.plugin2012.AbstractClient;
 import sc.plugin2012.IGameHandler;
 import sc.shared.SharedConfiguration;
@@ -11,8 +12,7 @@ import jargs.gnu.CmdLineParser.UnknownOptionException;
 
 public class Starter extends AbstractClient {
 
-	public Starter(String host, int port, String reservation, String strategy)
-			throws Exception {
+	public Starter(String host, int port, String reservation, String strategy) throws Exception {
 		// client starten
 		super(host, port);
 
@@ -22,14 +22,15 @@ public class Starter extends AbstractClient {
 		// Versuche für den strategy-Parameter eine passende Logik zu
 		// instanzieren,
 		// sonst verwende Standard
-		try {
-			logic = LogicFactory.valueOf(strategy.toUpperCase()).getInstance(
-					this);
-		} catch (Exception e) {
-			e.printStackTrace();
-			logic = LogicFactory.DEFAULT.getInstance(this);
-		}
-
+		// try {
+		// logic =
+		// LogicFactory.valueOf(strategy.toUpperCase()).getInstance(this);
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// logic = LogicFactory.DEFAULT.getInstance(this);
+		// }
+		
+		logic = new RandomLogic(this);
 		setHandler(logic);
 
 		// einem spiel beitreten
@@ -41,42 +42,37 @@ public class Starter extends AbstractClient {
 
 	}
 
-	public static void main(String[] args) throws IllegalOptionValueException,
-			UnknownOptionException, IOException {
+	public static void main(String[] args) throws IllegalOptionValueException, UnknownOptionException,
+			IOException {
 		System.setProperty("file.encoding", "UTF-8");
 
 		// parameter definieren
 		CmdLineParser parser = new CmdLineParser();
 		CmdLineParser.Option hostOption = parser.addStringOption('h', "host");
 		CmdLineParser.Option portOption = parser.addIntegerOption('p', "port");
-		CmdLineParser.Option strategyOption = parser.addStringOption('s',
-				"strategy");
-		CmdLineParser.Option reservationOption = parser.addStringOption('r',
-				"reservation");
+		CmdLineParser.Option strategyOption = parser.addStringOption('s', "strategy");
+		CmdLineParser.Option reservationOption = parser.addStringOption('r', "reservation");
 
 		try {
 			// Parameter auslesen
 			parser.parse(args);
 		} catch (CmdLineParser.OptionException e) { // Bei Fehler die Hilfe
-													// anzeigen
+			// anzeigen
 			showHelp(e.getMessage());
 			System.exit(2);
 		}
 
 		// Parameter laden
 		String host = (String) parser.getOptionValue(hostOption, "localhost");
-		int port = (Integer) parser.getOptionValue(portOption,
-				SharedConfiguration.DEFAULT_PORT);
-		String reservation = (String) parser.getOptionValue(reservationOption,
-				"");
+		int port = (Integer) parser.getOptionValue(portOption, SharedConfiguration.DEFAULT_PORT);
+		String reservation = (String) parser.getOptionValue(reservationOption, "");
 		String strategy = (String) parser.getOptionValue(strategyOption, "");
 
 		// einen neuen client erzeugen
 		try {
 			new Starter(host, port, reservation, strategy);
 		} catch (Exception e) {
-			System.err
-					.println("Beim Starten den Clients ist ein Fehler aufgetreten:");
+			System.err.println("Beim Starten den Clients ist ein Fehler aufgetreten:");
 			e.printStackTrace();
 		}
 
@@ -86,12 +82,11 @@ public class Starter extends AbstractClient {
 		System.out.println();
 		System.out.println(errorMsg);
 		System.out.println();
-		System.out
-				.println("Bitte das Programm mit folgenden Parametern (optional) aufrufen: \n"
-						+ "java -jar manhattan_player.jar [{-h,--host} hostname]\n"
-						+ "                               [{-p,--port} port]\n"
-						+ "                               [{-r,--reservation} reservierung]\n"
-						+ "                               [{-s,--strategy} strategie]");
+		System.out.println("Bitte das Programm mit folgenden Parametern (optional) aufrufen: \n"
+				+ "java -jar manhattan_player.jar [{-h,--host} hostname]\n"
+				+ "                               [{-p,--port} port]\n"
+				+ "                               [{-r,--reservation} reservierung]\n"
+				+ "                               [{-s,--strategy} strategie]");
 		System.out.println();
 		System.out
 				.println("Beispiel: \n"
