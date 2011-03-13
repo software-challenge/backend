09f66e4ac9dabed4dd8fac01692e07d8f05f3eab
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2011-03-13 14:03:09 +0000

    changed rails version, to server

--- web_app/config/environment.rb
+++ web_app/config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.7' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 ENV['VM_WATCH_FOLDER'] = File.join(File.dirname(__FILE__), '..', 'tmp', 'vmwatch')
 
 # Bootstrap the Rails environment, frameworks, and default configuration
