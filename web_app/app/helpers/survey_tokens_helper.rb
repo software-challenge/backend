2f455299e5d1b73bcd65878950d0740b4dce87c7
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2011-06-30 09:23:34 +0000

    changed surveyor stuff

--- web_app/app/helpers/survey_tokens_helper.rb
+++ web_app/app/helpers/survey_tokens_helper.rb
@@ -1,2 +1,18 @@
 module SurveyTokensHelper
+  def token_receiver_tooltip(token)
+     text = content_tag :b, token.token_owner.class.human_name
+     if token.token_owner.is_a? Person
+       text += ":<br> E-Mail: "
+       text += token.token_owner.email
+       text += "<br>Zuletzt online: #{I18n.l token.token_owner.last_seen.to_date}" if token.token_owner.last_seen 
+     elsif token.token_owner.is_a? PreliminaryContestant
+       text += "<b>-Voranmeldung:</b><br>Schule: "
+       text += token.token_owner.school.name
+       text += "<br>Bundesland: "
+       text += token.token_owner.school.state
+       text += "<br>Anmelder: "
+       text += token.token_owner.person.name
+     end
+     text.html_safe
+  end
 end
