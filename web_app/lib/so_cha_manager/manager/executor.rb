cf58fb06d696769f57a369ab6e3276562c0bda75
sca <sca@d26744b6-cbba-4121-8a74-f02c85b2345c>
2011-08-25 10:42:17 +0000

    Test

--- web_app/lib/so_cha_manager/manager/executor.rb
+++ web_app/lib/so_cha_manager/manager/executor.rb
@@ -15,6 +15,8 @@ module SoChaManager
             codes.each do |reservation|
               logger.info "Reservation ID: #{reservation}"
             end
+	    logger.info response.to_s
+            logger.info response['roomId']
             room_id = response.attributes['roomId'].value
 
             @client.observe room_id, "swordfish" do |success,response|
