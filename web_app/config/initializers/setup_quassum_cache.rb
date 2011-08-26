c64db1816777e7797cc363e6b5727267c304f9bd
and <and@d26744b6-cbba-4121-8a74-f02c85b2345c>
2011-08-26 01:24:33 +0000

    adding quassum for production and for development

--- web_app/config/initializers/setup_quassum_cache.rb
+++ web_app/config/initializers/setup_quassum_cache.rb
@@ -1,10 +1,19 @@
 QUASSUM = {}
 QUASSUM[:cache] = Rails.cache
-QUASSUM[:domain] = "sunlight.informatik.uni-kiel.de"
-QUASSUM[:project_slug] = "ein-projekt"
 
-# default user for getting infos from the quassum:
-QUASSUM[:user] = {:username => "sven@koschnicke.de", :token => "db1e123fa15bc06bfb85bcf70e99cfd7", :password => "cd960b526"}
+
+if RAILS_ENV == "development"
+  QUASSUM[:domain] = "sunlight.informatik.uni-kiel.de"
+  QUASSUM[:project_slug] = "ein-projekt"
+
+  # default user for getting infos from the quassum:
+  QUASSUM[:user] = {:username => "sven@koschnicke.de", :token => "db1e123fa15bc06bfb85bcf70e99cfd7", :password => "cd960b526"}
+
+elsif RAILS_ENV == "production"
+  QUASSUM[:domain] = "cau.quassum.com"
+  QUASSUM[:project_slug] = "Software Challenge"
+  QUASSUM[:user] = {:username => "software-challenge@gfxpro.eu", :token => "MISSING", :password => "MISSING" }
+end
 
 # setup settings for faye server
 QUASSUM[:faye] = {:url => "http://localhost:9292/faye"}
