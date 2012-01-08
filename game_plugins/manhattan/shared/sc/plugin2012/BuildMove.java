f9164de1bf624c8a72d7a8d4cd67e50a291d69e1
Felix <fdu@informatik.uni-kiel.de>
2012-01-08 12:01:46 +0100

    Bugfix #689 Interne Darstellung bei nicht vorhandener Baukarte in,  "für Menschen lesbaren" Index umgewandelt. (+1)

--- game_plugins/manhattan/shared/sc/plugin2012/BuildMove.java
+++ game_plugins/manhattan/shared/sc/plugin2012/BuildMove.java
@@ -83,7 +83,7 @@ public class BuildMove extends Move implements Cloneable {
 
 		if (!player.hasCard(slot)) {
 			throw new InvalideMoveException(player.getDisplayName() + " hat keine Karte für den Bauplatz "
-					+ slot);
+					+ slot + 1); //Bugfix #689
 		}
 
 		if (!tower.addPart(color, size)) {
