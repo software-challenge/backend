5db5a3f51826310670144664af80b1a87d4cde9c
FFittkau <ffi@informatik.uni-kiel.de>
2012-04-02 22:25:33 +0200

    git ignores

---
+++ game_plugins/qwirkle/shared/sc/plugin2014/StoneColor.java
@@ -0,0 +1,5 @@
+package sc.plugin2014;
+
+public enum StoneColor {
+    YELLOW, GREEN, RED, ORANGE, BLUE, PURPLE
+}
---
+++ game_plugins/qwirkle/shared/sc/plugin2014/StoneShape.java
@@ -0,0 +1,5 @@
+package sc.plugin2014;
+
+public enum StoneShape {
+    FLOWER, STAR, CIRCLE, RHOMBUS, SQUARE, FOUR_SPIKE
+}
