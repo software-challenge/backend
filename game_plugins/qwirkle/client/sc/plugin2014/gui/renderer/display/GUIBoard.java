1f87e05ceca3f4802fb94ad99e0d1a91b8aa50ab
FFittkau <ffi@informatik.uni-kiel.de>
2012-06-17 16:59:11 +0200

    gui enhancements

--- game_plugins/qwirkle/client/sc/plugin2014/gui/renderer/display/GUIBoard.java
+++ game_plugins/qwirkle/client/sc/plugin2014/gui/renderer/display/GUIBoard.java
@@ -5,17 +5,21 @@ import java.awt.Graphics2D;
 import java.util.List;
 import sc.plugin2014.entities.Board;
 import sc.plugin2014.entities.Field;
+import sc.plugin2014.util.Constants;
 
 public class GUIBoard {
-    public static void draw(Graphics2D g2, int xStart, int yStart,
-            List<GUIStone> toLayStones, Board board) {
+    public static void draw(Graphics2D g2, int xStart, int yStart, int width,
+            int height, List<GUIStone> toLayStones, Board board) {
+        int offsetX = calculateOffsetX(xStart, width);
+        int offsetY = calculateOffsetY(yStart, height);
+
         for (Field field : board.getFields()) {
             if (!field.isFree()) {
                 GUIStone stone = new GUIStone(field.getStone());
                 stone.setX((field.getPosX() * GUIConstants.STONE_WIDTH)
-                        + xStart);
+                        + offsetX);
                 stone.setY((field.getPosY() * GUIConstants.STONE_HEIGHT)
-                        + yStart);
+                        + offsetY);
                 stone.draw(g2);
             }
         }
@@ -24,34 +28,55 @@ public class GUIBoard {
             Field field = toLayStone.getField();
             if (field != null) {
                 toLayStone.setX((field.getPosX() * GUIConstants.STONE_WIDTH)
-                        + xStart);
+                        + offsetX);
                 toLayStone.setY((field.getPosY() * GUIConstants.STONE_HEIGHT)
-                        + yStart);
+                        + offsetY);
                 toLayStone.draw(g2);
             }
         }
     }
 
+    private static int calculateOffsetX(int xStart, int width) {
+        int boardWidth = GUIConstants.STONE_WIDTH * Constants.FIELDS_IN_X_DIM;
+        int deltaWidth = width - boardWidth;
+        int gapWidth = deltaWidth / 2;
+
+        return xStart + gapWidth;
+    }
+
+    private static int calculateOffsetY(int yStart, int height) {
+        int boardHeight = GUIConstants.STONE_HEIGHT * Constants.FIELDS_IN_Y_DIM;
+        int deltaHeight = height - boardHeight;
+        int gapHeight = deltaHeight / 2;
+        return yStart + gapHeight;
+    }
+
     public static void drawGrid(Graphics2D g2, int xStart, int yStart,
             int width, int height) {
+        int offsetX = calculateOffsetX(xStart, width);
+        int offsetY = calculateOffsetY(yStart, height);
+
         g2.setColor(DisplayHelper.getTransparentColor(Color.WHITE, 174));
-        int x = xStart + GUIConstants.STONE_WIDTH;
-        while (x < width) {
-            g2.drawLine(x, yStart, x, yStart + height);
-            x += GUIConstants.STONE_WIDTH;
+        for (int i = 1; i < Constants.FIELDS_IN_X_DIM; i++) {
+            int x = offsetX + (i * GUIConstants.STONE_WIDTH);
+            g2.drawLine(x, offsetY, x, offsetY
+                    + (Constants.FIELDS_IN_Y_DIM * GUIConstants.STONE_HEIGHT));
         }
 
-        int y = yStart + GUIConstants.STONE_HEIGHT;
-        while (y < height) {
-            g2.drawLine(xStart, y, xStart + width, y);
-            y += GUIConstants.STONE_HEIGHT;
+        for (int i = 1; i < Constants.FIELDS_IN_Y_DIM; i++) {
+            int y = offsetY + (i * GUIConstants.STONE_HEIGHT);
+            g2.drawLine(offsetX, y, offsetX
+                    + (Constants.FIELDS_IN_X_DIM * GUIConstants.STONE_WIDTH), y);
         }
     }
 
     public static Field getBelongingField(Board board, int xStart, int yStart,
-            GUIStone stone) {
-        int x = (stone.getX() - xStart) / GUIConstants.STONE_WIDTH;
-        int y = (stone.getY() - yStart) / GUIConstants.STONE_HEIGHT;
+            int width, int height, GUIStone stone) {
+        int offsetX = calculateOffsetX(xStart, width);
+        int offsetY = calculateOffsetY(yStart, height);
+
+        int x = (stone.getX() - offsetX) / GUIConstants.STONE_WIDTH;
+        int y = (stone.getY() - offsetY) / GUIConstants.STONE_HEIGHT;
 
         try {
             return board.getField(x, y);
