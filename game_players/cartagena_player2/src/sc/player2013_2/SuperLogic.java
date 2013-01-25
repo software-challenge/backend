bc2b21d063bcf696c1582c3cfd91ba6ff532c200
Felix Dubrownik <fdu@informatik.uni-kiel.de>
2013-01-25 12:31:32 +0100

    not so simple client implementiert

---
+++ game_players/cartagena_player2/src/sc/player2013_2/SuperLogic.java
@@ -0,0 +1,127 @@
+package sc.player2013_2;
+
+import java.security.SecureRandom;
+import java.util.LinkedList;
+import java.util.Random;
+
+import kiModell.MoveRater;
+
+import sc.plugin2013.BackwardMove;
+import sc.plugin2013.ForwardMove;
+import sc.plugin2013.GameState;
+import sc.plugin2013.IGameHandler;
+import sc.plugin2013.Move;
+import sc.plugin2013.MoveContainer;
+import sc.plugin2013.Player;
+import sc.plugin2013.PlayerColor;
+import sc.plugin2013.util.InvalidMoveException;
+import sc.shared.GameResult;
+
+/**
+ * Das Herz des Simpleclients: Eine sehr simple Logik, die ihre Zuege zufaellig
+ * waehlt, aber gueltige Zuege macht. Ausserdem werden zum Spielverlauf
+ * Konsolenausgaben gemacht.
+ */
+public class SuperLogic implements IGameHandler {
+
+	private Starter client;
+	private GameState gameState;
+	private Player currentPlayer;
+	private MoveRater moveRater;
+
+	/*
+	 * Klassenweit verfuegbarer Zufallsgenerator der beim Laden der klasse
+	 * einmalig erzeugt wird und dann immer zur Verfuegung steht.
+	 */
+	private static final Random rand = new SecureRandom();
+
+	/**
+	 * Erzeugt ein neues Strategieobjekt, das zufaellige Zuege taetigt.
+	 * 
+	 * @param client
+	 *            Der Zugrundeliegende Client der mit dem Spielserver
+	 *            kommunizieren kann.
+	 */
+	public SuperLogic(Starter client) {
+		this.client = client;
+		this.moveRater = new MoveRater();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void gameEnded(GameResult data, PlayerColor color,
+			String errorMessage) {
+
+		System.out.println("*** Das Spiel ist beendet");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onRequestAction() {
+		long currentTime = System.currentTimeMillis();
+		System.out.println("*** Es wurde ein Zug angefordert");
+		// Erstelle einen neuen MoveContainer, welcher unsere 3 Teilzüge
+		// beherbergt
+		MoveContainer moveC = new MoveContainer();
+		
+		moveC = moveRater.getBestNextMove(gameState);
+		
+		cloneGameStateSevenTimes();
+		
+		System.out.println("MoveContainer firstMove Index: " + moveC.firstMove.fieldIndex);
+
+		// Sende den Container mit allen durchgeführten Zügen.
+		sendAction(moveC);
+		System.out.println("Zugzeit:" + (System.currentTimeMillis() - currentTime));
+	}
+
+	private void cloneGameStateSevenTimes() {
+		for(int i = 0; i < 8; i++){
+			try {
+				GameState gs = (GameState) gameState.clone();
+				gs.gameEnded();
+			} catch (CloneNotSupportedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onUpdate(Player player, Player otherPlayer) {
+		currentPlayer = player;
+
+		System.out.println("*** Spielerwechsel: " + player.getPlayerColor());
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onUpdate(GameState gameState) {
+		this.gameState = gameState;
+		currentPlayer = gameState.getCurrentPlayer();
+
+		System.out.print("*** Das Spiel geht vorran: Zug = "
+				+ gameState.getTurn());
+		System.out.println(", Spieler = " + currentPlayer.getPlayerColor());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void sendAction(MoveContainer move) {
+		client.sendMove(move);
+	}
+
+}
