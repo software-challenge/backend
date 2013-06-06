2d1aa61266d1027f24956836c9576b85da2b61d5
Felix Dubrownik <fdu@informatik.uni-kiel.de>
2013-06-06 10:03:50 +0200

    sixpack: StoneShape.DIAMONT in DIAMOND umbenannt. XML Doku

--- game_plugins/sixpack/shared/sc/plugin2014/entities/StoneShape.java
+++ game_plugins/sixpack/shared/sc/plugin2014/entities/StoneShape.java
@@ -1,7 +1,7 @@
 package sc.plugin2014.entities;
 
 public enum StoneShape {
-    ACORN, BELL, CLUBS, DIAMONT, HEART, SPADES;
+    ACORN, BELL, CLUBS, DIAMOND, HEART, SPADES;
 
     public static StoneShape getShapeFromIndex(int index) {
         return StoneShape.values()[index];
