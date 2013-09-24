43ddd845bda4873f4f500035fbb1273920477949
Sven C <svencasi@web.de>
2013-09-24 12:37:24 +0200

    - Use of SecureRandom in StoneBag.java changed to avoid unnecessary reinitialization of PRNG - GameState.java and GameStateConverter.java changed to that a bare GameState is created during unmarshal (prevent unnecessary class instantiations).

--- game_plugins/sixpack/shared/sc/plugin2014/entities/StoneBag.java
+++ game_plugins/sixpack/shared/sc/plugin2014/entities/StoneBag.java
@@ -11,8 +11,10 @@ public class StoneBag implements Cloneable {
 
 	private final List<Stone> stones;
 	private final List<Stone> nextStones;
-
-	public StoneBag() {
+	
+	private SecureRandom sr;
+	
+	public StoneBag()  {
 		stones = new ArrayList<Stone>(Constants.STONES_COLOR_COUNT
 				* Constants.STONES_SHAPE_COUNT
 				* Constants.STONES_SAME_KIND_COUNT);
@@ -26,6 +28,13 @@ public class StoneBag implements Cloneable {
 				}
 			}
 		}
+		try {
+			sr = SecureRandom.getInstance("SHA1PRNG");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			sr = new SecureRandom();
+		}
+		sr.nextBytes(new byte[1]);
 		randomizeStones();
 
 		refreshNextStones();
@@ -43,22 +52,6 @@ public class StoneBag implements Cloneable {
 	}
 
 	private void randomizeStones() {
-		SecureRandom sr = null;
-		try {
-			sr = SecureRandom.getInstance("SHA1PRNG");
-
-			byte[] bytes = new byte[1024 / 8];
-			sr.nextBytes(bytes);
-
-			int seedByteCount = 10;
-			byte[] seed = sr.generateSeed(seedByteCount);
-
-			sr = SecureRandom.getInstance("SHA1PRNG");
-			sr.setSeed(seed);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-
 		Collections.shuffle(stones, sr);
 	}
 
