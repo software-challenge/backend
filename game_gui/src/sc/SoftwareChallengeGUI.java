a9c9ebe385cea42de0ecb907eb20ec0d9799f008
Felix Dubrownik <fdubrownik@gmail.com>
2013-10-10 11:44:11 +0200

    kommentar hinzugefügt

--- game_gui/src/sc/SoftwareChallengeGUI.java
+++ game_gui/src/sc/SoftwareChallengeGUI.java
@@ -203,7 +203,7 @@ public class SoftwareChallengeGUI extends JFrame implements IGUIApplication {
 			}
 		}
 		// quit application
-		presFac.getContextDisplay().cancelCurrentGame();
+		presFac.getContextDisplay().cancelCurrentGame(); //make sure replays are saved on quit
 		presFac.shutdown();
 		System.exit(0);
 	}
