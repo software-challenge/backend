b3d859a6d7241d560c75ee28537cfac297edf224
manfred4 <niklas.rentz@web.de>
2014-03-07 17:44:41 +0100

    Basiszustand für Eclipse

--- vm_administration/scripts/startVMConsumer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#/bin/java -jar /home/vbox/daemonconsumer.jar -h 192.168.56.2 -q vm-queue -b /bin/bash -c /home/vbox/startVM.sh
-/bin/java -jar /home/vbox/daemonconsumer.jar -h 134.245.253.5 -q vm-queue -b /bin/bash -c /home/vbox/startVM.sh
--- vm_administration/src/com/rra/IConsumerListener.java
+++
@@ -1,5 +0,0 @@
-package com.rra;
-
-public interface IConsumerListener {
-	public void onStartConsuming();
-}
