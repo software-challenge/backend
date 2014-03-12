8432f2ec5b19a18492c87112891568b624b11b50
manfred4 <niklas.rentz@web.de>
2014-03-12 13:51:26 +0100

    test rückgänging machen

--- bin/game_plugins/schaefchen_im_trockenen/documents/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-latex schaefchen_rules.tex
-dvips schaefchen_rules.dvi
-ps2pdf -dSAFER -sPAPERSIZE=a4 schaefchen_rules.ps
-
-latex schaefchen_server.tex
-dvips schaefchen_server.dvi
-ps2pdf -dSAFER -sPAPERSIZE=a4 schaefchen_server.ps
--- bin/vm_administration/scripts/startSWCConsumer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/bin/java -jar /home/scadmin/consumer.jar -h 192.168.56.2 -q swc-job-queue -b /bin/bash -c /home/scadmin/consume.sh
