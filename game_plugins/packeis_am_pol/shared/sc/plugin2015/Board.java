86eeae00647347d9dd0e7974ecc1d1c87a8ad144
soerendomroes <soed@kabelmail.de>
2014-03-20 10:35:53 +0100

    Board erstellt (leer)

---
+++ game_plugins/packeis_am_pol/shared/sc/plugin2015/Board.java
@@ -0,0 +1,22 @@
+package sc.plugin2015;
+
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+import sc.plugin2015.util.Constants;
+
+/**
+ * Klasse welche eine Spielbrett darstellt. 
+ * 
+ * 
+ * @author 
+ * 
+ */
+
