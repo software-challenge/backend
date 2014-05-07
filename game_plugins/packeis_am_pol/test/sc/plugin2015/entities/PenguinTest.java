dd992b7252ccc4257b8321b20bb5f100c386a761
manfred4 <niklas.rentz@web.de>
2014-05-07 18:18:45 +0200

    weitere Testfaelle

--- game_plugins/packeis_am_pol/test/sc/plugin2015/entities/PenguinTest.java
+++ game_plugins/packeis_am_pol/test/sc/plugin2015/entities/PenguinTest.java
@@ -33,16 +33,5 @@ public class PenguinTest {
 			e.printStackTrace();
 		}
 	}
-	
-    /*@Test
-    public void testStone() {
-        Stone stone = new Stone();
-        assertEquals(StoneColor.BLUE, stone.getColor());
-        assertEquals(StoneShape.ACORN, stone.getShape());
-
-        Stone stoneGreenFlower = new Stone(StoneColor.GREEN, StoneShape.BELL);
-        assertEquals(StoneColor.GREEN, stoneGreenFlower.getColor());
-        assertEquals(StoneShape.BELL, stoneGreenFlower.getShape());
-    }*/
 
 }
