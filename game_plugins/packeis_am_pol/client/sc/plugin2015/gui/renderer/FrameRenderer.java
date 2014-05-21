74ee3811f1793e760727c11c04f3f80fa5396706
Felix Dubrownik <fdubrownik@gmail.com>
2014-05-21 13:55:17 +0200

    RenderConfiguration angepasst

--- game_plugins/packeis_am_pol/client/sc/plugin2015/gui/renderer/FrameRenderer.java
+++ game_plugins/packeis_am_pol/client/sc/plugin2015/gui/renderer/FrameRenderer.java
@@ -3,20 +3,13 @@
  */
 package sc.plugin2015.gui.renderer;
 
-import static sc.plugin2015.gui.renderer.RenderConfiguration.ANTIALIASING;
-import static sc.plugin2015.gui.renderer.RenderConfiguration.BACKGROUND;
-import static sc.plugin2015.gui.renderer.RenderConfiguration.OPTIONS;
-import static sc.plugin2015.gui.renderer.RenderConfiguration.TRANSPARANCY;
-import static sc.plugin2015.gui.renderer.RenderConfiguration.MOVEMENT;
-import static sc.plugin2015.gui.renderer.RenderConfiguration.DEBUG_VIEW;
-
-import java.awt.Dimension;
 import java.awt.Image;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import processing.core.PApplet;
+import sc.plugin2015.gui.renderer.RenderConfigurationDialog;
 import sc.plugin2015.GameState;
 import sc.plugin2015.gui.renderer.primitives.Background;
 import sc.plugin2015.gui.renderer.primitives.GuiBoard;
@@ -42,10 +35,10 @@ public class FrameRenderer extends PApplet {
 
 	public void setup() {
 		//logger.debug("calling frameRenderer.size()");
-		size(this.width	, this.height , JAVA2D);	// Size and Renderer: either P2D, P3D or nothing(Java2D)
+		size(this.width	, this.height , P2D);	// Size and Renderer: either P2D, P3D or nothing(Java2D)
 		
 		noLoop();				// prevent thread from starving everything else
-		smooth(8);				// Anti Aliasing to 4
+		smooth(2);				// Anti Aliasing
 		
 		background = new Background(this);
 		logger.debug("Dimension when creating board: (" + this.width + "," + this.height + ")");
@@ -85,6 +78,13 @@ public class FrameRenderer extends PApplet {
 	
 	public void resize(){
 		
+	}
+	
+	public void keyPressed() {
+	if (key == 'c' || key == 'C') {
+			new RenderConfigurationDialog(FrameRenderer.this);
+	    }
+	
 	}
 
 }
