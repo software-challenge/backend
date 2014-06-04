f61af88a3a33e3694d0b923aa8368460df7ba96c
manfred4 <niklas.rentz@web.de>
2014-06-04 16:01:29 +0200

    fertig für heute

--- game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/FrameRenderer.java
+++ game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/FrameRenderer.java
@@ -38,19 +38,18 @@ public class FrameRenderer extends PApplet {
 	private ProgressBar progressBar;
 	private SideBar sidebar;
 	private BoardFrame boardFrame;
-	
-	//penguin as [OWNER][NUMBER]
+
+	// penguin as [OWNER][NUMBER]
 	private GuiPenguin[][] penguin;
 
-	 private GuiPenguin testPenguin;
-	 private GuiPenguin testPenguin2;
-	 private GuiPenguin testPenguin3;
-	 private GuiPenguin testPenguin4;
-	 private GuiPenguin testPenguin5;
-	 private GuiPenguin testPenguin6;
-	 private GuiPenguin testPenguin7;
-	 private GuiPenguin testPenguin8;
-	 
+	private GuiPenguin testPenguin;
+	private GuiPenguin testPenguin2;
+	private GuiPenguin testPenguin3;
+	private GuiPenguin testPenguin4;
+	private GuiPenguin testPenguin5;
+	private GuiPenguin testPenguin6;
+	private GuiPenguin testPenguin7;
+	private GuiPenguin testPenguin8;
 
 	public void setup() {
 		// logger.debug("calling frameRenderer.size()");
@@ -89,21 +88,22 @@ public class FrameRenderer extends PApplet {
 		penguin[1][1] = new GuiPenguin(this, -2, -1, PlayerColor.BLUE);
 		penguin[1][2] = new GuiPenguin(this, -3, -1, PlayerColor.BLUE);
 		penguin[1][3] = new GuiPenguin(this, -4, -1, PlayerColor.BLUE);
-		/*testPenguin = new GuiPenguin(this, 0, 0, PlayerColor.BLUE);
-		testPenguin2 = new GuiPenguin(this, 1, 0, PlayerColor.BLUE);
-		testPenguin3 = new GuiPenguin(this, 0, 1, PlayerColor.BLUE);
-		testPenguin4 = new GuiPenguin(this, 1, 1, PlayerColor.BLUE);
-		testPenguin5 = new GuiPenguin(this, 5, 0, PlayerColor.BLUE);
-		testPenguin6 = new GuiPenguin(this, 6, 0, PlayerColor.BLUE);
-		testPenguin7 = new GuiPenguin(this, 6, 1, PlayerColor.BLUE);
-		testPenguin8 = new GuiPenguin(this, 7, 1, PlayerColor.BLUE);*/
+		/*
+		 * testPenguin = new GuiPenguin(this, 0, 0, PlayerColor.BLUE);
+		 * testPenguin2 = new GuiPenguin(this, 1, 0, PlayerColor.BLUE);
+		 * testPenguin3 = new GuiPenguin(this, 0, 1, PlayerColor.BLUE);
+		 * testPenguin4 = new GuiPenguin(this, 1, 1, PlayerColor.BLUE);
+		 * testPenguin5 = new GuiPenguin(this, 5, 0, PlayerColor.BLUE);
+		 * testPenguin6 = new GuiPenguin(this, 6, 0, PlayerColor.BLUE);
+		 * testPenguin7 = new GuiPenguin(this, 6, 1, PlayerColor.BLUE);
+		 * testPenguin8 = new GuiPenguin(this, 7, 1, PlayerColor.BLUE);
+		 */
 
 		// initial draw
-		
+
 		boardFrame = new BoardFrame(this);
-		
-		
-		//initial draw
+
+		// initial draw
 		background.draw();
 		guiBoard.draw();
 		progressBar.draw();
@@ -113,22 +113,13 @@ public class FrameRenderer extends PApplet {
 				penguin[i][j].draw();
 			}
 		}/*
-		 testPenguin.resize();
-		 testPenguin.draw();
-		 testPenguin2.resize();
-		 testPenguin2.draw();
-		 testPenguin3.resize();
-		 testPenguin3.draw();
-		 testPenguin4.resize();
-		 testPenguin4.draw();
-		 testPenguin5.resize();
-		 testPenguin5.draw();
-		 testPenguin6.resize();
-		 testPenguin6.draw();
-		 testPenguin7.resize();
-		 testPenguin7.draw();
-		 testPenguin8.resize();
-		 testPenguin8.draw();*/
+		 * testPenguin.resize(); testPenguin.draw(); testPenguin2.resize();
+		 * testPenguin2.draw(); testPenguin3.resize(); testPenguin3.draw();
+		 * testPenguin4.resize(); testPenguin4.draw(); testPenguin5.resize();
+		 * testPenguin5.draw(); testPenguin6.resize(); testPenguin6.draw();
+		 * testPenguin7.resize(); testPenguin7.draw(); testPenguin8.resize();
+		 * testPenguin8.draw();
+		 */
 		boardFrame.draw();
 	}
 
@@ -143,26 +134,25 @@ public class FrameRenderer extends PApplet {
 				penguin[i][j].draw();
 			}
 		}/*
-		testPenguin.draw();
-		testPenguin2.draw();
-		testPenguin3.draw();
-		testPenguin4.draw();
-		testPenguin5.draw();
-		testPenguin6.draw();
-		testPenguin7.draw();
-		testPenguin8.draw();*/
+		 * testPenguin.draw(); testPenguin2.draw(); testPenguin3.draw();
+		 * testPenguin4.draw(); testPenguin5.draw(); testPenguin6.draw();
+		 * testPenguin7.draw(); testPenguin8.draw();
+		 */
 		boardFrame.draw();
 	}
 
 	public void updateGameState(GameState gameState) {
-		System.out.println("Update fährt");
+		PlayerColor lastPlayerColor;
 		guiBoard.update(gameState.getBoard());
 		int i = gameState.getCurrentPlayerColor() == PlayerColor.RED ? 1 : 0;
 		for (int j = 0; j < 4; j++) {
-			penguin[i][j]
-					.update(gameState.getLastMove(),
-							(gameState.getCurrentPlayerColor() == PlayerColor.RED ? PlayerColor.BLUE
-									: PlayerColor.RED), gameState.getTurn());
+			if (gameState.getTurn() == 9) {
+				lastPlayerColor = gameState.getCurrentPlayerColor();
+			} else {
+				lastPlayerColor = gameState.getOtherPlayerColor();
+			}
+			penguin[i][j].update(gameState.getLastMove(), lastPlayerColor,
+					gameState.getTurn());
 		}
 		redraw();
 	}
