ad61a23f5e38c91132ed69c119d891b2d7ce0b59
Felix Dubrownik <fdubrownik@gmail.com>
2014-06-04 13:45:13 +0200

    Side und Progressbar

--- game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/primitives/ProgressBar.java
+++ game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/primitives/ProgressBar.java
@@ -4,28 +4,41 @@ import processing.core.PApplet;
 
 /**
  * Zeichnet den Spielverlauf. Rundenanzahl + Bar
+ * 
  * @author felix
- *
+ * 
  */
-public class ProgressBar extends PrimitiveBase{
+public class ProgressBar extends PrimitiveBase {
 
 	public ProgressBar(PApplet par) {
 		super(par);
 	}
 
-
 	@Override
 	public void draw() {
 		parent.pushStyle();
-		
-		parent.stroke(1.0f);		// Umrandung
-		parent.fill(30, 30, 30);	// Filling
-		
+
+
+		// Umrandung
 		parent.pushMatrix();
-		parent.translate(50	, parent.getHeight() - 30);
+		parent.translate(0, parent.getHeight() * GuiConstants.SIDE_BAR_HEIGHT);
+		parent.fill(GuiConstants.colorSideBarBG);
+		parent.rect(0, 0, parent.getWidth(),
+				parent.getHeight() - parent.getHeight()
+						* GuiConstants.SIDE_BAR_HEIGHT);
+
+		parent.popMatrix();
+
+		// Statusbalken
+		parent.pushMatrix();
+		
+		parent.stroke(1.0f); // Umrandung
+		parent.fill(30, 30, 30); // Filling
+		
+		parent.translate(50, parent.getHeight() - 30);
 		parent.rect(0, 0, parent.getWidth() - 60, 20, 7);
 		parent.popMatrix();
-		
+
 		parent.popStyle();
 	}
 
