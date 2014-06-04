2c4881c825023fc9e0c5ba83d6329c18f98adb2a
Felix Dubrownik <fdubrownik@gmail.com>
2014-06-04 13:11:46 +0200

    Background image hinzugefügt

--- game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/primitives/Background.java
+++ game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/primitives/Background.java
@@ -1,17 +1,21 @@
 package sc.plugin2015.gui.renderer.primitives;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Background {
 	
 	PApplet parent;
+	PImage image;
 
 	public Background(PApplet parent) {
 		this.parent = parent;
+		image = parent.loadImage(GuiConstants.BACKGROUND_IMAGE);
 	}
 	
 	public void draw(){
-		parent.background(parent.color(250, 250, 255));
+		parent.background(GuiConstants.colorBackGround);
+		parent.image(image, 0, 0);
 	}
 
 }
