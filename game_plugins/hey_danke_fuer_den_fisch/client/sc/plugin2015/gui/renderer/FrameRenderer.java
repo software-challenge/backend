831e04ac03eff260b56e4c109e12be1609486da3
Felix Dubrownik <fdubrownik@gmail.com>
2014-06-18 11:14:09 +0200

    Konstruktor eingführt. Initialisierung geschieht nun hier.

--- game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/FrameRenderer.java
+++ game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/FrameRenderer.java
@@ -43,8 +43,6 @@ public class FrameRenderer extends PApplet {
 
 	private EPlayerId id;
 
-	
-
 	private GuiBoard guiBoard;
 	private Background background;
 	private ProgressBar progressBar;
@@ -54,37 +52,14 @@ public class FrameRenderer extends PApplet {
 	// penguin as [OWNER][NUMBER]
 	private GuiPenguin[][] penguin;
 
-	private GuiPenguin testPenguin;
-	private GuiPenguin testPenguin2;
-	private GuiPenguin testPenguin3;
-	private GuiPenguin testPenguin4;
-	private GuiPenguin testPenguin5;
-	private GuiPenguin testPenguin6;
-	private GuiPenguin testPenguin7;
-	private GuiPenguin testPenguin8;
+	public FrameRenderer() {
+		super();
 
-	public void setup() {
-		this.frameRate(30);
 		// logger.debug("calling frameRenderer.size()");
 		this.humanPlayer = false;
 		this.id = EPlayerId.OBSERVER;
 
 		RenderConfiguration.loadSettings();
-
-		// choosing renderer from options - using P2D as default
-		if (RenderConfiguration.optionRenderer.equals("JAVA2D")) {
-			size(this.width, this.height, JAVA2D);
-			logger.debug("Using P2D as Renderer");
-		} else if (RenderConfiguration.optionRenderer.equals("P3D")) {
-			size(this.width, this.height, P3D);
-			logger.debug("Using P3D as Renderer");
-		} else {
-			size(this.width, this.height, P2D);
-			logger.debug("Using Java2D as Renderer");
-		}
-
-		noLoop(); // prevent thread from starving everything else
-		smooth(RenderConfiguration.optionAntiAliasing); // Anti Aliasing
 
 		background = new Background(this);
 		logger.debug("Dimension when creating board: (" + this.width + ","
@@ -103,43 +78,33 @@ public class FrameRenderer extends PApplet {
 		penguin[1][1] = new GuiPenguin(this, -2, -1, PlayerColor.BLUE);
 		penguin[1][2] = new GuiPenguin(this, -3, -1, PlayerColor.BLUE);
 		penguin[1][3] = new GuiPenguin(this, -4, -1, PlayerColor.BLUE);
-		/*
-		 * testPenguin = new GuiPenguin(this, 0, 0, PlayerColor.BLUE);
-		 * testPenguin2 = new GuiPenguin(this, 1, 0, PlayerColor.BLUE);
-		 * testPenguin3 = new GuiPenguin(this, 0, 1, PlayerColor.BLUE);
-		 * testPenguin4 = new GuiPenguin(this, 1, 1, PlayerColor.BLUE);
-		 * testPenguin5 = new GuiPenguin(this, 5, 0, PlayerColor.BLUE);
-		 * testPenguin6 = new GuiPenguin(this, 6, 0, PlayerColor.BLUE);
-		 * testPenguin7 = new GuiPenguin(this, 6, 1, PlayerColor.BLUE);
-		 * testPenguin8 = new GuiPenguin(this, 7, 1, PlayerColor.BLUE);
-		 */
-
-		// initial draw
 
 		boardFrame = new BoardFrame(this);
+	}
+
+	public void setup() {
+		this.frameRate(30);
+		// choosing renderer from options - using P2D as default
+		if (RenderConfiguration.optionRenderer.equals("JAVA2D")) {
+			size(this.width, this.height, JAVA2D);
+			logger.debug("Using P2D as Renderer");
+		} else if (RenderConfiguration.optionRenderer.equals("P3D")) {
+			size(this.width, this.height, P3D);
+			logger.debug("Using P3D as Renderer");
+		} else {
+			size(this.width, this.height, P2D);
+			logger.debug("Using Java2D as Renderer");
+		}
+
+		//noLoop(); // prevent thread from starving everything else
+		smooth(RenderConfiguration.optionAntiAliasing); // Anti Aliasing
 
 		// initial draw
-		background.draw();
-		guiBoard.draw();
-		progressBar.draw();
-		sidebar.draw();
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 4; j++) {
-				penguin[i][j].draw();
-			}
-		}/*
-		 * testPenguin.resize(); testPenguin.draw(); testPenguin2.resize();
-		 * testPenguin2.draw(); testPenguin3.resize(); testPenguin3.draw();
-		 * testPenguin4.resize(); testPenguin4.draw(); testPenguin5.resize();
-		 * testPenguin5.draw(); testPenguin6.resize(); testPenguin6.draw();
-		 * testPenguin7.resize(); testPenguin7.draw(); testPenguin8.resize();
-		 * testPenguin8.draw();
-		 */
-		boardFrame.draw();
+		resize();
+		redraw();
 	}
 
 	public void draw() {
-		this.resize();
 		background.draw();
 		guiBoard.draw();
 		progressBar.draw();
@@ -148,11 +113,7 @@ public class FrameRenderer extends PApplet {
 			for (int j = 0; j < 4; j++) {
 				penguin[i][j].draw();
 			}
-		}/*
-		 * testPenguin.draw(); testPenguin2.draw(); testPenguin3.draw();
-		 * testPenguin4.draw(); testPenguin5.draw(); testPenguin6.draw();
-		 * testPenguin7.draw(); testPenguin8.draw();
-		 */
+		}
 		boardFrame.draw();
 	}
 
@@ -170,7 +131,7 @@ public class FrameRenderer extends PApplet {
 			i = gameState.getCurrentPlayerColor() == PlayerColor.RED ? 1 : 0;
 		}
 		for (int j = 0; j < 4; j++) {
-			System.out.println(" test "+ penguin[i][j].getFieldX());
+			System.out.println(" test " + penguin[i][j].getFieldX());
 			penguin[i][j].update(gameState.getLastMove(), lastPlayerColor,
 					gameState.getTurn());
 		}
@@ -190,8 +151,8 @@ public class FrameRenderer extends PApplet {
 	}
 
 	public void mouseClicked() {
-		// this.resize();
-		// this.redraw();
+		//this.resize();
+		//this.redraw();
 	}
 
 	public void mousePressed(MouseEvent e) {
@@ -310,11 +271,11 @@ public class FrameRenderer extends PApplet {
 		}
 
 	}
-	
+
 	public boolean isHumanPlayer() {
 		return humanPlayer;
 	}
-	
+
 	public EPlayerId getId() {
 		return id;
 	}
