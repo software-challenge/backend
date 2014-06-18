fd82d665687daecfbb4a7034b6f26523404a4f12
manfred4 <niklas.rentz@web.de>
2014-06-18 13:32:11 +0200

    added GuiButton

---
+++ game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/primitives/GuiButton.java
@@ -0,0 +1,31 @@
+package sc.plugin2015.gui.renderer.primitives;
+
+import sc.plugin2015.gui.renderer.FrameRenderer;
+
+public class GuiButton extends PrimitiveBase {
+	
+	
+
+	public GuiButton(FrameRenderer parent) {
+		super(parent);
+	}
+
+	@Override
+	public void draw() {
+		parent.pushMatrix();
+		parent.translate(parent.getWidth() / 2f - 50, parent.getHeight() * GuiConstants.SIDE_BAR_HEIGHT + 5);
+		parent.stroke(1.0f);
+		parent.fill(GuiConstants.colorLightGrey);
+		parent.rect(0, 0, 100, 25, 3);
+		parent.translate(3, 20);
+		parent.fill(GuiConstants.colorBlack);
+		parent.textSize(19);
+		parent.text("Aussetzen", 0, 0);
+		
+		
+		
+		
+		parent.popMatrix();
+	}
+
+}
