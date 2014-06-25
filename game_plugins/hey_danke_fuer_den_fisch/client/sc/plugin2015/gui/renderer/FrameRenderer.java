6e0c5d6ec41ffd799daa2b6437c0532a488a6915
soerendomroes <soed@kabelmail.de>
2014-06-25 15:50:47 +0200

    gameEnded

--- game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/FrameRenderer.java
+++ game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/FrameRenderer.java
@@ -143,7 +143,7 @@ public class FrameRenderer extends PApplet {
 			guiBoard.update(gameState.getBoard());
 		if (currentGameState == null
 				|| lastTurn == currentGameState.getTurn() - 1) {
-			if (maxTurn == currentGameState.getTurn() - 1)
+			if (maxTurn == currentGameState.getTurn() - 1) {
 				maxTurn++;
 				humanPlayerMaxTurn = false;
 			}
