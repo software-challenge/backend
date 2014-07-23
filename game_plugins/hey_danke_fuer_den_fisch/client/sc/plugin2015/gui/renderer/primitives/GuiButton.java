217790e210ba9fbae41dfdb5b5e6323a59639887
soerendomroes <soed@kabelmail.de>
2014-07-23 11:06:58 +0200

    fixes #32

--- game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/primitives/GuiButton.java
+++ game_plugins/hey_danke_fuer_den_fisch/client/sc/plugin2015/gui/renderer/primitives/GuiButton.java
@@ -1,5 +1,7 @@
 package sc.plugin2015.gui.renderer.primitives;
 
+import sc.plugin2015.Player;
+import sc.plugin2015.PlayerColor;
 import sc.plugin2015.gui.renderer.FrameRenderer;
 
 public class GuiButton extends PrimitiveBase {
@@ -12,21 +14,40 @@ public class GuiButton extends PrimitiveBase {
 
 	@Override
 	public void draw() {
-		parent.pushMatrix();
-		parent.translate(parent.getWidth() / 2f - 50, parent.getHeight() * GuiConstants.SIDE_BAR_HEIGHT + 5);
-		parent.stroke(1.0f);
-		parent.fill(GuiConstants.colorLightGrey);
-		parent.rect(0, 0, 100, 25, 3);
-		parent.translate(3, 20);
-		parent.fill(GuiConstants.colorBlack);
-		parent.textFont(GuiConstants.fonts[0]);
-		parent.textSize(GuiConstants.fontSizes[0]);		
-		parent.text("Aussetzen", 6, 0);
+		if(parent != null && parent.currentGameState != null && parent.currentGameState.getTurn() >= 8){
+			parent.pushMatrix();
+			int x = (int) (parent.getWidth() / 2f - 50);
+			int y = (int)(parent.getHeight() * GuiConstants.SIDE_BAR_HEIGHT + 5);
+			parent.translate(parent.getWidth() / 2f - 50, parent.getHeight() * GuiConstants.SIDE_BAR_HEIGHT + 5);
+			// draw stroke
+			if(parent.currentGameState.getCurrentPlayerColor() == PlayerColor.BLUE)
+				parent.stroke(GuiConstants.colorBlue);
+			else if(parent.currentGameState.getCurrentPlayerColor() == PlayerColor.RED)
+				parent.stroke(GuiConstants.colorRed);
+			else
+				parent.stroke(GuiConstants.colorBlack);
+			// fill rect
+			parent.fill(GuiConstants.colorLightGrey);
+			if(parent.getMousePosition() != null)
+				if(parent.mouseX >= x
+						&& parent.mouseX < x + 100 
+						&& parent.mouseY >= y 
+						&& parent.mouseY <y + 25)
+					parent.fill(GuiConstants.colorLightLightGrey);
+			parent.rect(0, 0, 100, 25, 3);
+			// draw text
+			parent.translate(3, 20);
+			parent.fill(GuiConstants.colorBlack);
+			
+			parent.textFont(GuiConstants.fonts[0]);
+			parent.textSize(GuiConstants.fontSizes[0]);		
+			parent.text("Aussetzen", 6, 0);
+			parent.popMatrix();
+		}
 		
 		
 		
-		
-		parent.popMatrix();
+
 	}
 
 }
