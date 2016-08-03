65b0b7461c5a0d1e7d3186f98e1d1de7660d0e01
Valentin <apexys@gmx.de>
2016-08-03 16:25:46 +0200

    Basic V8 Interface experiments

--- v8interface/src/v8interface/V8Interface.java
+++ v8interface/src/v8interface/V8Interface.java
@@ -1,5 +1,30 @@
 package v8interface;
 
-public class V8Interface {
+import java.util.concurrent.LinkedBlockingQueue;
 
+public class V8Interface implements Runnable{
+  LinkedBlockingQueue<String> buffer;
+
+  public V8Interface(){
+    buffer = new LinkedBlockingQueue<String>(1);
+  }
+
+  public String test(){
+    return "Hello World";
+  }
+
+  public String testAsync() throws InterruptedException{
+    Thread t = new Thread(this);
+    t.start();
+      return buffer.take();
+  }
+
+  public void run(){
+    try{
+      Thread.sleep(2000);
+      buffer.put("Hello Async World!");
+    }catch(InterruptedException ie){
+
+    }
+  }
 }
