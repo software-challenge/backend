82318068406a618d42babdfb99a8205a09f7701f
Valentin <apexys@gmx.de>
2016-08-04 17:52:16 +0200

    More work on the v8 interface

--- v8interface/src/v8interface/V8Interface.java
+++ v8interface/src/v8interface/V8Interface.java
@@ -1,13 +1,80 @@
 package v8interface;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
+
+
+import sc.server.Lobby;
+import sc.server.gaming.GameRoom;
+import sc.shared.SlotDescriptor;
+import sc.server.Configuration;
 
 public class V8Interface implements Runnable{
   LinkedBlockingQueue<V8Turn> currentTurn;
 
+  
+  LinkedBlockingQueue<String> log;
+  
+  ArrayList<SlotDescriptor> slots;
+
+  Lobby server;
+  
+  Integer port;
+  
+  public Lobby getServer() throws Exception{
+	  if(server != null){
+		  server.close();
+		  server = null;
+		  log.put("Stopped residual server");
+	  }
+	  server = createLobby(this.port);
+	  return server;
+  }
+  
+  private Lobby createLobby(Integer port) throws Exception{
+	  if(port != null){
+		  Configuration.set(Configuration.PORT_KEY,port.toString());
+		  log.put("Set Lobby port to " + port);
+	  }
+	  final Lobby server = new Lobby();
+	  server.start();
+	  return server;
+  }
+  
   public V8Interface(){
     currentTurn = new LinkedBlockingQueue<V8Turn>();
+    log  = new LinkedBlockingQueue<String>();
+    slots = new ArrayList<SlotDescriptor>();
   }
+  
+  public void setPort(Integer port){
+	  this.port = port;
+  }
+  
+  public void addClient(String displayName, boolean canTimeout, boolean shouldBePaused){
+	  slots.add(new SlotDescriptor(displayName, canTimeout, shouldBePaused));
+  }
+  
+  public GameRoom createGame(String type) throws Exception{
+	  GameRoom gr = server.getGameManager().createGame(type);
+	  gr.openSlots(slots);
+	  return gr;
+  }
+  
+  public String[] getReservations(GameRoom gr) throws InterruptedException{
+	  List<String> l = gr.reserveAllSlots();
+	  String[] keys = new String[2];
+	  keys[0] = l.get(0);
+	  keys[1] = l.get(1);
+	  log.put("Slot 1: " + keys[0]);
+	  log.put("Slot 2: " + keys[1]);
+	  return keys;
+  }
+  
+  
+  
+  /*
 
   public V8Game createGame(){
 
@@ -56,6 +123,14 @@ public class V8Interface implements Runnable{
 
   public void setMove(V8Move move){
 
+  }*/
+  
+  public void run(){
+	  
+  }
+  
+  public String getLogEntry() throws InterruptedException{
+	  return log.take();
   }
 
 }
