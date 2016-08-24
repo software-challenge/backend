de50e17238785c904ffe9a3379111e180690ac01
Sven Koschnicke <s.koschnicke@gfxpro.com>
2016-08-24 16:06:33 +0200

    more tests

--- game_plugins/mississippi_queen/test/sc/plugin2017/MoveTest.java
+++ game_plugins/mississippi_queen/test/sc/plugin2017/MoveTest.java
@@ -90,7 +90,7 @@ public class MoveTest {
     red.setX(-1);
     red.setY(-1);
     red.setDirection(0);
-    red.setMovement(1);
+    red.setSpeed(1);
 
     Move move = new Move();
     move.actions.add(new Acceleration(1, 0));
@@ -104,4 +104,95 @@ public class MoveTest {
     assertEquals(board.getField(-1, 0), blue.getField(board));
   }
 
+  @Test
+  public void pushIntoLog() throws InvalidMoveException {
+    // Red player pushes blue player into log field, blue player should lose one speed.
+    String tileString =
+        ".W.W.W.W...\n" +
+        "..r.b.L.W..\n" +
+        "...W.W.W.W.\n" +
+        "..W.W.W.W..\n" +
+        ".W.W.W.W...\n";
+    board.getTiles().set(0, TextTileHelper.parseTile(tileString, -2, -2));
+    blue.setX(0);
+    blue.setY(-1);
+    blue.setDirection(1);
+    blue.setMovement(3);
+    red.setX(-1);
+    red.setY(-1);
+    red.setDirection(0);
+    red.setMovement(1);
+
+    Move move = new Move();
+    move.actions.add(new Acceleration(1, 0));
+    move.actions.add(new Step(1, 1));
+    move.actions.add(new Push(0, 2));
+
+    move.perform(state, red);
+    // blue player should be pushed to 1,-1
+    assertEquals(board.getField(1, -1), blue.getField(board));
+    assertEquals(2, blue.getMovement());
+  }
+
+  @Test
+  public void pushIntoSandbank() throws InvalidMoveException {
+    // Red player pushes blue player into sandbank field, blue player should lose all speed.
+    String tileString =
+        ".W.W.W.W...\n" +
+        "..r.b.S.W..\n" +
+        "...W.W.W.W.\n" +
+        "..W.W.W.W..\n" +
+        ".W.W.W.W...\n";
+    board.getTiles().set(0, TextTileHelper.parseTile(tileString, -2, -2));
+    blue.setX(0);
+    blue.setY(-1);
+    blue.setDirection(1);
+    blue.setMovement(3);
+    red.setX(-1);
+    red.setY(-1);
+    red.setDirection(0);
+    red.setMovement(1);
+
+    Move move = new Move();
+    move.actions.add(new Acceleration(1, 0));
+    move.actions.add(new Step(1, 1));
+    move.actions.add(new Push(0, 2));
+
+    move.perform(state, red);
+    // blue player should be pushed to 1,-1
+    assertEquals(board.getField(1, -1), blue.getField(board));
+    assertEquals(1, blue.getMovement());
+  }
+
+  @Test
+  public void additionalTurnsCostCoal() throws InvalidMoveException {
+    // Red player turns three times and should lose two coal.
+    String tileString =
+        ".W.W.W.W...\n" +
+        "..r.W.W.W..\n" +
+        "...W.W.W.W.\n" +
+        "..b.W.W.W..\n" +
+        ".W.W.W.W...\n";
+    board.getTiles().set(0, TextTileHelper.parseTile(tileString, -2, -2));
+    blue.setX(-1);
+    blue.setY(1);
+    red.setX(-1);
+    red.setY(-1);
+    red.setDirection(0);
+    red.setSpeed(2);
+    red.setCoal(6);
+
+    Move move = new Move();
+    move.actions.add(new Step(1, 0));
+    move.actions.add(new Turn(-1, 1));
+    move.actions.add(new Turn(-1, 2));
+    move.actions.add(new Step(1, 3));
+    move.actions.add(new Turn(1, 4));
+
+    move.perform(state, red);
+    // red player should arrive at -1,0
+    assertEquals(board.getField(-1, 0), red.getField(board));
+    assertEquals(5, red.getDirection());
+    assertEquals(4, red.getCoal());
+  }
 }
