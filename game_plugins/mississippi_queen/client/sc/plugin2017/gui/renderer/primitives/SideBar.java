176116e63cd0eaa8ac94913816356f7460e9e3a4
Sven Koschnicke <s.koschnicke@gfxpro.com>
2016-09-12 15:59:15 +0200

    optimizing memory usage

--- game_plugins/mississippi_queen/client/sc/plugin2017/gui/renderer/primitives/SideBar.java
+++ game_plugins/mississippi_queen/client/sc/plugin2017/gui/renderer/primitives/SideBar.java
@@ -12,7 +12,6 @@ import sc.plugin2017.gui.renderer.RenderConfiguration;
  * Zeichnet Spielerinformationen (Punkte) aktueller Zug
  * des Spieles.
  */
-// TODO find out why dispalyName == null
 public class SideBar extends PrimitiveBase {
 
   private PlayerColor currentColor;
@@ -20,6 +19,11 @@ public class SideBar extends PrimitiveBase {
   private int redPoints = GuiConstants.DEFAULT_RED_POINTS;
   private String blueName = GuiConstants.DEFAULT_BLUE_NAME;
   private int bluePoints = GuiConstants.DEFAULT_BLUE_POINTS;
+
+  private float width;
+  private float height;
+  private float baseFontSize;
+  private String referenceString = "Dieser Text sollte passen.";
 
   public SideBar(FrameRenderer parent) {
     super(parent);
@@ -32,10 +36,26 @@ public class SideBar extends PrimitiveBase {
     this.redPoints = redPoints;
     this.blueName = blueName;
     this.bluePoints = bluePoints;
+
+    // calculate maximum font size to fit longest player name, or, if that is
+    // shorter than the reference string, fit the reference string into sidebar
+    float maxTextWidth = width * 0.9f;
+    String longestPlayerName;
+    if (redName.length() > blueName.length()) {
+      longestPlayerName = redName;
+    } else {
+      longestPlayerName = blueName;
+    }
+    if (longestPlayerName.length() > referenceString.length()) {
+      referenceString = longestPlayerName;
+    }
+    parent.textSize(10);
+    baseFontSize = maxTextWidth / parent.textWidth(referenceString) * 10;
   }
 
   public void update(PlayerColor currentColor) {
     update(currentColor, GuiConstants.DEFAULT_RED_NAME, GuiConstants.DEFAULT_RED_POINTS, GuiConstants.DEFAULT_BLUE_NAME, GuiConstants.DEFAULT_BLUE_POINTS);
+
   }
 
   @Override
@@ -46,27 +66,12 @@ public class SideBar extends PrimitiveBase {
     parent.fill(GuiConstants.colorSideBarBG);
 
     parent.pushMatrix();
-    float width = parent.getWidth() * GuiConstants.SIDE_BAR_WIDTH;
-    float height = parent.getHeight() * GuiConstants.SIDE_BAR_HEIGHT;
+    width = parent.getWidth() * GuiConstants.SIDE_BAR_WIDTH;
+    height = parent.getHeight() * GuiConstants.SIDE_BAR_HEIGHT;
     parent.translate(parent.getWidth() * GuiConstants.SIDE_BAR_START_X,
         GuiConstants.SIDE_BAR_START_Y);
     parent.rect(0, 0, width, height);
 
-    float maxTextWidth = width * 0.9f;
-    // calculate maximum font size to fit longest player name, or, if that is
-    // shorter than the reference string, fit the reference string into sidebar
-    String longestPlayerName;
-    if (redName.length() > blueName.length()) {
-      longestPlayerName = redName;
-    } else {
-      longestPlayerName = blueName;
-    }
-    String referenceString = "Dieser Text sollte passen.";
-    if (longestPlayerName.length() > referenceString.length()) {
-      referenceString = longestPlayerName;
-    }
-    parent.textSize(10);
-    float baseFontSize = maxTextWidth / parent.textWidth(referenceString) * 10;
     parent.textSize(baseFontSize);
 
     // Text
