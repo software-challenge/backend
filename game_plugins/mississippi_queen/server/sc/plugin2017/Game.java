fa29cfbc079cb788651989c0875aa4e71d602482
soed <soed@kabelmail.de>
2017-01-18 15:09:35 +0100

    auto reformat

--- game_plugins/mississippi_queen/server/sc/plugin2017/Game.java
+++ game_plugins/mississippi_queen/server/sc/plugin2017/Game.java
@@ -31,220 +31,226 @@ import sc.shared.ScoreCause;
  */
 @XStreamAlias(value = "game")
 public class Game extends RoundBasedGameInstance<Player> {
-	private static Logger logger = LoggerFactory.getLogger(Game.class);
+  private static Logger logger = LoggerFactory.getLogger(Game.class);
 
-	@XStreamOmitField
-	private List<PlayerColor> availableColors = new LinkedList<>();
+  @XStreamOmitField
+  private List<PlayerColor> availableColors = new LinkedList<>();
 
-	@XStreamOmitField
-	private int halfturns = 0;
+  @XStreamOmitField
+  private int halfturns = 0;
 
-	private GameState gameState = new GameState();
+  private GameState gameState = new GameState();
 
-	public GameState getGameState() {
-		return this.gameState.getVisible();
-	}
+  public GameState getGameState() {
+    return this.gameState.getVisible();
+  }
 
-	public Player getActivePlayer() {
-		return this.activePlayer;
-	}
+  public Player getActivePlayer() {
+    return this.activePlayer;
+  }
 
-	public Game() {
-		this.availableColors.add(PlayerColor.RED);
-		this.availableColors.add(PlayerColor.BLUE);
-	}
+  public Game() {
+    this.availableColors.add(PlayerColor.RED);
+    this.availableColors.add(PlayerColor.BLUE);
+  }
 
-	@Override
-	protected Object getCurrentState() {
-		return this.gameState.getVisible(); // return only the for the players visible board
-	}
+  @Override
+  protected Object getCurrentState() {
+    return this.gameState.getVisible(); // return only the for the players
+                                        // visible board
+  }
 
-	/**
-	 * Someone did something, check out what it was (move maybe? Then check the
-	 * move)
-	 */
-	@Override
-	protected void onRoundBasedAction(IPlayer fromPlayer, Object data)
-			throws GameLogicException {
+  /**
+   * Someone did something, check out what it was (move maybe? Then check the
+   * move)
+   */
+  @Override
+  protected void onRoundBasedAction(IPlayer fromPlayer, Object data) throws GameLogicException {
 
-		Player author = (Player) fromPlayer;
+    Player author = (Player) fromPlayer;
 
-		/**
-		 *  NOTE: Checking if right player sent move was already done by
-		 *  {@link sc.framework.plugins.RoundBasedGameInstance#onAction(IPlayer, Object)}.
-		 *  There is no need to do it here again.
-		 */
-		try {
-			if (!(data instanceof Move)) {
-				throw new InvalidMoveException(author.getDisplayName()
-						+ " hat kein Zug-Objekt gesendet.");
-			}
+    /**
+     * NOTE: Checking if right player sent move was already done by
+     * {@link sc.framework.plugins.RoundBasedGameInstance#onAction(IPlayer, Object)}.
+     * There is no need to do it here again.
+     */
+    try {
+      if (!(data instanceof Move)) {
+        throw new InvalidMoveException(author.getDisplayName() + " hat kein Zug-Objekt gesendet.");
+      }
 
       final Move move = (Move) data;
       move.perform(this.gameState, author);
       this.gameState.prepareNextTurn(move);
       this.halfturns++;
-			next(this.gameState.getCurrentPlayer());
-		} catch (InvalidMoveException e) {
-			author.setViolated(true);
-			String err = "Ungueltiger Zug von '" + author.getDisplayName()
-					+ "'.\n" + e.getMessage();
-			author.setViolationReason(e.getMessage());
-			logger.error(err, e);
-			throw new GameLogicException(err);
-		}
-	}
+      next(this.gameState.getCurrentPlayer());
+    } catch (InvalidMoveException e) {
+      author.setViolated(true);
+      String err = "Ungueltiger Zug von '" + author.getDisplayName() + "'.\n" + e.getMessage();
+      author.setViolationReason(e.getMessage());
+      logger.error(err, e);
+      throw new GameLogicException(err);
+    }
+  }
 
-	/**
-	 * In this game, a new turn begins when both players made one move. The order
-	 * in which the players make their move may change.
-	 */
-	@Override
-	protected boolean increaseTurnIfNecessary(Player nextPlayer) {
-	  return getGameState().getTurn() % 2 == 0;
-	}
+  /**
+   * In this game, a new turn begins when both players made one move. The order
+   * in which the players make their move may change.
+   */
+  @Override
+  protected boolean increaseTurnIfNecessary(Player nextPlayer) {
+    return getGameState().getTurn() % 2 == 0;
+  }
 
   @Override
-	public IPlayer onPlayerJoined() throws TooManyPlayersException {
-		if (this.players.size() >= GamePlugin.MAX_PLAYER_COUNT)
-			throw new TooManyPlayersException();
-		final Player player;
-		// When starting a game from a imported state the players should not be overwritten
-		PlayerColor playerColor =  this.availableColors.remove(0);
-	  if (PlayerColor.RED == playerColor && gameState.getRedPlayer() != null) {
-	    player = gameState.getRedPlayer();
-	  } else if (PlayerColor.BLUE == playerColor && gameState.getBluePlayer() != null) {
-	    player = gameState.getBluePlayer();
-	  } else {
-	    player = new Player(playerColor);
-	  }
-		
-		this.players.add(player);
-		this.gameState.addPlayer(player);
+  public IPlayer onPlayerJoined() throws TooManyPlayersException {
+    if (this.players.size() >= GamePlugin.MAX_PLAYER_COUNT)
+      throw new TooManyPlayersException();
+    final Player player;
+    // When starting a game from a imported state the players should not be
+    // overwritten
+    PlayerColor playerColor = this.availableColors.remove(0);
+    if (PlayerColor.RED == playerColor && gameState.getRedPlayer() != null) {
+      player = gameState.getRedPlayer();
+    } else if (PlayerColor.BLUE == playerColor && gameState.getBluePlayer() != null) {
+      player = gameState.getBluePlayer();
+    } else {
+      player = new Player(playerColor);
+    }
 
-		return player;
-	}
+    this.players.add(player);
+    this.gameState.addPlayer(player);
 
-	@Override
-	public void onPlayerLeft(IPlayer player) {
-		if (!player.hasViolated()) {
-			onPlayerLeft(player, ScoreCause.LEFT);
-		} else {
-			onPlayerLeft(player, ScoreCause.RULE_VIOLATION);
-		}
-	}
+    return player;
+  }
 
-	@Override
-	public void onPlayerLeft(IPlayer player, ScoreCause cause) {
-		Map<IPlayer, PlayerScore> res = generateScoreMap();
+  @Override
+  public void onPlayerLeft(IPlayer player) {
+    if (!player.hasViolated()) {
+      onPlayerLeft(player, ScoreCause.LEFT);
+    } else {
+      onPlayerLeft(player, ScoreCause.RULE_VIOLATION);
+    }
+  }
 
-		for (Entry<IPlayer, PlayerScore> entry : res.entrySet()) {
-			PlayerScore score = entry.getValue();
+  @Override
+  public void onPlayerLeft(IPlayer player, ScoreCause cause) {
+    Map<IPlayer, PlayerScore> res = generateScoreMap();
 
-			if (entry.getKey() == player) {
-				score.setCause(cause);
-			}
-			
-			// TODO 
-//			if (entry.getKey() == player) {
-//        score.setCause(cause);
-//        score.setValueAt(0, new BigDecimal(0));
-//      } else {
-//        score.setValueAt(0, new BigDecimal(2));
-//      }
-		}
+    for (Entry<IPlayer, PlayerScore> entry : res.entrySet()) {
+      PlayerScore score = entry.getValue();
 
-		notifyOnGameOver(res);
-	}
+      if (entry.getKey() == player) {
+        score.setCause(cause);
+      }
 
-	@Override
-	public boolean ready() {
-		return this.players.size() == GamePlugin.MAX_PLAYER_COUNT;
-	}
+      // TODO
+      // if (entry.getKey() == player) {
+      // score.setCause(cause);
+      // score.setValueAt(0, new BigDecimal(0));
+      // } else {
+      // score.setValueAt(0, new BigDecimal(2));
+      // }
+    }
 
-	@Override
-	public void start() {
-		for (final Player p : this.players) {
-			p.notifyListeners(new WelcomeMessage(p.getPlayerColor()));
-		}
+    notifyOnGameOver(res);
+  }
 
-		super.start();
-	}
+  @Override
+  public boolean ready() {
+    return this.players.size() == GamePlugin.MAX_PLAYER_COUNT;
+  }
 
-	@Override
-	protected void onNewTurn() {
+  @Override
+  public void start() {
+    for (final Player p : this.players) {
+      p.notifyListeners(new WelcomeMessage(p.getPlayerColor()));
+    }
 
-	}
+    super.start();
+  }
 
-	@Override
-	protected PlayerScore getScoreFor(Player p) {
+  @Override
+  protected void onNewTurn() {
 
-	  logger.debug("get score for player {}", p.getPlayerColor());
-	  logger.debug("player violated: {}", p.hasViolated());
-		int[] stats = this.gameState.getPlayerStats(p);
-		int matchPoints = 1;
-		int[] oppPoints = this.gameState.getPlayerStats(p.getPlayerColor()
-				.opponent());
-		WinCondition winCondition = checkWinCondition();
-		String winningReason = null;
-		if (winCondition != null) {
-		  winningReason = winCondition.getReason();
-		}
-		if (stats[Constants.GAME_STATS_POINTS_INDEX] > oppPoints[Constants.GAME_STATS_POINTS_INDEX] 
-		    || (stats[Constants.GAME_STATS_POINTS_INDEX] == oppPoints[Constants.GAME_STATS_POINTS_INDEX] 
-		        && stats[Constants.GAME_STATS_PASSENGER_INDEX] > oppPoints[Constants.GAME_STATS_PASSENGER_INDEX]))
-			matchPoints = 2;
-		else if (stats[Constants.GAME_STATS_POINTS_INDEX] < oppPoints[Constants.GAME_STATS_POINTS_INDEX] 
-		    || (stats[Constants.GAME_STATS_POINTS_INDEX] == oppPoints[Constants.GAME_STATS_POINTS_INDEX] 
-		        && stats[Constants.GAME_STATS_PASSENGER_INDEX] < oppPoints[Constants.GAME_STATS_PASSENGER_INDEX]))
-			matchPoints = 0;
-		// FIXME score calculation is done at too many places and does not respect score definition but assumes a fixed schema (points and matchpoints).
-		Player opponent = p.getPlayerColor().opponent() == PlayerColor.BLUE ? this.gameState.getBluePlayer() : this.gameState.getRedPlayer();
-		if (opponent.hasViolated() && !p.hasViolated()) {
-		  matchPoints = 2;
-		}
-		return p.hasViolated() ? new PlayerScore(ScoreCause.RULE_VIOLATION, p.getViolationReason(), 0,
-				stats[Constants.GAME_STATS_POINTS_INDEX], stats[Constants.GAME_STATS_PASSENGER_INDEX]) : new PlayerScore(ScoreCause.REGULAR, winningReason,
-				matchPoints, stats[Constants.GAME_STATS_POINTS_INDEX], stats[Constants.GAME_STATS_PASSENGER_INDEX]);
+  }
 
-	}
+  @Override
+  protected PlayerScore getScoreFor(Player p) {
 
-	@Override
-	protected ActionTimeout getTimeoutFor(Player player) {
-		return new ActionTimeout(true, 10000l, 2000l);
-	}
+    logger.debug("get score for player {}", p.getPlayerColor());
+    logger.debug("player violated: {}", p.hasViolated());
+    int[] stats = this.gameState.getPlayerStats(p);
+    int matchPoints = 1;
+    int[] oppPoints = this.gameState.getPlayerStats(p.getPlayerColor().opponent());
+    WinCondition winCondition = checkWinCondition();
+    String winningReason = null;
+    if (winCondition != null) {
+      winningReason = winCondition.getReason();
+    }
+    if (stats[Constants.GAME_STATS_POINTS_INDEX] > oppPoints[Constants.GAME_STATS_POINTS_INDEX]
+        || (stats[Constants.GAME_STATS_POINTS_INDEX] == oppPoints[Constants.GAME_STATS_POINTS_INDEX]
+            && stats[Constants.GAME_STATS_PASSENGER_INDEX] > oppPoints[Constants.GAME_STATS_PASSENGER_INDEX]))
+      matchPoints = 2;
+    else if (stats[Constants.GAME_STATS_POINTS_INDEX] < oppPoints[Constants.GAME_STATS_POINTS_INDEX]
+        || (stats[Constants.GAME_STATS_POINTS_INDEX] == oppPoints[Constants.GAME_STATS_POINTS_INDEX]
+            && stats[Constants.GAME_STATS_PASSENGER_INDEX] < oppPoints[Constants.GAME_STATS_PASSENGER_INDEX]))
+      matchPoints = 0;
+    // FIXME score calculation is done at too many places and does not respect
+    // score definition but assumes a fixed schema (points and matchpoints).
+    Player opponent = p.getPlayerColor().opponent() == PlayerColor.BLUE ? this.gameState.getBluePlayer()
+        : this.gameState.getRedPlayer();
+    if (opponent.hasViolated() && !p.hasViolated()) {
+      matchPoints = 2;
+    }
+    return p.hasViolated()
+        ? new PlayerScore(ScoreCause.RULE_VIOLATION, p.getViolationReason(), 0,
+            stats[Constants.GAME_STATS_POINTS_INDEX], stats[Constants.GAME_STATS_PASSENGER_INDEX])
+        : new PlayerScore(ScoreCause.REGULAR, winningReason, matchPoints, stats[Constants.GAME_STATS_POINTS_INDEX],
+            stats[Constants.GAME_STATS_PASSENGER_INDEX]);
 
-	/**
-	 * checks if one player reached the goal with enough passengers
-	 * @return the player who reached the goal or null if no player reached the goal
-	 */
-	private Player checkGoalReached() {
-	  for (final Player player : this.players) {
-      if (player.getField(this.gameState.getBoard()).getType() == FieldType.GOAL && player.getPassenger() >= 2 && player.getSpeed() == 1) {
+  }
+
+  @Override
+  protected ActionTimeout getTimeoutFor(Player player) {
+    return new ActionTimeout(true, 10000l, 2000l);
+  }
+
+  /**
+   * checks if one player reached the goal with enough passengers
+   * 
+   * @return the player who reached the goal or null if no player reached the
+   *         goal
+   */
+  private Player checkGoalReached() {
+    for (final Player player : this.players) {
+      if (player.getField(this.gameState.getBoard()).getType() == FieldType.GOAL && player.getPassenger() >= 2
+          && player.getSpeed() == 1) {
         return player;
       }
-	  }
-	  return null;
-	}
+    }
+    return null;
+  }
 
-	/**
-	 * Checks if a win condition in the current game state is met.
-	 * @return WinCondition with winner and reason or null, if no win condition is yet met.
-	 */
-	public WinCondition checkWinCondition() {
-	  if (this.gameState.getTurn() > 1) {
-	    // XXX only for test
-      //return new WinCondition(PlayerColor.BLUE, "Das Rundenlimit von 2 wurde erreicht.");
-	  }
+  /**
+   * Checks if a win condition in the current game state is met.
+   * 
+   * @return WinCondition with winner and reason or null, if no win condition is
+   *         yet met.
+   */
+  public WinCondition checkWinCondition() {
+    if (this.gameState.getTurn() > 1) {
+      // XXX only for test
+      // return new WinCondition(PlayerColor.BLUE, "Das Rundenlimit von 2 wurde
+      // erreicht.");
+    }
     int[][] stats = this.gameState.getGameStats();
     if (this.gameState.getTurn() >= 2 * Constants.ROUND_LIMIT) {
       // round limit reached
       PlayerColor winner = null;
-      if (stats[Constants.GAME_STATS_RED_INDEX][Constants.GAME_STATS_POINTS_INDEX] 
-          > stats[Constants.GAME_STATS_BLUE_INDEX][Constants.GAME_STATS_POINTS_INDEX]) {
+      if (stats[Constants.GAME_STATS_RED_INDEX][Constants.GAME_STATS_POINTS_INDEX] > stats[Constants.GAME_STATS_BLUE_INDEX][Constants.GAME_STATS_POINTS_INDEX]) {
         winner = PlayerColor.RED;
-      } else if (stats[Constants.GAME_STATS_RED_INDEX][Constants.GAME_STATS_POINTS_INDEX] 
-          < stats[Constants.GAME_STATS_BLUE_INDEX][Constants.GAME_STATS_POINTS_INDEX]) {
+      } else if (stats[Constants.GAME_STATS_RED_INDEX][Constants.GAME_STATS_POINTS_INDEX] < stats[Constants.GAME_STATS_BLUE_INDEX][Constants.GAME_STATS_POINTS_INDEX]) {
         winner = PlayerColor.BLUE;
       }
       return new WinCondition(winner, "Das Rundenlimit wurde erreicht.");
@@ -252,11 +258,11 @@ public class Game extends RoundBasedGameInstance<Player> {
       // one player reached the goal
       PlayerColor winner = checkGoalReached().getPlayerColor();
       return new WinCondition(winner, "Das Spiel ist beendet.\nEin Spieler ist im Ziel");
-    } else if (getActivePlayer() != this.gameState.getStartPlayer() &&
-        Math.abs(this.gameState.getRedPlayer().getTile() - this.gameState.getBluePlayer().getTile()) > 3) {
+    } else if (getActivePlayer() != this.gameState.getStartPlayer()
+        && Math.abs(this.gameState.getRedPlayer().getTile() - this.gameState.getBluePlayer().getTile()) > 3) {
       // a player is more than three tiles before the other player
       PlayerColor winner;
-      if(this.gameState.getRedPlayer().getTile() > this.gameState.getBluePlayer().getTile()) {
+      if (this.gameState.getRedPlayer().getTile() > this.gameState.getBluePlayer().getTile()) {
         winner = PlayerColor.RED;
       } else {
         winner = PlayerColor.BLUE;
@@ -264,32 +270,34 @@ public class Game extends RoundBasedGameInstance<Player> {
       return new WinCondition(winner, "Das Spiel ist vorzeitig zu Ende.\nEin Spieler wurde abgehängt.");
     }
     return null;
-	}
+  }
 
-	@Override
-	protected boolean checkGameOverCondition() {
-	  return checkWinCondition() != null;
-	}
+  @Override
+  protected boolean checkGameOverCondition() {
+    return checkWinCondition() != null;
+  }
 
-	@Override
-	public void loadFromFile(String file) {
-		GameLoader gl = new GameLoader(new Class<?>[] { GameState.class });
-		Object gameInfo = gl.loadGame(Configuration.getXStream(), file);
-		if (gameInfo != null) {
-			loadGameInfo(gameInfo);
-		}
-	}
+  @Override
+  public void loadFromFile(String file) {
+    GameLoader gl = new GameLoader(new Class<?>[] { GameState.class });
+    Object gameInfo = gl.loadGame(Configuration.getXStream(), file);
+    if (gameInfo != null) {
+      loadGameInfo(gameInfo);
+    }
+  }
 
-	@Override
-	public void loadGameInfo(Object gameInfo) {
-		if (gameInfo instanceof GameState) {
-			this.gameState = (GameState) gameInfo;
-			// when loading from a state the listeners are not initialized
-			this.gameState.getRedPlayer().initListeners();
+  @Override
+  public void loadGameInfo(Object gameInfo) {
+    if (gameInfo instanceof GameState) {
+      this.gameState = (GameState) gameInfo;
+      // when loading from a state the listeners are not initialized
+      this.gameState.getRedPlayer().initListeners();
       this.gameState.getBluePlayer().initListeners();
-      // the currentPlayer has to be RED (else the Move request is send to the wrong player)
-      // if it isn't red, the players have to be switched and red is made currentPlayer
-      if(this.gameState.getCurrentPlayerColor() != PlayerColor.RED) {
+      // the currentPlayer has to be RED (else the Move request is send to the
+      // wrong player)
+      // if it isn't red, the players have to be switched and red is made
+      // currentPlayer
+      if (this.gameState.getCurrentPlayerColor() != PlayerColor.RED) {
         this.gameState.setCurrentPlayer(PlayerColor.RED);
         try {
           Player newRed = (Player) this.gameState.getBluePlayer().clone();
@@ -303,32 +311,33 @@ public class Game extends RoundBasedGameInstance<Player> {
           e.printStackTrace();
         }
       }
-		}
-	}
+    }
+  }
 
-	@Override
-	public List<IPlayer> getWinners() {
-	  WinCondition win = checkWinCondition();
-			List<IPlayer> winners = new LinkedList<IPlayer>();
-	  if (win != null) {
+  @Override
+  public List<IPlayer> getWinners() {
+    WinCondition win = checkWinCondition();
+    List<IPlayer> winners = new LinkedList<IPlayer>();
+    if (win != null) {
       for (Player player : this.players) {
         if (player.getPlayerColor() == win.getWinner()) {
           winners.add(player);
           break;
         }
       }
-	  } else {
-	    // No win condition met, player with highest score wins. Winning score is
-	    // determined by matchpoints ("Siegpunkte"). The winning player has 2
-	    // matchpoints. Find this player. If no player has 2 matchpoints, it is a draw.
+    } else {
+      // No win condition met, player with highest score wins. Winning score is
+      // determined by matchpoints ("Siegpunkte"). The winning player has 2
+      // matchpoints. Find this player. If no player has 2 matchpoints, it is a
+      // draw.
       for (Player player : this.players) {
         if (getScoreFor(player).getValues().get(0).intValueExact() == 2) {
           winners.add(player);
           break;
         }
       }
-	  }
-		return winners;
-	}
+    }
+    return winners;
+  }
 
 }
