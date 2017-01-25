769ec24e0870b0124cc62aa7efa21bec3dda7eda
Sven Koschnicke <s.koschnicke@gfxpro.com>
2017-01-25 14:07:27 +0100

    added comments to make ordering of actions clearer

--- game_plugins/mississippi_queen/shared/sc/plugin2017/Action.java
+++ game_plugins/mississippi_queen/shared/sc/plugin2017/Action.java
@@ -9,7 +9,8 @@ import sc.plugin2017.util.InvalidMoveException;
 public abstract class Action implements Comparable<Action> {
 
   /**
-   * Zeigt an welche Nummer die Aktion hat
+   * Zeigt an welche Nummer die Aktion hat. Die Aktion eines Zuges mit der
+   * niedrigsten Nummer wird als erstes ausgeführt.
    */
   @XStreamAsAttribute
   public int order;
