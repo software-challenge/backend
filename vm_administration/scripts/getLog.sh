dd4912a33c6cf8129ec53f47548c88899e659cd2
Sven Koschnicke <s.koschnicke@gfxpro.com>
2017-03-01 10:10:48 +0100

    fixed key verification for getLog script

--- vm_administration/scripts/getLog.sh
+++ vm_administration/scripts/getLog.sh
@@ -13,7 +13,8 @@ echo "Copying log file" >> $log
 echo "Max file size: ${MAX_FILE_SIZE_BYTES}" >> $log
 echo "IP: $1" >> $log
 echo "VMName: $2" >> $log
-ssh -l scadmin -i "$HOME/.ssh/client_key" $1 "head -c ${MAX_FILE_SIZE_BYTES} ~/logs/exec.log" > ~/clientlogs/$2.log
+SSH_OPTIONS="-q -o StrictHostKeyChecking=no -o BatchMode=true -o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -i $HOME/.ssh/client_key"
+ssh -l scadmin $SSH_OPTIONS $1 "head -c ${MAX_FILE_SIZE_BYTES} ~/logs/exec.log" > ~/clientlogs/$2.log
 
 #client_id=`cat ~/clientlogs/$2.log | grep '192.168.56.2:/home/scadmin/tmp/' | sed 's/.*192\.168\.56\.2:\/home\/scadmin\/tmp\/[0-9]*_[0-9]*_.*_\([0-9]*\)\.zip.*/\1/;q'`
 client_id=`grep "Client ID: " clientlogs/$2.log | sed 's/Client ID: \([0-9]*\)/\1/;q'`
