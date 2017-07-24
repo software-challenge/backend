0d399d73b208f5640e540fe541798dffe48b7d5d
soed <soed@kabelmail.de>
2017-07-24 14:18:33 +0200

    welcome message fix

--- game_plugins/hase_und_igel_new/shared/sc/plugin2018/WelcomeMessage.java
+++ game_plugins/hase_und_igel_new/shared/sc/plugin2018/WelcomeMessage.java
@@ -1,20 +1,31 @@
 package sc.plugin2018;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import sc.shared.PlayerColor;
 
-@XStreamAlias(value="welcomeMessage")
-public class WelcomeMessage
-{
-	private PlayerColor color;
-	
-	public WelcomeMessage(PlayerColor color)
-	{
-		this.color = color;
+/**
+ * Nachricht, die zu Beginn eines Spiels an einen Client geschickt wird, um ihm
+ * seine Spielerfarbe mitzuteilen
+ *
+ */
+@XStreamAlias(value = "welcomeMessage")
+public class WelcomeMessage {
+
+	@XStreamAsAttribute
+	private String color;
+
+	/**
+	 * might be needed by XStream
+	 */
+	public WelcomeMessage() {
 	}
-	
-	public PlayerColor getPlayerColor()
-	{
-		return color;
+
+	public WelcomeMessage(PlayerColor c) {
+		color = c.toString().toLowerCase();
+	}
+
+	public PlayerColor getPlayerColor() {
+		return PlayerColor.valueOf(color.toUpperCase());
 	}
 }
