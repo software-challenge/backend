3821fde8d4605c36da55fd2f5ffc9a77acc7ecad
Xerus <27jf@web.de>
2018-05-02 21:42:58 +0200

    Improve AbstractClient

--- game_plugins/hase_und_igel_2018/client/sc/plugin2018/AbstractClient.java
+++ game_plugins/hase_und_igel_2018/client/sc/plugin2018/AbstractClient.java
@@ -1,6 +1,7 @@
 package sc.plugin2018;
 
 import java.io.IOException;
+import java.net.ConnectException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,174 +19,170 @@ import sc.shared.PlayerColor;
 import sc.shared.WelcomeMessage;
 
 /**
- * Abstrakter Client nach Vorschrift des SDKs. Beinhaltet einen LobbyClient, der
- * den tatsächlichen Client darstellt.
-
+ * Abstrakter Client nach Vorschrift des SDK
+ * Beinhaltet einen LobbyClient, der den tatsächlichen Client darstellt.
  */
 public abstract class AbstractClient implements ILobbyClientListener {
-  private static final Logger logger = LoggerFactory
-      .getLogger(AbstractClient.class);
-	// The handler reacts to messages from the server received by the lobby
-	// client
-	protected IGameHandler handler;
 
-	// The lobby client, that connects to the room
-	private LobbyClient client;
+  private static final Logger logger = LoggerFactory.getLogger(AbstractClient.class);
 
-	private String gameType;
+  /** The handler reacts to messages from the server received by the lobby client */
+  protected IGameHandler handler;
 
-	// If the client made an error (rule violation), store reason here
-	private String error;
+  /** The lobby client, that connects to the room */
+  private LobbyClient client;
 
-	// current id to identify the client instance internal
-	private EPlayerId id;
-	// the current room in which the player is
-	private String roomId;
-	// the current host
-	private String host;
-	// the current port
-	private int port;
-	// current figurecolor to identify which client belongs to which player
-	private PlayerColor color;
+  private String gameType;
 
-	public AbstractClient(String host, int port, EPlayerId id) throws IOException {
-		this.gameType = GamePlugin.PLUGIN_UUID;
-		this.client = new LobbyClient(Configuration.getXStream(), Configuration.getClassesToRegister(), host, port);
-		this.client.addListener(this);
-		this.client.start();
-		this.id = id;
-		this.port = port;
-		this.host = host;
-		this.error = null;
-	}
+  /** If the client made an error (rule violation), store reason here */
+  private String error;
 
-	// wenn es nur einen client gibt
-	public AbstractClient(String host, int port) throws IOException {
-		this(host, port, EPlayerId.PLAYER_ONE);
-	}
+  /** current id to identify the client instance internal */
+  private EPlayerId id;
+  /** the current room in which the player is */
+  private String roomId;
+  /** the current host */
+  private String host;
+  /** the current port */
+  private int port;
+  /** current figurecolor to identify which client belongs to which player */
+  private PlayerColor color;
 
-	public void setHandler(IGameHandler handler) {
-		this.handler = handler;
-	}
+  public AbstractClient(String host, int port, EPlayerId id) throws IOException {
+    this.gameType = GamePlugin.PLUGIN_UUID;
+    try {
+      this.client = new LobbyClient(Configuration.getXStream(), Configuration.getClassesToRegister(), host, port);
+    } catch (ConnectException e) {
+      logger.error("Could not connect to Server: " + e.getMessage());
+      System.exit(1);
+    }
+    this.client.addListener(this);
+    this.client.start();
+    this.id = id;
+    this.port = port;
+    this.host = host;
+    this.error = null;
+  }
 
-	/**
-	 * Tell this client to observe the game given by the preparation handler
-	 *
-	 * @param handle Handle
-	 * @return controllable game
-	 */
-	public IControllableGame observeGame(PrepareGameProtocolMessage handle) {
-		return this.client.observe(handle);
-	}
+  /** wenn es nur einen client gibt */
+  public AbstractClient(String host, int port) throws IOException {
+    this(host, port, EPlayerId.PLAYER_ONE);
+  }
 
-	/**
-	 * Called when a new message is sent to the room, e.g. move requests
-	 */
-	@Override
-	public void onRoomMessage(String roomId, Object data) {
-		if (data instanceof MoveRequest) {
-			this.handler.onRequestAction();
-		} else if (data instanceof WelcomeMessage) {
-			WelcomeMessage welc = (WelcomeMessage) data;
-			this.color = welc.getPlayerColor();
-		}
-		this.roomId = roomId;
-	}
+  public void setHandler(IGameHandler handler) {
+    this.handler = handler;
+  }
 
-	/**
-	 * sends the <code>move</code> to the server
-	 *
-	 * @param move
-	 *            the move you want to do
-	 */
-	public void sendMove(Move move) {
-		this.client.sendMessageToRoom(this.roomId, move);
-	}
+  /**
+   * Tell this client to observe the game given by the preparation handler
+   *
+   * @param handle Handle
+   *
+   * @return controllable game
+   */
+  public IControllableGame observeGame(PrepareGameProtocolMessage handle) {
+    return this.client.observe(handle);
+  }
 
-	/**
-	 * Called, when an error is sent to the room
-	 */
-	@Override
-	public void onError(String roomId, ProtocolErrorMessage response) {
-	  logger.debug("onError: Client {} received error {}", this, response.getMessage());
-		this.error = response.getMessage();
-	}
+  /**
+   * Called when a new message is sent to the room, e.g. move requests
+   */
+  @Override
+  public void onRoomMessage(String roomId, Object data) {
+    if (data instanceof MoveRequest) {
+      this.handler.onRequestAction();
+    } else if (data instanceof WelcomeMessage) {
+      this.color = ((WelcomeMessage) data).getPlayerColor();
+    }
+    this.roomId = roomId;
+  }
 
-	/**
-	 * Called when game state has been received Happens, after a client made a
-	 * move.
-	 */
-	@Override
-	public void onNewState(String roomId, Object state) {
+  /**
+   * sends the <code>move</code> to the server
+   *
+   * @param move the move you want to do
+   */
+  public void sendMove(Move move) {
+    this.client.sendMessageToRoom(this.roomId, move);
+  }
 
-		GameState gameState = (GameState) state;
-	  logger.debug("{} got new state {}", this, gameState);
+  /** Called when an error is sent to the room */
+  @Override
+  public void onError(String roomId, ProtocolErrorMessage response) {
+    logger.debug("onError: Client {} received error {}", this, response.getMessage());
+    this.error = response.getMessage();
+  }
 
-		if (this.id != EPlayerId.OBSERVER) {
-			this.handler.onUpdate(gameState);
+  /**
+   * Called when game state has been received.
+   * Happens after a client made a move.
+   */
+  @Override
+  public void onNewState(String roomId, Object state) {
+    GameState gameState = (GameState) state;
+    logger.debug("{} got new state {}", this, gameState);
 
-			if (gameState.getCurrentPlayer().getPlayerColor() == this.color) {
-				// active player is own
-				this.handler.onUpdate(gameState.getCurrentPlayer(), gameState.getOtherPlayer());
-			} else {
-				// active player is the enemy
-				this.handler.onUpdate(gameState.getOtherPlayer(), gameState.getCurrentPlayer());
-			}
-		}
-	}
+    if (this.id != EPlayerId.OBSERVER) {
+      this.handler.onUpdate(gameState);
 
-	public void joinAnyGame() {
-		this.client.joinRoomRequest(this.gameType);
-	}
+      if (gameState.getCurrentPlayer().getPlayerColor() == this.color) {
+        // active player is own
+        this.handler.onUpdate(gameState.getCurrentPlayer(), gameState.getOtherPlayer());
+      } else {
+        // active player is the enemy
+        this.handler.onUpdate(gameState.getOtherPlayer(), gameState.getCurrentPlayer());
+      }
+    }
+  }
 
-	@Override
-	public void onGameJoined(String roomId) {
+  public void joinAnyGame() {
+    this.client.joinRoomRequest(this.gameType);
+  }
 
-	}
+  @Override
+  public void onGameJoined(String roomId) {
+  }
 
-	@Override
-	public void onGameLeft(String roomId) {
+  @Override
+  public void onGamePrepared(PrepareGameProtocolMessage response) {
+  }
+
+  @Override
+  public void onGamePaused(String roomId, SimplePlayer nextPlayer) {
+  }
+
+  @Override
+  public void onGameLeft(String roomId) {
     logger.info("{} got game left {}", this, roomId);
-
     this.client.stop();
-	}
+  }
 
-	public void joinPreparedGame(String reservation) {
-		this.client.joinPreparedGame(reservation);
-	}
+  @Override
+  public void onGameOver(String roomId, GameResult data) {
+    logger.debug("{} onGameOver got game result {}", this, data);
+    if (this.handler != null) {
+      this.handler.gameEnded(data, this.color, this.error);
+    }
+  }
 
-	@Override
-	public void onGamePrepared(PrepareGameProtocolMessage response) {
-		// not needed
-	}
+  public void joinPreparedGame(String reservation) {
+    this.client.joinPreparedGame(reservation);
+  }
 
-	public String getHost() {
-		return this.host;
-	}
+  public String getHost() {
+    return this.host;
+  }
 
-	public int getPort() {
-		return this.port;
-	}
+  public int getPort() {
+    return this.port;
+  }
 
-	@Override
-	public void onGameOver(String roomId, GameResult data) {
-		logger.debug("{} onGameOver got game result {}", this, data);
-		if (this.handler != null) {
-			this.handler.gameEnded(data, this.color, this.error);
-		}
-	}
+  public String getError() {
+    return this.error;
+  }
 
-	@Override
-	public void onGamePaused(String roomId, SimplePlayer nextPlayer) {
-		// not needed
-	}
-
-	public String getError() {
-		return this.error;
-	}
-
-	public PlayerColor getColor() {
-		return this.color;
-	}
+  public PlayerColor getColor() {
+    return this.color;
+  }
 
 }
