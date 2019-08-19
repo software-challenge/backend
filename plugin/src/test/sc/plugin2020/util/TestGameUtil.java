cfaa9c13537dc645298857d9731ad8d10dfb2a51
Tim Lukas Harken <tlh@tlharken.de>
2019-08-19 09:19:49 +0200

    All errors are removed

--- plugin/src/test/sc/plugin2020/util/TestGameUtil.java
+++ plugin/src/test/sc/plugin2020/util/TestGameUtil.java
@@ -3,8 +3,32 @@ package sc.plugin2020.util;
 import org.junit.Assert;
 import sc.plugin2020.Board;
 import sc.plugin2020.FieldState;
+import sc.plugin2020.Piece;
+import sc.plugin2020.PieceType;
+import sc.shared.PlayerColor;
 
 public class TestGameUtil {
+
+  private static Piece parsePiece (PlayerColor pc, char c){
+    switch (c)
+    {
+      case 'Q':
+        return new Piece(pc, PieceType.BEE);
+
+      case 'B':
+        return new Piece(pc, PieceType.BEETLE);
+
+      case 'G':
+        return new Piece(pc, PieceType.GRASSHOPPER);
+
+      case 'S':
+        return new Piece(pc, PieceType.SPIDER);
+
+      case 'A':
+        return new Piece(pc, PieceType.ANT);
+    }
+    return null;
+  }
 
   public static Board createCustomBoard(String boardString) {//Hardcoded auf Feldgröße von 9
     Assert.assertEquals("Length of boardString does not match size of the Board",
@@ -21,12 +45,25 @@ public class TestGameUtil {
                                     {-4,0},{-3,-1},{-2,-2},{-1,-3},{0,-4}
     };
     Board board = new Board();
+    char[] tmp = boardString.toCharArray();
 
     for(int i = 0; i < fields.length; i++) {
+      board.getField(fields[i][0],fields[i][1]).getPieces().clear();
+      switch (tmp[i*2-1])
+      {
+        case 'R':
+          board.getField(fields[i][0],fields[i][1]).getPieces().add(parsePiece(PlayerColor.RED, tmp[i*2]));
+          break;
 
+        case 'B':
+          board.getField(fields[i][0],fields[i][1]).getPieces().add(parsePiece(PlayerColor.BLUE, tmp[i*2]));
+          break;
+
+        case 'O':
+          board.getField(fields[i][0],fields[i][1]).setObstructed(true);
+      }
     }
     return board;
   }
-
 }
 
