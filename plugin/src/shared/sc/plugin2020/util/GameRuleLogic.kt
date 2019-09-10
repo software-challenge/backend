c8be94d3c0c895638d861196cda3673586f973e3
Xerus <27jf@web.de>
2019-09-10 14:09:37 +0200

    fix(gameLogic): restrain beetle from jumping

--- plugin/src/shared/sc/plugin2020/util/GameRuleLogic.kt
+++ plugin/src/shared/sc/plugin2020/util/GameRuleLogic.kt
@@ -202,6 +202,8 @@ object GameRuleLogic {
     @JvmStatic
     fun validateBeetleMove(board: Board, move: DragMove) {
         validateDestinationNextToStart(move)
+        if(sharedNeighboursOfTwoCoords(board, move.start, move.destination).none { it.pieces.isNotEmpty() })
+            throw InvalidMoveException("Beetle has to move along swarm")
     }
 
     @Throws(InvalidMoveException::class)
