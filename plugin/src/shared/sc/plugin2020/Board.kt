f4e1ad557f1d911aa2ba6c473db74bbe0a189abc
Xerus <27jf@web.de>
2020-03-13 11:53:59 +0100

    feat(plugin): refactor Board methods and expose more construction possibilities

--- plugin/src/shared/sc/plugin2020/Board.kt
+++ plugin/src/shared/sc/plugin2020/Board.kt
@@ -9,7 +9,6 @@ import sc.api.plugins.IBoard
 import sc.plugin2020.util.Constants
 import sc.plugin2020.util.CubeCoordinates
 import sc.shared.PlayerColor
-import java.util.*
 import kotlin.collections.HashSet
 import kotlin.math.max
 import kotlin.math.min
@@ -25,9 +24,15 @@ data class Board(
         get() = gameField.flatMap { it.filterNotNull() }
     
     constructor(): this(fillGameField()) {
-        generateObstructed()
+        blockFields(randomFields())
     }
     
+    /** Creates a new Board with blockers at the given [blockedFields]. */
+    constructor(vararg blockedFields: CubeCoordinates): this() {
+        blockFields(blockedFields.toSet())
+    }
+    
+    /** Creates a new Board from clones of the given [fields]. Does not fill it up! */
     constructor(fields: Collection<Field>): this(gameFieldFromFields(fields))
    
     /** Copy constructor to create a new deeply copied state from the given [board]. */
@@ -36,13 +41,18 @@ data class Board(
     /** Creates a deep copy of this [Board]. */
     public override fun clone() = Board(this)
     
-    private fun generateObstructed() {
+    /** Returns [amount] random fields from [fields]. */
+    fun randomFields(amount: Int = 3): Collection<Field> {
         val all = this.fields
-        val toBeObstructed = HashSet<Field>()
-        while(toBeObstructed.size < 3)
-            toBeObstructed.add(all.random())
-        toBeObstructed.forEach {
-            this.gameField[it.x + SHIFT][it.y + SHIFT] = Field(it.x, it.y, it.z, Stack(), true)
+        val fields = HashSet<Field>()
+        while(fields.size < amount)
+            fields.add(all.random())
+        return fields
+    }
+    
+    private fun blockFields(fields: Collection<CubeCoordinates>) {
+        fields.forEach {
+            this.gameField[it.x + SHIFT][it.y + SHIFT] = Field(it.x, it.y, true)
         }
     }
     
@@ -101,7 +111,9 @@ data class Board(
         
         private fun emptyGameField() = Array(Constants.BOARD_SIZE) { arrayOfNulls<Field>(Constants.BOARD_SIZE) }
         
-        private fun fillGameField(gameField: Array<Array<Field?>> = emptyGameField()): Array<Array<Field?>> {
+        /** Fills the given [gameField] with all missing Fields for a valid Board. */
+        @JvmStatic
+        fun fillGameField(gameField: Array<Array<Field?>> = emptyGameField()): Array<Array<Field?>> {
             for(x in -SHIFT..SHIFT) {
                 for(y in max(-SHIFT, -x - SHIFT)..min(SHIFT, -x + SHIFT)) {
                     if(gameField[x + SHIFT][y + SHIFT] == null) {
@@ -112,7 +124,10 @@ data class Board(
             return gameField
         }
         
-        private fun gameFieldFromFields(fields: Collection<Field>): Array<Array<Field?>> {
+        /** Creates a new gameField from clones of the given [fields].
+         * @return a gameField consisting only of clones of the [fields] */
+        @JvmStatic
+        fun gameFieldFromFields(fields: Collection<Field>): Array<Array<Field?>> {
             val gameField = emptyGameField()
             var x: Int
             var y: Int
