f4a0e61fd4e499ec9850d235fff9616c5fbeae6f
Aaron Alef <aaron.alef@code.berlin>
2020-04-28 11:22:51 +0200

    test(sdk): remove obsolete* PlayerScore constructor test
    * Obsolete through conversion of class to Kotlin

--- socha-sdk/src/test/sc/shared/PlayerScoreTest.kt
+++ socha-sdk/src/test/sc/shared/PlayerScoreTest.kt
@@ -9,10 +9,6 @@ import sc.shared.PlayerScore
 import java.math.BigDecimal
 
 class PlayerScoreTest: StringSpec({
-    "construct PlayerScore" {
-        val scoreList: BigDecimal? = null
-        shouldThrow<IllegalArgumentException> { PlayerScore(ScoreCause.UNKNOWN, "", scoreList) }
-    }
     "check equality" {
         val playerScoreUnknown1 = PlayerScore(ScoreCause.UNKNOWN, "", 0)
         val playerScoreUnknown2 = PlayerScore(ScoreCause.UNKNOWN, "", 0)
