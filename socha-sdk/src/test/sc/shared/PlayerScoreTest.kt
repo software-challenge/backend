71d28ed6d627995e53b7822eee385381c33402e2
Aaron Alef <aaron.alef@code.berlin>
2020-04-28 11:08:29 +0200

    test(sdk): add PlayerScore equality test

--- socha-sdk/src/test/sc/shared/PlayerScoreTest.kt
+++ socha-sdk/src/test/sc/shared/PlayerScoreTest.kt
@@ -1,6 +1,7 @@
 package sc.shared
 
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.StringSpec
 import sc.shared.PlayerScore
@@ -11,5 +12,23 @@ class PlayerScoreTest: StringSpec({
     "construct PlayerScore" {
         val scoreList: BigDecimal? = null
         shouldThrow<IllegalArgumentException> { PlayerScore(ScoreCause.UNKNOWN, "", scoreList) }
+    }
+    "check equality" {
+        val playerScoreUnknown1 = PlayerScore(ScoreCause.UNKNOWN, "", 0)
+        val playerScoreUnknown2 = PlayerScore(ScoreCause.UNKNOWN, "", 0)
+        playerScoreUnknown1 shouldBe playerScoreUnknown1
+        playerScoreUnknown1 shouldBe playerScoreUnknown2
+
+        val playerScoreRegular = PlayerScore(ScoreCause.REGULAR, "", 0)
+        playerScoreUnknown1 shouldNotBe playerScoreRegular
+        
+        val playerScoreReason = PlayerScore(ScoreCause.UNKNOWN, "different", 0)
+        playerScoreUnknown1 shouldNotBe playerScoreReason
+        
+        val playerScoreScores = PlayerScore(ScoreCause.UNKNOWN, "", 0, 1)
+        playerScoreUnknown1 shouldNotBe playerScoreScores
+        
+        val noPlayerScore: Any? = null
+        noPlayerScore shouldNotBe playerScoreUnknown1
     }
 })
