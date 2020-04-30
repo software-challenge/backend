115bd47c6f3d05d800abb089c587826a29f98f33
xerus2000 <27jf@pm.me>
2020-04-30 14:42:52 +0200

    test(sdk): shorten PlayerScore equality tests
    # Conflicts:
    #	server/src/sc/server/Lobby.kt
    #	server/src/sc/server/network/ClientManager.java
    #	server/src/sc/server/network/NewClientListener.java

--- socha-sdk/src/test/sc/shared/PlayerScoreTest.kt
+++ socha-sdk/src/test/sc/shared/PlayerScoreTest.kt
@@ -9,21 +9,14 @@ import io.kotlintest.specs.StringSpec
 class PlayerScoreTest: StringSpec({
     "check equality" {
         val playerScoreUnknown1 = PlayerScore(ScoreCause.UNKNOWN, "", 0)
-        val playerScoreUnknown2 = PlayerScore(ScoreCause.UNKNOWN, "", 0)
         playerScoreUnknown1 shouldBe playerScoreUnknown1
-        playerScoreUnknown1 shouldBe playerScoreUnknown2
-
         val playerScoreRegular = PlayerScore(ScoreCause.REGULAR, "", 0)
         playerScoreUnknown1 shouldNotBe playerScoreRegular
-        
         val playerScoreReason = PlayerScore(ScoreCause.UNKNOWN, "different", 0)
         playerScoreUnknown1 shouldNotBe playerScoreReason
-        
         val playerScoreScores = PlayerScore(ScoreCause.UNKNOWN, "", 0, 1)
         playerScoreUnknown1 shouldNotBe playerScoreScores
-        
-        val noPlayerScore: Any? = null
-        noPlayerScore shouldNotBe playerScoreUnknown1
+        playerScoreScores shouldBe playerScoreScores
     }
     "convert XML" {
         val playerScore = PlayerScore(ScoreCause.REGULAR, "Reason", 0, 1, 2)
