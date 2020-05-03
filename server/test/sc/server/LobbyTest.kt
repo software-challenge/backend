769a663aab300352fae070b7990c713046a3fc02
xerus2000 <27jf@pm.me>
2020-05-03 15:21:53 +0200

    test(server): clean up LobbyTest
    Also re-enables one test from #124 since it seems to be working again.
    # Conflicts:
    #	server/src/sc/server/Lobby.kt
    #	server/src/sc/server/network/ClientManager.java
    #	server/src/sc/server/network/NewClientListener.java

--- server/test/sc/server/LobbyTest.kt
+++ server/test/sc/server/LobbyTest.kt
@@ -1,6 +1,5 @@
 package sc.server
 
-import org.junit.Assert
 import org.junit.Test
 import sc.server.helpers.TestHelper
 import sc.server.network.RealServerTest
@@ -10,26 +9,18 @@ class LobbyTest: RealServerTest() {
     
     @Test
     fun shouldConnectAndDisconnect() {
-        try {
-            val player1 = connectClient("localhost", serverPort)
-            val player2 = connectClient("localhost", serverPort)
-            
-            player1.joinRoomRequest(TestPlugin.TEST_PLUGIN_UUID)
-            player2.joinRoomRequest(TestPlugin.TEST_PLUGIN_UUID)
-            
-            // Was game created?
-            TestHelper.assertEqualsWithTimeout(1, { lobby.games.size }, 2000)
-            Assert.assertNotNull(lobby.games)
-            Assert.assertNotEquals(0, lobby.games.size.toLong())
-            Assert.assertNotNull(lobby.clientManager)
-            
-            player1.stop()
-            // FIXME sometimes fails - see Issue #124
-            // TestHelper.assertEqualsWithTimeout(0, { lobby.games.size }, 5000)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Assert.fail()
-        }
+        val player1 = connectClient("localhost", serverPort)
+        val player2 = connectClient("localhost", serverPort)
+        
+        player1.joinRoomRequest(TestPlugin.TEST_PLUGIN_UUID)
+        player2.joinRoomRequest(TestPlugin.TEST_PLUGIN_UUID)
+        
+        // Wait for game to be created
+        TestHelper.assertEqualsWithTimeout(1, { lobby.games.size }, 2000)
+        
+        // Game should be stopped when one player dies
+        player1.stop()
+        TestHelper.assertEqualsWithTimeout(0, { lobby.games.size }, 5000)
     }
     
 }
