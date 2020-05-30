f6a804dd03069af01a2717b95a44e7c469565eb3
Aaron Alef <aaron.alef@code.berlin>
2020-05-30 14:52:41 +0200

    fix(sdk): annotate kotlin constructor for java compatibility
    One constructor only called the other one with "null" as one value.
    Having this value default to null in kotlin broke the compatibility,
    though. Thus, this fixes it as far as I see it

--- socha-sdk/src/framework/sc/shared/InvalidMoveException.kt
+++ socha-sdk/src/framework/sc/shared/InvalidMoveException.kt
@@ -5,5 +5,5 @@ import kotlin.RuntimeException
 
 /** NOTE: It is important to have information about the move in the reason
  *  because that is the only place where the invalid move is logged */
-class InvalidMoveException(reason: String, @JvmField val move: IMove? = null):
+class InvalidMoveException @JvmOverloads constructor(reason: String, @JvmField val move: IMove? = null):
         RuntimeException(reason + (if (move != null) "; move was $move" else ""))
