9fe7c50b3effc7efdd876211eac4511f6cbc0c11
Aaron Alef <aaron.alef@code.berlin>
2020-05-30 14:41:00 +0200

    refactor(sdk): convert InvalidMoveException to kotlin

---
+++ socha-sdk/src/framework/sc/shared/InvalidMoveException.kt
@@ -0,0 +1,9 @@
+package sc.shared
+
+import sc.api.plugins.IMove
+import kotlin.RuntimeException
+
+/** NOTE: It is important to have information about the move in the reason
+ *  because that is the only place where the invalid move is logged */
+class InvalidMoveException(reason: String, @JvmField val move: IMove? = null):
+        RuntimeException(reason + (if (move != null) "; move was $move" else ""))
