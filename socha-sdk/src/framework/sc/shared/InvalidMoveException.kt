a26cfee1741161df820f8f191e2ccd9573ec76d6
Aaron Alef <aaron.alef@code.berlin>
2020-05-30 18:19:31 +0200

    refactor(sdk): make the InvalidMoveException reason algo more legible

--- socha-sdk/src/framework/sc/shared/InvalidMoveException.kt
+++ socha-sdk/src/framework/sc/shared/InvalidMoveException.kt
@@ -6,4 +6,4 @@ import kotlin.RuntimeException
 /** NOTE: It is important to have information about the move in the reason
  *  because that is the only place where the invalid move is logged */
 class InvalidMoveException @JvmOverloads constructor(reason: String, @JvmField val move: IMove? = null):
-        RuntimeException(reason + (if (move != null) "; move was $move" else ""))
+        RuntimeException(reason + move?.let { "; move was $it"}.orEmpty())
