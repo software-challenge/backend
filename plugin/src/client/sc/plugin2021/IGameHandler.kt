fb3b3773b4a94dba45ef8303df79c42051766a84
Aaron Alef <aaron.alef@code.berlin>
2020-07-03 19:44:22 +0200

    feat(plugin): add Client skeletons
    Since IGameHandler is an Interface, it is pretty much complete as skeleton

---
+++ plugin/src/client/sc/plugin2021/IGameHandler.kt
@@ -0,0 +1,39 @@
+package sc.plugin2021
+
+import sc.framework.plugins.Player
+import sc.shared.GameResult
+import sc.shared.PlayerColor
+
+interface IGameHandler {
+    
+    /**
+     * wird aufgerufen, wenn Spieler aktualisiert werden
+     *
+     * @param player      eigener Spieler
+     * @param otherPlayer anderer Spieler
+     */
+    fun onUpdate(player: Player, otherPlayer: Player)
+    
+    /** wird aufgerufen, wenn sich das Spielbrett aendert. */
+    fun onUpdate(gamestate: GameState)
+    
+    /** wird aufgerufen, wenn der Spieler zum Zug aufgefordert wurde. */
+    fun onRequestAction()
+    
+    /**
+     * sendet dem Spielserver den übergebenen Zug
+     *
+     * @param move zu taetigender Zug
+     */
+    fun sendAction(move: Move)
+    
+    /**
+     * aufgerufen, wenn das Spiel beendet ist.
+     *
+     * @param data         mit getScores() kann man die Punkte erfragen
+     * @param color        Playercolor
+     * @param errorMessage Fehlernachricht
+     */
+    fun gameEnded(data: GameResult, color: PlayerColor, errorMessage: String)
+    
+}
