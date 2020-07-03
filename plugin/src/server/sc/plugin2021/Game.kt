8a1f01a8f1eabeef90405506f86a719eca902e89
Aaron Alef <aaron.alef@code.berlin>
2020-07-03 19:06:17 +0200

    feat(plugin): add Game class skeleton

---
+++ plugin/src/server/sc/plugin2021/Game.kt
@@ -0,0 +1,60 @@
+package sc.plugin2021
+
+import com.thoughtworks.xstream.annotations.XStreamAlias
+import sc.api.plugins.IGameState
+import sc.framework.plugins.RoundBasedGameInstance
+import sc.framework.plugins.Player
+import sc.protocol.responses.ProtocolMessage
+import sc.shared.PlayerScore
+import sc.shared.WinCondition
+
+
+@XStreamAlias(value = "game")
+class Game: RoundBasedGameInstance<Player>() {
+    
+    override fun getWinners(): MutableList<Player> {
+        TODO("Not yet implemented")
+    }
+    
+    override fun getPlayerScores(): MutableList<PlayerScore> {
+        TODO("Not yet implemented")
+    }
+    
+    override fun getPlayers(): MutableList<Player> {
+        TODO("Not yet implemented")
+    }
+    
+    override fun checkWinCondition(): WinCondition {
+        TODO("Not yet implemented")
+    }
+    
+    override fun loadGameInfo(gameInfo: Any?) {
+        TODO("Not yet implemented")
+    }
+    
+    override fun loadFromFile(file: String?) {
+        TODO("Not yet implemented")
+    }
+    
+    override fun loadFromFile(file: String?, turn: Int) {
+        TODO("Not yet implemented")
+    }
+    
+    override fun onPlayerJoined(): Player {
+        TODO("Not yet implemented")
+    }
+    
+    override fun getScoreFor(p: Player?): PlayerScore {
+        TODO("Not yet implemented")
+    }
+    
+    override fun onRoundBasedAction(fromPlayer: Player?, data: ProtocolMessage?) {
+        TODO("Not yet implemented")
+    }
+    
+    override fun getCurrentState(): IGameState {
+        TODO("Not yet implemented")
+    }
+    
+    
+}
