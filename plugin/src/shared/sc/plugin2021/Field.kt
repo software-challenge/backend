b829407c61b996a85191741610efd4d616753f29
Aaron Alef <aaron.alef@code.berlin>
2020-07-03 18:58:39 +0200

    feat(logic): give a Char representation to FieldContents
    This will be needed to display / serialise the Board

--- plugin/src/shared/sc/plugin2021/Field.kt
+++ plugin/src/shared/sc/plugin2021/Field.kt
@@ -2,8 +2,12 @@ package sc.plugin2021
 
 import sc.api.plugins.IField
 
-enum class FieldContent {
-    EMPTY, RED, GREEN, BLUE, YELLOW
+enum class FieldContent(val letter: Char) {
+    EMPTY('-'),
+    RED('R'),
+    GREEN('G'),
+    BLUE('B'),
+    YELLOW('Y')
 }
 
 class Field(val coordinates: Coordinates, val content: FieldContent): IField
