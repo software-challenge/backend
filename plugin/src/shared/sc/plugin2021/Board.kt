b6c64f2970ec965cba1e2c1812f6764930789d65
Aaron Alef <aaron.alef@code.berlin>
2020-07-06 12:06:28 +0200

    refactor(plugin): let Boards store FieldContents  instead of Fields
    Thus we remove the redundancy (and possible source of errors) through
    duplicate assignments of coordinates.
    I.e., before, a Field could potentially have a different value than its
    position indicates.
    
    Also, removes the unused function to get a list of all fields

--- plugin/src/shared/sc/plugin2021/Board.kt
+++ plugin/src/shared/sc/plugin2021/Board.kt
@@ -6,43 +6,18 @@ import sc.plugin2021.util.Constants
 import sc.plugin2021.Field
 
 class Board(
-        val gameField: Array<Array<Field?>>
+        private val gameField: Array<Array<FieldContent>> =
+                Array(Constants.BOARD_SIZE) { Array(Constants.BOARD_SIZE) { FieldContent.EMPTY }}
 ): IBoard {
     
-    operator fun get(x: Int, y: Int) = gameField[x][y]!!
+    override fun getField(x: Int, y: Int) = this[x, y]
+    operator fun get(x: Int, y: Int) =
+            Field(Coordinates(x, y), gameField[x][y])
     
     fun set(x: Int, y: Int, content: FieldContent) {
-        gameField[x][y] = Field(Coordinates(x, y), content)
+        gameField[x][y] = content
     }
     
-    fun getFields(): List<IField> {
-        return gameField.flatMap { it.filterNotNull() }
-    }
-
-    constructor() : this(Board.fillGameField()) {}
-
-    override fun getField(x: Int, y: Int): IField =
-            gameField[x][y] ?: throw IndexOutOfBoundsException("No field at ($x,$y)")
-
     override fun hashCode(): Int =
             gameField.contentDeepHashCode()
-    
-    companion object {
-
-        private fun emptyGameField() = Array(Constants.BOARD_SIZE) { arrayOfNulls <Field?>(Constants.BOARD_SIZE )}
-
-        /** Fills the given [gameField] with all missing Fields for a valid Board. */
-        @JvmStatic
-        fun fillGameField(gameField: Array<Array<Field?>> = emptyGameField()): Array<Array<Field?>> {
-            for (x in 0 until Constants.BOARD_SIZE) {
-                for (y in 0 until Constants.BOARD_SIZE) {
-                    if (gameField[x][y] == null) {
-                        gameField[x][y] = Field(Coordinates(x, y), FieldContent.EMPTY)
-                    }
-                }
-            }
-            return gameField
-        }
-
-    }
 }
