9e5317ef825e4fd667f79799553c1ef75eb7a166
Aaron Alef <aaron.alef@code.berlin>
2020-07-15 14:41:24 +0200

    feat(plugin): implement AbstractClient for blokus
    This AbstractClient is pretty much a refactor from hive's
    AbstractClient.
    Since they don't really differ, we should probably move this to a
    plugin-independent location

--- plugin/src/client/sc/plugin2021/AbstractClient.kt
+++ plugin/src/client/sc/plugin2021/AbstractClient.kt
@@ -1,7 +1,129 @@
 package sc.plugin2021
 
+import org.slf4j.LoggerFactory
+import sc.framework.plugins.Player
+import sc.framework.plugins.protocol.MoveRequest
+import sc.networking.clients.IControllableGame
 import sc.networking.clients.ILobbyClientListener
+import sc.networking.clients.LobbyClient
+import sc.plugin2021.util.Configuration
+import sc.protocol.responses.PrepareGameProtocolMessage
+import sc.protocol.responses.ProtocolErrorMessage
+import sc.shared.GameResult
+import sc.shared.WelcomeMessage
+import java.io.IOException
+import java.net.ConnectException
+import kotlin.system.exitProcess
 
-abstract class AbstractClient: ILobbyClientListener {
-    val gameType = GamePlugin.PLUGIN_UUID
+/** Abstract client following the SDK.
+ * Contains a LobbyClient as actual client;
+ * this class is a plugin specific abstract wrapper.
+ */
+abstract class AbstractClient @Throws(IOException::class) constructor(
+        host: String,
+        port: Int,
+        private val id: PlayerType = PlayerType.PLAYER_ONE
+): ILobbyClientListener {
+    
+    companion object {
+        private val logger = LoggerFactory.getLogger(AbstractClient::class.java);
+        private val gameType = GamePlugin.PLUGIN_UUID
+    }
+    
+    /** The handler reacts to messages from the server received by the lobby client.
+     *  It *must* be initialised before start.
+     */
+    protected lateinit var handler: IGameHandler
+    
+    /** Initialise game handler. */
+    fun setGameHandler(handler: IGameHandler) {
+        this.handler = handler
+    }
+    
+    /** The lobby client that connects to the room. Stops on connection failure. */
+    private val client = try {
+        LobbyClient(Configuration.xStream, Configuration.classesToRegister, host, port)
+    } catch(e: ConnectException) {
+        logger.error("Could not connect to Server: " + e.message)
+        exitProcess(1)
+    }
+    
+    /** Storage for the reason of a rule violation, if any occurs. */
+    private lateinit var error: String
+    fun getError() = error
+    
+    private lateinit var roomID: String
+    
+    /** The team the client belongs to in order to connect client and player. */
+    private var team = when(id) {
+            PlayerType.PLAYER_ONE -> Team.ONE
+            PlayerType.PLAYER_TWO -> Team.TWO
+            else                  -> Team.NONE
+    }
+    get() = field
+    
+    /** Tell this client to observe the game given by the preparation handler.
+     *
+     * @return controllable game
+     */
+    fun observeGame(handle: PrepareGameProtocolMessage): IControllableGame =
+            client.observe(handle)
+    
+    /** Called for any new message sent to the game room, e.g., move requests. */
+    override fun onRoomMessage(roomId: String, data: Any) {
+        if(data is MoveRequest) {
+            handler.onRequestAction()
+        }
+        if(data is WelcomeMessage) {
+            team = data.playerColor as Team
+        }
+        roomID = roomId
+    }
+    
+    /** Sends the selected move to the server. */
+    fun sendMove(move: Move) =
+            client.sendMessageToRoom(roomID, move)
+    
+    /** Called when an erroneous message is sent to the room. */
+    override fun onError(roomId: String, error: ProtocolErrorMessage) {
+        logger.debug("onError: Client {} received error {}", this, error.message)
+        this.error = error.message
+    }
+    
+    override fun onNewState(roomId: String, state: Any) {
+        val gameState = state as GameState
+        logger.debug("{} got a new state {}", this, gameState)
+        
+        if(id == PlayerType.OBSERVER) return
+        
+        handler.onUpdate(gameState)
+        if(gameState.currentPlayerColor == team) {
+            handler.onUpdate(gameState.currentPlayer, gameState.otherPlayer)
+        } else {
+            handler.onUpdate(gameState.otherPlayer, gameState.currentPlayer)
+        }
+    }
+    
+    fun joinAnyGame() =
+            client.joinRoomRequest(gameType)
+    
+    override fun onGameJoined(roomId: String) {}
+    override fun onGamePrepared(response: PrepareGameProtocolMessage) {}
+    override fun onGamePaused(roomId: String, nextPlayer: Player) {}
+    override fun onGameObserved(roomId: String) {}
+    
+    override fun onGameLeft(roomId: String) {
+        logger.info("{} got game left {}", this, roomId)
+        client.stop()
+    }
+    
+    override fun onGameOver(roomId: String, data: GameResult) {
+        logger.info("{} on Game Over with game result {}", this, data)
+        if (this::handler.isInitialized) {
+            handler.gameEnded(data, team, error.orEmpty())
+        }
+    }
+    
+    fun joinPreparedGame(reservation: String) =
+            client.joinPreparedGame(reservation)
 }
