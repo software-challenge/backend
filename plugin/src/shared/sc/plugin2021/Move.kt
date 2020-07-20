b69fb3d6ed01fee18fef6be0ad43df50256bc044
Aaron Alef <aaron.alef@code.berlin>
2020-07-20 12:07:33 +0200

    feat(plugin): add new Move, SetMove, to place down a single piece

--- plugin/src/shared/sc/plugin2021/Move.kt
+++ plugin/src/shared/sc/plugin2021/Move.kt
@@ -4,4 +4,8 @@ import sc.api.plugins.IMove
 
 sealed class Move: IMove {
     abstract val piece: Piece
+    
+    override fun toString(): String = piece.toString()
 }
+
+class SetMove(override val piece: Piece): Move()
