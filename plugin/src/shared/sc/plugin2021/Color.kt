f10bbb9fd8dd57bde1bf2f8c3e263fcf3c177660
Aaron Alef <aaron.alef@code.berlin>
2020-07-22 12:13:14 +0200

    fix(plugin): balance out off-by-one error of Color's corner coords

--- plugin/src/shared/sc/plugin2021/Color.kt
+++ plugin/src/shared/sc/plugin2021/Color.kt
@@ -9,11 +9,11 @@ enum class Color(val team: Team, val letter: Char, val corner: Coordinates) {
         NONE.next = NONE }},
     BLUE  (Team.ONE,  'B', Coordinates(0, 0)),
     
-    YELLOW(Team.ONE,  'Y', Coordinates(Constants.BOARD_SIZE, 0)) { init {
+    YELLOW(Team.ONE,  'Y', Coordinates(Constants.BOARD_SIZE -1, 0)) { init {
             BLUE.next = YELLOW }},
-    RED   (Team.TWO,  'R', Coordinates(Constants.BOARD_SIZE, Constants.BOARD_SIZE)) { init {
+    RED   (Team.TWO,  'R', Coordinates(Constants.BOARD_SIZE -1, Constants.BOARD_SIZE -1)) { init {
             YELLOW.next = RED }},
-    GREEN (Team.TWO,  'G', Coordinates(0, Constants.BOARD_SIZE)) { init {
+    GREEN (Team.TWO,  'G', Coordinates(0, Constants.BOARD_SIZE -1)) { init {
             RED.next = GREEN
             GREEN.next = BLUE }};
     
