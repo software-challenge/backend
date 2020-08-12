1cf646f1a4e0729fffa7cfe3d5078a910ac1fc83
Aaron Alef <aaron.alef@code.berlin>
2020-08-12 12:41:24 +0200

    test(plugin): rename game test

--- plugin/src/test/sc/plugin2021/GameTest.kt
+++ plugin/src/test/sc/plugin2021/GameTest.kt
@@ -2,15 +2,24 @@ package sc.plugin2021
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import sc.plugin2020.util.Constants
+import sc.shared.PlayerScore
+import sc.shared.ScoreCause
 
 class GameTest: StringSpec({
-    "Start a game" {
+    "Game starting and stopping works" {
+        Color.BLUE.team
         val game = Game()
-        val playerOne = game.onPlayerJoined()
-        val playerTwo = game.onPlayerJoined()
+        val state = game.gameState
+        game.onPlayerJoined() shouldBe Team.ONE
+        game.onPlayerJoined() shouldBe Team.TWO
         
-        playerOne.color shouldBe Team.ONE
-        playerTwo.color shouldBe Team.TWO
         game.start()
+        game.onAction(state.currentPlayer, PassMove(state.currentColor))
+        game.onAction(state.currentPlayer, PassMove(state.currentColor))
+        game.onAction(state.currentPlayer, PassMove(state.currentColor))
+        game.onAction(state.currentPlayer, PassMove(state.currentColor))
+        
+        game.playerScores shouldBe List(2) { PlayerScore(ScoreCause.REGULAR, "", Constants.DRAW_SCORE, -178) }
     }
 })
