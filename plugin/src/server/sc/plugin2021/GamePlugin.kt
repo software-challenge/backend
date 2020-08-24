2cde70a31f0528f12f5774938cd7859ad02d34ab
Aaron Alef <aaron.alef@code.berlin>
2020-08-24 16:00:53 +0200

    feat(plugin): implement ScoreDefinition

--- plugin/src/server/sc/plugin2021/GamePlugin.kt
+++ plugin/src/server/sc/plugin2021/GamePlugin.kt
@@ -5,7 +5,9 @@ import sc.api.plugins.IGamePlugin
 import sc.api.plugins.host.IGamePluginHost
 import sc.plugin2021.util.Configuration.classesToRegister
 import sc.plugins.PluginDescriptor
+import sc.shared.ScoreAggregation
 import sc.shared.ScoreDefinition
+import sc.shared.ScoreFragment
 
 @PluginDescriptor(name = "Blokus", uuid = "swc_2021_blokus", author = "")
 class GamePlugin: IGamePlugin {
@@ -13,6 +15,11 @@ class GamePlugin: IGamePlugin {
     companion object {
         val PLUGIN_AUTHOR = ""
         val PLUGIN_UUID = "swc_2021_blokus"
+        
+        val SCORE_DEFINITION = ScoreDefinition().apply {
+            add("Gewinner")
+            add(ScoreFragment("\u2205 belegte Felder", ScoreAggregation.AVERAGE))
+        }
     }
     
     override fun createGame(): IGameInstance {
@@ -27,8 +34,5 @@ class GamePlugin: IGamePlugin {
         TODO("Not yet implemented")
     }
     
-    override fun getScoreDefinition(): ScoreDefinition {
-        TODO("Not yet implemented")
-    }
-    
+    override fun getScoreDefinition(): ScoreDefinition = SCORE_DEFINITION
 }
