d4dd3087e8e2863dfe9f88691cd77176199cbb82
Aaron Alef <aaron.alef@code.berlin>
2020-08-25 11:13:58 +0200

    fix(plugin): ensure a game is running smoothly with random moves

--- plugin/src/test/sc/plugin2021/GameTest.kt
+++ plugin/src/test/sc/plugin2021/GameTest.kt
@@ -2,9 +2,11 @@ package sc.plugin2021
 
 import io.kotlintest.matchers.collections.shouldContainExactly
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.StringSpec
 import org.junit.jupiter.api.assertThrows
 import sc.plugin2020.util.Constants
+import sc.plugin2021.util.GameRuleLogic
 import sc.shared.InvalidMoveException
 import sc.shared.PlayerScore
 import sc.shared.ScoreCause
@@ -28,12 +30,33 @@ class GameTest: StringSpec({
             game.onAction(state.currentPlayer, PassMove(state.currentColor))
         }
     
-        println(game.winners)
         game.winners shouldBe listOf(second)
         
         game.playerScores shouldContainExactly listOf(
                 PlayerScore(ScoreCause.RULE_VIOLATION, e.message, Constants.LOSE_SCORE, -178),
                 PlayerScore(ScoreCause.REGULAR, "", Constants.WIN_SCORE, -178)
         )
+    }
+    "A few moves can be performd without issues" {
+        val game = Game()
+        val state = game.gameState
+        Pair(game.onPlayerJoined(), game.onPlayerJoined())
+        game.start()
+        
+        while (true) {
+            try {
+                val condition = game.checkWinCondition()
+                if (condition != null) {
+                    println(condition)
+                    break
+                }
+                val moves = GameRuleLogic.getPossibleMoves(state)
+                moves shouldNotBe emptySet<SetMove>()
+                game.onAction(state.currentPlayer, moves.random())
+            } catch (e: Exception) {
+                println(e.message)
+                break
+            }
+        }
     }
 })
