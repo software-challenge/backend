986924d6227d4cc303426623ab99b72ab8207f03
Aaron Alef <aaron.alef@code.berlin>
2020-09-05 21:24:17 +0200

    test(sdk): refactor two test lists into one list of pairs

--- socha-sdk/src/test/sc/shared/SlotDescriptorTest.kt
+++ socha-sdk/src/test/sc/shared/SlotDescriptorTest.kt
@@ -12,19 +12,20 @@ class SlotDescriptorTest : StringSpec({
             autodetectAnnotations(true)
         }
         val descriptors = listOf(
-                SlotDescriptor(),
-                SlotDescriptor("Display Name"),
-                SlotDescriptor("name", false),
-                SlotDescriptor("another name", true, false)
-        )
-        val XMLs = listOf(
+                SlotDescriptor() to
                 """<slotDescriptor displayName="Unknown" canTimeout="true" shouldBePaused="true"/>""",
+                
+                SlotDescriptor("Display Name") to
                 """<slotDescriptor displayName="Display Name" canTimeout="true" shouldBePaused="true"/>""",
+                
+                SlotDescriptor("name", false) to
                 """<slotDescriptor displayName="name" canTimeout="false" shouldBePaused="true"/>""",
+                
+                SlotDescriptor("another name", true, false) to
                 """<slotDescriptor displayName="another name" canTimeout="true" shouldBePaused="false"/>"""
         )
         
-        (descriptors zip XMLs).forAll {
+        descriptors.forAll {
             xstream.toXML(it.first) shouldBe it.second
             xstream.fromXML(it.second).toString() shouldBe it.first.toString()
         }
