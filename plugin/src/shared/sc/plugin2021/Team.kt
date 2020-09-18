fd9c93e2ec742fbbe14ccb48522a3c921fcd3968
Aaron Alef <aaron.alef@code.berlin>
2020-09-18 18:19:55 +0200

    fix(plugin): give each team the right list of colors (#291)

--- plugin/src/shared/sc/plugin2021/Team.kt
+++ plugin/src/shared/sc/plugin2021/Team.kt
@@ -6,11 +6,11 @@ import sc.shared.WelcomeMessage
 
 @XStreamAlias(value = "team")
 enum class Team(override val index: Int, val colors: List<Color>): ITeam {
-    ONE(0, listOf(Color.BLUE, Color.YELLOW)) {
+    ONE(0, listOf(Color.BLUE, Color.RED)) {
         override fun opponent(): Team = TWO
         override fun toString(): String = "One"
     },
-    TWO(1, listOf(Color.RED, Color.GREEN)) {
+    TWO(1, listOf(Color.YELLOW, Color.GREEN)) {
         override fun opponent(): Team = ONE
         override fun toString(): String = "Two"
     };
