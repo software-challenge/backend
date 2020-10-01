80914066159102f27f7eac71554e48f326955524
Aaron Alef <aaron.alef@code.berlin>
2020-10-01 09:26:25 +0200

    docs(plugin): comment the WinReason enum

--- plugin/src/shared/sc/plugin2021/WinReason.kt
+++ plugin/src/shared/sc/plugin2021/WinReason.kt
@@ -3,11 +3,14 @@ package sc.plugin2021.util
 import com.thoughtworks.xstream.annotations.XStreamAlias
 import sc.shared.IWinReason
 
+/** Eine Beschreibung, welches Team gewonnen hat und warum. */
 @XStreamAlias(value = "winreason")
 enum class WinReason(override val message: String): IWinReason {
+    /** Beide Teams haben die gleiche Punktzahl. */
     EQUAL_SCORE("Das Spiel ist beendet.\nBeide Teams haben die gleiche Punktzahl erzielt.") {
         override fun getMessage(playerName: String?) = message
     },
+    /** Ein Team hat eine höhere Punktzahl als das andere. */
     DIFFERING_SCORES("Das Spiel ist beended.\n%s hat am meisten Punkte erzielt.") {
         override fun getMessage(playerName: String?) =
                 String.format(message, playerName.let{"???"})
