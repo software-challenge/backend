81d843d281d892c237004fac68bab437a1c19d8d
Aaron Alef <aaron.alef@code.berlin>
2020-10-01 11:08:40 +0200

    docs(plugin): add Rotation documentation

--- plugin/src/shared/sc/plugin2021/Rotation.kt
+++ plugin/src/shared/sc/plugin2021/Rotation.kt
@@ -2,14 +2,19 @@ package sc.plugin2021
 
 import com.thoughtworks.xstream.annotations.XStreamAlias
 
-/** Determines how often a Piece has been rotated clockwise. */
+/** Beschreibt, wie weit eine [PieceShape] gedreht werden soll. */
 @XStreamAlias(value = "rotation")
 enum class Rotation(val value: Int) {
     NONE(0),
     RIGHT(1),
+    /** Dreht die Form um 180 grad. */
     MIRROR(2),
     LEFT(3);
-    
+
+    /** 
+     * Summiere beide Rotationen auf.
+     * (Die resultierende Rotation hat den gleichen Effekt wie die beiden Rotationen einzeln).
+     */
     fun rotate(rotation: Rotation): Rotation =
             Rotation.values()[(Rotation.values().size + value + rotation.value) % Rotation.values().size]
 }
