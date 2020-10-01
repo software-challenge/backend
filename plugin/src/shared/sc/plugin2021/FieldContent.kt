1347efacd1a9561de0b8683f4bd5a22c0205f593
Aaron Alef <aaron.alef@code.berlin>
2020-10-01 10:05:19 +0200

    docs(plugin): add documentation for FieldContent

--- plugin/src/shared/sc/plugin2021/FieldContent.kt
+++ plugin/src/shared/sc/plugin2021/FieldContent.kt
@@ -2,6 +2,10 @@ package sc.plugin2021
 
 import com.thoughtworks.xstream.annotations.XStreamAlias
 
+/**
+ * Die Farbe eines [Field]s.
+ * Kann entweder eine der vier [Color]s haben oder leer sein.
+ */
 @XStreamAlias(value = "content")
 enum class FieldContent(val letter: Char) {
     BLUE('B'),
@@ -9,9 +13,11 @@ enum class FieldContent(val letter: Char) {
     RED('R'),
     GREEN('G'),
     EMPTY('-');
-    
+
+    /** Prüfe, ob der Inhalt des Felds leer ist. */
     fun empty(): Boolean = this == EMPTY
-    
+
+    /** Konvertiert zur entsprechenden Farbe oder null wenn leer. */
     operator fun unaryPlus(): Color? = when(this) {
         BLUE   -> Color.BLUE
         YELLOW -> Color.YELLOW
