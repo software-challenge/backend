b7450e5ab068687d88f66f59c42d68ceaeae2e87
Aaron Alef <aaron.alef@code.berlin>
2020-11-03 13:20:27 +0100

    feat(plugin): add a subjective toString to MoveMistakes
    This toString method returns a message telling which color violated and
    why

--- plugin/src/shared/sc/plugin2021/util/MoveMistake.kt
+++ plugin/src/shared/sc/plugin2021/util/MoveMistake.kt
@@ -10,31 +10,41 @@ import sc.plugin2021.Color
 enum class MoveMistake {
     WRONG_COLOR {
         override fun toString(): String = "Die Farbe des Zuges ist nicht an der Reihe"
+        override fun toString(color: Color): String = "Farbe $color ist nicht an der Reihe"
     },
     NOT_IN_CORNER {
         override fun toString(): String = "Der erste Zug muss auf eine freie Ecke gesetzt werden"
+        override fun toString(color: Color): String = "Farbe $color hat den ersten Zug nicht auf eine freie Ecke gesetzt"
     },
     NO_SHARED_CORNER {
         override fun toString(): String = "Alle Teile müssen ein vorheriges Teil gleicher Farbe über mindestens eine Ecke berühren"
+        override fun toString(color: Color): String = "Farbe $color hat einen Stein nicht an die Ecke eines vorhandenen Teils gleicher Farbe gelegt"
     },
     WRONG_SHAPE {
         override fun toString(): String = "Der erste Zug muss den festgelegten Spielstein setzen"
+        override fun toString(color: Color): String = "Farbe $color hat im ersten Zug den falschen Spielstein gewählt"
     },
     SKIP_FIRST_TURN {
         override fun toString(): String = "Der erste Zug muss einen Stein setzen"
+        override fun toString(color: Color): String = "Farbe $color hat in der erstes Runde gepasst"
     },
     DUPLICATE_SHAPE {
         override fun toString(): String = "Der gewählte Stein wurde bereits gesetzt"
+        override fun toString(color: Color): String = "Farbe $color hat einen bereits gelegten Stein erneut gelegt"
     },
     OUT_OF_BOUNDS {
         override fun toString(): String = "Der Spielstein passt nicht vollständig auf das Spielfeld"
+        override fun toString(color: Color): String = "Farbe $color hat einen Stein nicht vollständig aufs Spielfeld gelegt"
     },
     OBSTRUCTED {
         override fun toString(): String = "Der Spielstein würde eine andere Farbe überlagern"
+        override fun toString(color: Color): String = "Farbe $color hat einen Stein auf einen anderen Stein gelegt"
     },
     TOUCHES_SAME_COLOR {
         override fun toString(): String = "Der Spielstein berührt ein Feld gleicher Farbe"
+        override fun toString(color: Color): String = "Farbe $color hat einen Stein neben einen Stein gleicher Farbe gelegt"
     };
 
     abstract override fun toString(): String
+    abstract fun toString(color: Color): String
 }
