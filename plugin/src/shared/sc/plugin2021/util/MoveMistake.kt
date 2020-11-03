85da2a1d20a4e918fee3ca544122b1a8d2140aed
Aaron Alef <aaron.alef@code.berlin>
2020-11-03 13:12:42 +0100

    refactor(plugin): remove the internal msg of MoveMistakes

--- plugin/src/shared/sc/plugin2021/util/MoveMistake.kt
+++ plugin/src/shared/sc/plugin2021/util/MoveMistake.kt
@@ -1,20 +1,40 @@
 package sc.plugin2021.util
 
+import sc.plugin2021.Color
+
 /**
  * Wird optional bei Validierung von Zügen zurückgegeben, falls ein Zug nicht valide ist.
  * MoveMistakes entstehen bei Zügen, die theoretisch möglich sein könnten,
  * es aber bei dem jeweiligen Spielstand nicht sind.
  */
-enum class MoveMistake(val msg: String = "") {
-    WRONG_COLOR("Die Farbe des Zuges ist nicht an der Reihe"),
-    NOT_IN_CORNER("Der erste Zug muss auf eine freie Ecke gesetzt werden"),
-    NO_SHARED_CORNER("Alle Teile müssen ein vorheriges Teil gleicher Farbe über mindestens eine Ecke berühren"),
-    WRONG_SHAPE("Der erste Zug muss den festgelegten Spielstein setzen"),
-    SKIP_FIRST_TURN("Der erste Zug muss einen Stein setzen"),
-    DUPLICATE_SHAPE("Der gewählte Stein wurde bereits gesetzt"),
-    OUT_OF_BOUNDS("Der Spielstein passt nicht vollständig auf das Spielfeld"),
-    OBSTRUCTED("Der Spielstein würde eine andere Farbe überlagern"),
-    TOUCHES_SAME_COLOR("Der Spielstein berührt ein Feld gleicher Farbe");
+enum class MoveMistake {
+    WRONG_COLOR {
+        override fun toString(): String = "Die Farbe des Zuges ist nicht an der Reihe"
+    },
+    NOT_IN_CORNER {
+        override fun toString(): String = "Der erste Zug muss auf eine freie Ecke gesetzt werden"
+    },
+    NO_SHARED_CORNER {
+        override fun toString(): String = "Alle Teile müssen ein vorheriges Teil gleicher Farbe über mindestens eine Ecke berühren"
+    },
+    WRONG_SHAPE {
+        override fun toString(): String = "Der erste Zug muss den festgelegten Spielstein setzen"
+    },
+    SKIP_FIRST_TURN {
+        override fun toString(): String = "Der erste Zug muss einen Stein setzen"
+    },
+    DUPLICATE_SHAPE {
+        override fun toString(): String = "Der gewählte Stein wurde bereits gesetzt"
+    },
+    OUT_OF_BOUNDS {
+        override fun toString(): String = "Der Spielstein passt nicht vollständig auf das Spielfeld"
+    },
+    OBSTRUCTED {
+        override fun toString(): String = "Der Spielstein würde eine andere Farbe überlagern"
+    },
+    TOUCHES_SAME_COLOR {
+        override fun toString(): String = "Der Spielstein berührt ein Feld gleicher Farbe"
+    };
 
-    override fun toString(): String = msg
+    abstract override fun toString(): String
 }
