c9d6201c6f6d4b8d0204154f76b6d5070489582e
Aaron Alef <aaron.alef@code.berlin>
2020-11-23 11:51:04 +0100

    feat(sdk): store the MoveMistake enum within InvalidMoveExceptions
    With this, automated detection of the type of mistake that happened is
    possible

--- sdk/src/framework/sc/shared/InvalidMoveException.kt
+++ sdk/src/framework/sc/shared/InvalidMoveException.kt
@@ -1,9 +1,12 @@
 package sc.shared
 
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute
 import sc.api.plugins.IMove
 import kotlin.RuntimeException
 
 /** NOTE: It is important to have information about the move in the reason
  *  because that is the only place where the invalid move is logged */
-class InvalidMoveException @JvmOverloads constructor(mistake: IMoveMistake, @JvmField val move: IMove? = null):
+class InvalidMoveException @JvmOverloads constructor(
+        @JvmField val mistake: IMoveMistake,
+        @JvmField val move: IMove? = null):
         RuntimeException("$mistake${move?.let { "; move was $it"}.orEmpty()}")
