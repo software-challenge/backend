db1532c9e3d8afb30991e82fd7556e5f1e293b61
Aaron Alef <aaron.alef@code.berlin>
2020-11-27 14:58:37 +0100

    test(server): convert TestPreparedGameResponseListener to Kotlin

---
+++ server/test/sc/server/client/TestPreparedGameResponseListener.kt
@@ -0,0 +1,27 @@
+package sc.server.client
+
+import sc.api.plugins.IGameState
+import sc.framework.plugins.Player
+import sc.networking.clients.ILobbyClientListener
+import sc.protocol.responses.PrepareGameProtocolMessage
+import sc.protocol.responses.ProtocolErrorMessage
+import sc.protocol.responses.ProtocolMessage
+import sc.shared.GameResult
+
+class TestPreparedGameResponseListener : ILobbyClientListener {
+    lateinit var response: PrepareGameProtocolMessage
+
+    override fun onGamePrepared(gameResponse: PrepareGameProtocolMessage) {
+        response = gameResponse
+        println("------------------got here")
+    }
+
+    override fun onNewState(roomId: String, state: IGameState) {}
+    override fun onError(roomId: String, error: ProtocolErrorMessage) {}
+    override fun onRoomMessage(roomId: String, data: ProtocolMessage) {}
+    override fun onGameLeft(roomId: String) {}
+    override fun onGameJoined(roomId: String) {}
+    override fun onGameOver(roomId: String, data: GameResult) {}
+    override fun onGamePaused(roomId: String, nextPlayer: Player) {}
+    override fun onGameObserved(roomId: String) {}
+}
