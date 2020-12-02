ed28e0e9338641874b0bf37e17438d4bf11fc9db
Aaron Alef <aaron.alef@code.berlin>
2020-12-02 21:21:58 +0100

    refactor(sdk): make ScoreFragment fields immutable

--- sdk/src/framework/sc/shared/ScoreFragment.kt
+++ sdk/src/framework/sc/shared/ScoreFragment.kt
@@ -6,8 +6,8 @@ import com.thoughtworks.xstream.annotations.XStreamAsAttribute
 @XStreamAlias(value = "scoreFragment")
 data class ScoreFragment @JvmOverloads constructor(
         @XStreamAsAttribute
-        val name: String?,
-        val aggregation: ScoreAggregation? = ScoreAggregation.SUM,
+        val name: String,
+        val aggregation: ScoreAggregation = ScoreAggregation.SUM,
         val relevantForRanking: Boolean = true) {
 
     override fun toString(): String =
