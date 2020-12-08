bcc4ac8d025997547b4352f60f8994a8592f6336
xerus2000 <27jf@pm.me>
2020-12-08 13:54:34 +0100

    refactor(sdk): introduce AbstractLobbyClientListener

---
+++ sdk/src/server-api/sc/networking/clients/AbstractLobbyClientListener.kt
@@ -0,0 +1,20 @@
+package sc.networking.clients
+
+import sc.api.plugins.IGameState
+import sc.framework.plugins.Player
+import sc.protocol.responses.PrepareGameProtocolMessage
+import sc.protocol.responses.ProtocolErrorMessage
+import sc.protocol.responses.ProtocolMessage
+import sc.shared.GameResult
+
+abstract class AbstractLobbyClientListener: ILobbyClientListener {
+    override fun onNewState(roomId: String, state: IGameState) {}
+    override fun onRoomMessage(roomId: String, data: ProtocolMessage) {}
+    override fun onError(roomId: String, error: ProtocolErrorMessage) {}
+    override fun onGamePrepared(response: PrepareGameProtocolMessage) {}
+    override fun onGameLeft(roomId: String) {}
+    override fun onGameJoined(roomId: String) {}
+    override fun onGameOver(roomId: String, data: GameResult) {}
+    override fun onGamePaused(roomId: String, nextPlayer: Player) {}
+    override fun onGameObserved(roomId: String) {}
+}
--- sdk/src/server-api/sc/networking/clients/IAdministrativeListener.java
+++
@@ -1,8 +0,0 @@
-package sc.networking.clients;
-
-import sc.framework.plugins.Player;
-
-public interface IAdministrativeListener
-{
-	void onGamePaused(String roomId, Player nextPlayer);
-}
