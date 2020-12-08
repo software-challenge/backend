44521053171d785ed6baa5ef1ad094b4944e05f1
xerus2000 <27jf@pm.me>
2020-12-08 13:54:34 +0100

    refactor(sdk): introduce AbstractLobbyClientListener

---
+++ sdk/src/server-api/sc/networking/clients/ILobbyClientListener.kt
@@ -0,0 +1,20 @@
+package sc.networking.clients
+
+import sc.api.plugins.IGameState
+import sc.framework.plugins.Player
+import sc.protocol.responses.PrepareGameProtocolMessage
+import sc.protocol.responses.ProtocolErrorMessage
+import sc.protocol.responses.ProtocolMessage
+import sc.shared.GameResult
+
+interface ILobbyClientListener {
+    fun onNewState(roomId: String, state: IGameState)
+    fun onRoomMessage(roomId: String, data: ProtocolMessage)
+    fun onError(roomId: String, error: ProtocolErrorMessage)
+    fun onGamePrepared(response: PrepareGameProtocolMessage)
+    fun onGameLeft(roomId: String)
+    fun onGameJoined(roomId: String)
+    fun onGameOver(roomId: String, data: GameResult)
+    fun onGamePaused(roomId: String, nextPlayer: Player)
+    fun onGameObserved(roomId: String)
+}
--- server/test/sc/server/client/TestObserverListener.java
+++ server/test/sc/server/client/TestObserverListener.java
@@ -1,14 +1,8 @@
 package sc.server.client;
 
-import sc.api.plugins.IGameState;
-import sc.framework.plugins.Player;
-import sc.networking.clients.ILobbyClientListener;
-import sc.protocol.responses.PrepareGameProtocolMessage;
-import sc.protocol.responses.ProtocolErrorMessage;
-import sc.protocol.responses.ProtocolMessage;
-import sc.shared.GameResult;
+import sc.networking.clients.AbstractLobbyClientListener;
 
-public class TestObserverListener implements ILobbyClientListener {
+public class TestObserverListener extends AbstractLobbyClientListener {
 
   public String roomid;
 
@@ -16,37 +10,4 @@ public class TestObserverListener implements ILobbyClientListener {
   public void onGameObserved(String roomId) {
     this.roomid = roomId;
   }
-
-  @Override
-  public void onNewState(String roomId, IGameState state) {
-  }
-
-  @Override
-  public void onError(String roomId, ProtocolErrorMessage error) {
-  }
-
-  @Override
-  public void onRoomMessage(String roomId, ProtocolMessage data) {
-  }
-
-  @Override
-  public void onGamePrepared(PrepareGameProtocolMessage response) {
-  }
-
-  @Override
-  public void onGameLeft(String roomId) {
-  }
-
-  @Override
-  public void onGameJoined(String roomId) {
-  }
-
-  @Override
-  public void onGameOver(String roomId, GameResult data) {
-  }
-
-  @Override
-  public void onGamePaused(String roomId, Player nextPlayer) {
-  }
-
 }
