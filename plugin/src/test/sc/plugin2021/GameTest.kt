ac5cce224baa596b89deae87ebe67d1312eb0aa5
xerus2000 <27jf@pm.me>
2021-01-18 19:26:23 +0100

    test(plugin): use SingleInstance for GameTest

--- plugin/src/test/sc/plugin2021/GameTest.kt
+++ plugin/src/test/sc/plugin2021/GameTest.kt
@@ -2,11 +2,11 @@ package sc.plugin2021
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import org.slf4j.LoggerFactory
 import sc.api.plugins.exceptions.GameLogicException
 import sc.plugin2021.util.Constants
 import sc.plugin2021.util.GameRuleLogic
@@ -15,18 +15,18 @@ import sc.shared.ScoreCause
 import java.math.BigDecimal
 
 class GameTest: WordSpec({
-    val logger = LoggerFactory.getLogger(GameTest::class.java)
-
-    "A Game" When {
+    isolationMode = IsolationMode.SingleInstance
+    val startGame = {
         val game = Game()
-        val state = game.currentState
-        "started with two players" should {
-            val one = game.onPlayerJoined()
-            one.color shouldBe Team.ONE
-            val two = game.onPlayerJoined()
-            two.color shouldBe Team.TWO
-            game.start()
-            "finish without issues and return correct scores" {
+        game.onPlayerJoined().color shouldBe Team.ONE
+        game.onPlayerJoined().color shouldBe Team.TWO
+        game.start()
+        Pair(game, game.currentState)
+    }
+    "A Game start with two players" When {
+        "played normally" should {
+            val (game, state) = startGame()
+            "finish without issues" {
                 while (true) {
                     try {
                         val condition = game.checkWinCondition()
@@ -42,45 +42,47 @@ class GameTest: WordSpec({
                         break
                     }
                 }
-
+            }
+            "return regular scores"  {
                 val scores = game.playerScores
-                val score1 = game.getScoreFor(one)
-                val score2 = game.getScoreFor(two)
+                val score1 = game.getScoreFor(game.players.first())
+                val score2 = game.getScoreFor(game.players.last())
                 scores shouldBe listOf(score1, score2)
-                score1.cause shouldBe ScoreCause.REGULAR
-                score2.cause shouldBe ScoreCause.REGULAR
-
-                val points1 = BigDecimal(state.getPointsForPlayer(one.color))
-                val points2 = BigDecimal(state.getPointsForPlayer(two.color))
+                scores.forEach { it.cause shouldBe ScoreCause.REGULAR }
+        
+                val points1 = BigDecimal(state.getPointsForPlayer(Team.ONE))
+                val points2 = BigDecimal(state.getPointsForPlayer(Team.TWO))
                 when {
                     points1 < points2 -> {
-                        score1.parts shouldBe listOf(BigDecimal(0), points1)
-                        score2.parts shouldBe listOf(BigDecimal(2), points2)
+                        score1.parts shouldBe listOf(BigDecimal(Constants.LOSE_SCORE), points1)
+                        score2.parts shouldBe listOf(BigDecimal(Constants.WIN_SCORE), points2)
                     }
                     points1 > points2 -> {
-                        score1.parts shouldBe listOf(BigDecimal(2), points1)
-                        score2.parts shouldBe listOf(BigDecimal(0), points2)
+                        score1.parts shouldBe listOf(BigDecimal(Constants.WIN_SCORE), points1)
+                        score2.parts shouldBe listOf(BigDecimal(Constants.LOSE_SCORE), points2)
                     }
                     points1 == points2 -> {
-                        score1.parts shouldBe listOf(BigDecimal(1), points1)
-                        score2.parts shouldBe listOf(BigDecimal(1), points2)
+                        score1.parts shouldBe listOf(BigDecimal(Constants.DRAW_SCORE), points1)
+                        score2.parts shouldBe listOf(BigDecimal(Constants.DRAW_SCORE), points2)
                     }
                 }
             }
-            "end in a draw when everyone skips" {
-                for (s in 0 until 4)
-                    game.onAction(state.currentPlayer, GameRuleLogic.streamPossibleMoves(state).first())
-
-                shouldNotThrowAny {
-                    while (!game.checkGameOver()) {
-                        logger.debug("is Game Over? ${game.checkGameOver()} - ${state.round} > ${Constants.ROUND_LIMIT}")
-                        game.onAction(state.currentPlayer, SkipMove(state.currentColor))
-                    }
-                }
-                shouldThrow<GameLogicException> {
+        }
+        "everyone skips" should {
+            val (game, state) = startGame()
+            for (s in 0 until 4)
+                game.onAction(state.currentPlayer, GameRuleLogic.streamPossibleMoves(state).first())
+            
+            shouldNotThrowAny {
+                while (!game.checkGameOver()) {
                     game.onAction(state.currentPlayer, SkipMove(state.currentColor))
                 }
-
+            }
+            shouldThrow<GameLogicException> {
+                game.onAction(state.currentPlayer, SkipMove(state.currentColor))
+            }
+    
+            "end in a draw" {
                 game.playerScores shouldContainExactly List(2) {
                     PlayerScore(ScoreCause.REGULAR, "", Constants.DRAW_SCORE, 10)
                 }
