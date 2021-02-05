25d7157e0f47c1fbf5f12b408eb1a6a4c9872008
xerus2000 <27jf@pm.me>
2021-02-05 12:40:56 +0100

    test(server/LobbyRequestTest): sanitize initialisation

--- server/test/sc/server/network/LobbyRequestTest.kt
+++ server/test/sc/server/network/LobbyRequestTest.kt
@@ -1,33 +1,33 @@
 package sc.server.network
 
 import io.kotest.assertions.timing.eventually
+import io.kotest.assertions.withClue
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import sc.server.plugins.TestPlugin
-import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
-
-@ExperimentalTime
-suspend fun <T> assertEqualWithTimeout(value: T, f: () -> T, duration: Duration = 100.milliseconds) =
-    eventually(duration) { f() shouldBe value }
 
 @ExperimentalTime
 class LobbyRequestTest: WordSpec({
     isolationMode = IsolationMode.SingleInstance
     "A Lobby with connected clients" When {
         val lobby = autoClose(TestLobby())
-        val players = Array(3) {
+        val players = Array(3) { pos ->
             val player = lobby.connectClient("localhost", lobby.serverPort)
-            Thread.sleep(200)
+            withClue("Client connected") {
+                eventually(200.milliseconds) { lobby.clientManager.clients.size shouldBe pos + 1 }
+            }
             player
         }
         "a player joined" should {
             players[0].joinRoomRequest(TestPlugin.TEST_PLUGIN_UUID)
             "create a room for it" {
-                assertEqualWithTimeout(1, { lobby.games.size })
+                withClue("Room opened") {
+                    eventually(100.milliseconds) { lobby.games.size shouldBe 1 }
+                }
                 lobby.games.single().clients shouldHaveSize 1
             }
         }
