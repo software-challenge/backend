d740dd6edf306c64f9b83e57aa1fa47b1421c813
Sven Koschnicke <s.koschnicke@gfxpro.com>
2021-02-09 18:49:04 +0100

    chore: update deployment script

--- push_production.sh
+++ push_production.sh
@@ -10,6 +10,6 @@ docker push localhost:5000/swc_game-server
 ssh -S ssh-ctrl-socket -O exit $server
 # this is currently done in webapp
 #scp docker-compose-production.yml $server:./docker-compose.yml
-ssh $server 'sudo docker-compose pull' && \
+ssh $server 'sudo docker pull localhost:5000/swc_game-server' && \
 # NOTE This requires the other services (client-controller and gameserver) in the compose file also already deployed
-ssh $server 'sudo docker-compose up -d'
+ssh $server 'sudo docker service update --image localhost:5000/swc_game-server:latest --with-registry-auth deploy_gameserver'
