74a91827d5bb08f00cd11288212f412113305aea
xerus2000 <27jf@pm.me>
2021-02-09 13:43:38 +0100

    test(server): fix all tests in LobbyTest

--- server/test/sc/server/network/LobbyTest.kt
+++ server/test/sc/server/network/LobbyTest.kt
@@ -1,21 +1,18 @@
 package sc.server.network
 
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import sc.server.client.TestLobbyClientListener
 import sc.server.gaming.GameRoom
-import sc.server.helpers.TestHelper
 import sc.server.plugins.TestPlugin
 import sc.shared.ScoreCause
-import java.util.concurrent.TimeUnit
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
 class LobbyTest: RealServerTest() {
     
     @Test
-    fun shouldConnectAndDisconnect() {
+    fun shouldEndGameOnDisconnect() {
         val player1 = connectClient("localhost", serverPort)
         val player2 = connectClient("localhost", serverPort)
         
@@ -29,38 +26,30 @@ class LobbyTest: RealServerTest() {
         await("GameRoom closes after one player died") { lobby.games.isEmpty() }
     }
     
-    @Disabled
-    @Test // TODO seems to switch the players sometimes
+    @Test
     fun shouldEndGameOnIllegalMessage() {
         val player1 = connectClient("localhost", serverPort)
-        waitForConnect(1)
         val player2 = connectClient("localhost", serverPort)
-        waitForConnect(2)
     
         val listener = TestLobbyClientListener()
         player1.addListener(listener)
         
         player1.joinRoomRequest(TestPlugin.TEST_PLUGIN_UUID)
+        await { listener.gameJoinedReceived }
         player2.joinRoomRequest(TestPlugin.TEST_PLUGIN_UUID)
+    
+        await("Game started") { lobby.games.single().status == GameRoom.GameStatus.ACTIVE }
         
-        // TODO Listen for RoomJoinedResponse directly instead
-        //TestHelper.assertEqualsWithTimeout(1, { player1.rooms.size })
-        //TestHelper.assertEqualsWithTimeout(1, { player2.rooms.size })
-        listener.gameJoinedReceived shouldBe true
-        listener.roomId shouldNotBe null
-        TestHelper.assertEqualsWithTimeout(1, { this@LobbyTest.gameMgr.games.size })
-        
-        val theRoom = this@LobbyTest.gameMgr.games.iterator().next()
-        
-        Assertions.assertEquals(false, theRoom.isOver)
+        val room = gameMgr.games.single()
+        room.isOver shouldBe false
         
         player1.sendCustomData("<yarr>")
         
-        TestHelper.assertEqualsWithTimeout(true, { theRoom.isOver })
-        TestHelper.assertEqualsWithTimeout(ScoreCause.LEFT, { theRoom.result.scores[0].cause }, 2, TimeUnit.SECONDS)
+        await("Game is over") { room.isOver }
+        await("GameResult") { room.result != null }
+        room.result.scores.first().cause shouldBe ScoreCause.LEFT
         
-        // should cleanup gamelist
-        TestHelper.assertEqualsWithTimeout(0, { this@LobbyTest.gameMgr.games.size })
+        await("GameRoom closes") { gameMgr.games.isEmpty() }
     }
     
 }
