51eedef922951131c1c4b7ac2a72e352c1d26597
xerus2000 <27jf@pm.me>
2021-04-30 10:45:19 +0200

    fix(player): simplify IGameHandler

--- player/src/main/sc/player2021/logic/Logic.java
+++ player/src/main/sc/player2021/logic/Logic.java
@@ -5,18 +5,19 @@ import org.slf4j.LoggerFactory;
 import sc.api.plugins.TwoPlayerGameState;
 import sc.framework.plugins.Player;
 import sc.player.IGameHandler;
-import sc.plugin2021.*;
+import sc.plugin2021.GameState;
+import sc.plugin2021.Move;
 import sc.plugin2021.util.GameRuleLogic;
 import sc.shared.GameResult;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Das Herz des Clients:
  * Eine sehr simple Logik, die ihre Zuege zufaellig waehlt,
  * aber gueltige Zuege macht.
- *
+ * <p>
  * Ausserdem werden zum Spielverlauf Konsolenausgaben gemacht.
  */
 public class Logic implements IGameHandler {
@@ -24,24 +25,22 @@ public class Logic implements IGameHandler {
 
   /** Aktueller Spielstatus. */
   private GameState gameState;
-  /** Aktueller eigener Spieler. */
-  private Player currentPlayer;
 
-  public void gameEnded(GameResult data, String errorMessage) {
+  public void onGameOver(GameResult data, String errorMessage) {
     log.info("Das Spiel ist beendet, Ergebnis: {}", data);
   }
 
   @Override
   public Move calculateMove() {
     long startTime = System.currentTimeMillis();
-    log.info("Es wurde ein Zug angefordert.");
-    List<Move> possibleMoves = new ArrayList<>(GameRuleLogic.getPossibleMoves(gameState));
-    return possibleMoves.get((int) (Math.random() * possibleMoves.size()));
-  }
+    Player player = gameState.getCurrentPlayer();
+    log.info("Es wurde ein Zug von {} angefordert.", player);
 
-  @Override
-  public void onUpdate(Player player, Player otherPlayer) {
-    currentPlayer = player;
+    List<Move> possibleMoves = new ArrayList<>(GameRuleLogic.getPossibleMoves(gameState));
+    Move move = possibleMoves.get((int) (Math.random() * possibleMoves.size()));
+
+    log.info("Sende {} nach {}ms.", move, System.currentTimeMillis() - startTime);
+    return move;
   }
 
   @Override
