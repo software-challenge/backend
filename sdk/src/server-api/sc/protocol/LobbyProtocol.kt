27fd42bf88c69bd40a0634f4bb48bf2d625d076a
xeruf <27jf@pm.me>
2021-05-20 22:55:05 +0200

    fix(network): remove FreeReservationRequest
    Not needed now that games can be prepared without reservations.

--- sdk/src/server-api/sc/protocol/LobbyProtocol.kt
+++ sdk/src/server-api/sc/protocol/LobbyProtocol.kt
@@ -16,7 +16,7 @@ object LobbyProtocol {
     fun registerMessages(xStream: XStream): XStream {
         registerAdditionalMessages(xStream, listOf(ErrorPacket::class.java, ErrorMessage::class.java, GamePaused::class.java, JoinedRoomResponse::class.java, RemovedFromGame::class.java, MementoMessage::class.java, GamePreparedResponse::class.java, ObservationResponse::class.java, RoomPacket::class.java, WelcomeMessage::class.java))
     
-        registerAdditionalMessages(xStream, listOf(AuthenticateRequest::class.java, CancelRequest::class.java, FreeReservationRequest::class.java, JoinPreparedRoomRequest::class.java, JoinRoomRequest::class.java, ObservationRequest::class.java, PauseGameRequest::class.java, PrepareGameRequest::class.java, StepRequest::class.java, PlayerScoreRequest::class.java, TestModeRequest::class.java, PlayerScoreResponse::class.java, TestModeResponse::class.java, RoomWasJoinedEvent::class.java))
+        registerAdditionalMessages(xStream, listOf(AuthenticateRequest::class.java, CancelRequest::class.java, JoinPreparedRoomRequest::class.java, JoinRoomRequest::class.java, ObservationRequest::class.java, PauseGameRequest::class.java, PrepareGameRequest::class.java, StepRequest::class.java, PlayerScoreRequest::class.java, TestModeRequest::class.java, PlayerScoreResponse::class.java, TestModeResponse::class.java, RoomWasJoinedEvent::class.java))
     
         registerAdditionalMessages(xStream, listOf(GameResult::class.java, PlayerScore::class.java, ScoreAggregation::class.java, ITeam::class.java, ScoreCause::class.java, ScoreDefinition::class.java, ScoreFragment::class.java, WinCondition::class.java, SlotDescriptor::class.java, Score::class.java, ScoreValue::class.java))
 
