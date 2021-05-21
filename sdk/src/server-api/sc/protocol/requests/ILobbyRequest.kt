16ff18514d481c547bb0d05173d8b8e30bb7f23b
xeruf <27jf@pm.me>
2021-05-21 10:29:12 +0200

    docs(network): unify request KDocs

--- sdk/src/server-api/sc/protocol/requests/ILobbyRequest.kt
+++ sdk/src/server-api/sc/protocol/requests/ILobbyRequest.kt
@@ -5,5 +5,5 @@ import sc.protocol.ProtocolPacket
 /** Parent for all packets handled by the Lobby.  */
 interface ILobbyRequest: ProtocolPacket
 
-/** Marks requests only allowed as an administrative client. */
+/** Marks requests only allowed after successful authentication. */
 interface AdminLobbyRequest: ILobbyRequest
