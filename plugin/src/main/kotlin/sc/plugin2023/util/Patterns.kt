d0d719eea904490e1dc44bde9839f9d8b2bc79e3
maxbl <stu222782@mail.uni-kiel.de>
2023-07-08 00:39:08 +0200

    feat: add Pattern enum for managing positional patterns
    A new enum class Pattern has been added to manage different patterns of movement in different directions. This refactoring simplifies the patterns adoption by providing a single place to define and apply patterns. This change also reduces duplicate code in the Board.kt, contributing to code maintainability by encapsulating the pattern logic into the Pattern enum.

---
+++ plugin/src/main/kotlin/sc/plugin2023/util/Patterns.kt
@@ -0,0 +1,71 @@
+package sc.plugin2023.util
+
+import sc.api.plugins.HexDirection
+
+/**
+ * Enumeration representing different patterns.
+ *
+ * This enumeration provides patterns for different directions, such as right, left, up, down, etc.
+ * Each pattern is defined by a list of pairs representing the pattern's movement in the X and Y coordinates.
+ */
+enum class Pattern(val pattern: List<Pair<Int, Int>>) {
+    RIGHT(createPatternRight()),
+    DOWN_RIGHT(createPatternDownRight()),
+    LEFT(RIGHT.pattern.map { it.copy(first = -it.first) }),
+    DOWN_LEFT(DOWN_RIGHT.pattern.reversed().map { it.copy(first = -it.first) }),
+    UP_RIGHT(DOWN_RIGHT.pattern.map { it.copy(second = -it.second) }),
+    UP_LEFT(DOWN_RIGHT.pattern.reversed().map { it.copy(first = -it.first, second = -it.second) });
+    
+    companion object {
+        /**
+         * Returns the corresponding Pattern for a given HexDirection.
+         *
+         * @param hexDirection The HexDirection to get the Pattern for.
+         * @return The corresponding Pattern for the given HexDirection.
+         */
+        fun match(hexDirection: HexDirection): Pattern {
+            return when(hexDirection) {
+                HexDirection.RIGHT -> RIGHT
+                HexDirection.UP_RIGHT -> UP_RIGHT
+                HexDirection.UP_LEFT -> UP_LEFT
+                HexDirection.LEFT -> LEFT
+                HexDirection.DOWN_LEFT -> DOWN_LEFT
+                HexDirection.DOWN_RIGHT -> DOWN_RIGHT
+            }
+        }
+    }
+}
+
+/**
+ * Returns a list representing a right pattern.
+ *
+ * The pattern is a list of pairs, where each pair represents the coordinates of a point.
+ * The points are arranged in a right pattern, with each row having two points on alternating columns.
+ * The pattern starts in the top-left corner and continues to the bottom-right corner.
+ * The coordinates are represented as pairs of integers, where the first value is the x-coordinate and the second value is the y-coordinate.
+ *
+ * @return a list representing the right pattern
+ */
+private fun createPatternRight() = listOf(
+        0 to 0, 2 to 0, 4 to 0, 6 to 0,
+        1 to 1, 3 to 1, 5 to 1, 7 to 1,
+        2 to 2, 4 to 2, 6 to 2, 8 to 2,
+        1 to 3, 3 to 3, 5 to 3, 7 to 3,
+        0 to 4, 2 to 4, 4 to 4, 6 to 4
+)
+
+/**
+ * Creates a pattern in the down-right direction.
+ *
+ * This method returns a list of coordinates representing a specific pattern in the down-right direction.
+ *
+ * @return a list of coordinates representing the pattern
+ */
+private fun createPatternDownRight() = listOf(
+        0 to 0, -1 to 1, 1 to 1, -6 to 2,
+        -4 to 2, -2 to 2, 0 to 2, 2 to 2,
+        -5 to 3, -3 to 3, -1 to 3, 1 to 3,
+        3 to 3, -4 to 4, -2 to 4, 0 to 4,
+        2 to 4, -3 to 5, -1 to 5, 1 to 5,
+        0 to 6
+)
--- plugin/src/main/kotlin/sc/plugin2024/Board.kt
+++ plugin/src/main/kotlin/sc/plugin2024/Board.kt
@@ -5,6 +5,7 @@ import sc.api.plugins.Coordinates
 import sc.api.plugins.HexDirection
 import sc.api.plugins.RectangularBoard
 import sc.api.plugins.TwoDBoard
+import sc.plugin2023.util.Pattern
 import sc.plugin2024.util.PluginConstants.MAX_ISLANDS
 import sc.plugin2024.util.PluginConstants.MAX_SPECIAL
 import sc.plugin2024.util.PluginConstants.MIN_ISLANDS
@@ -15,10 +16,10 @@ import sc.plugin2024.util.PluginConstants as Constants
 
 @XStreamAlias(value = "board")
 open class Board(
-    gameField: TwoDBoard<Field> = initBoard(),
-    segments: ArrayList<Segment> = ArrayList()
+        gameField: TwoDBoard<Field> = initBoard(),
+        segments: ArrayList<Segment> = ArrayList(),
 ):
-    RectangularBoard<Field>(gameField) {
+        RectangularBoard<Field>(gameField) {
     
     private fun getYCoordinateInDirection(y: Int, direction: HexDirection?) {
     
@@ -31,8 +32,8 @@ open class Board(
     open fun getFieldInDirection(direction: HexDirection, field: Field): Field? {
         val coordinateInDirection = field.coordinate.plus(direction)
         return if(
-            coordinateInDirection.x in gameField.indices &&
-            coordinateInDirection.y in 0 until gameField[coordinateInDirection.x].size
+                coordinateInDirection.x in gameField.indices &&
+                coordinateInDirection.y in 0 until gameField[coordinateInDirection.x].size
         ) {
             gameField[coordinateInDirection.x][coordinateInDirection.y]
         } else {
@@ -40,40 +41,19 @@ open class Board(
         }
     }
     
-    private fun buildPattern(direction: HexDirection): List<Pair<Int, Int>> {
-        val rightPattern = listOf(
-            Pair(0, 0), Pair(2, 0), Pair(4, 0), Pair(6, 0), Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1),
-            Pair(2, 2), Pair(4, 2), Pair(6, 2), Pair(8, 2), Pair(1, 3), Pair(3, 3), Pair(5, 3), Pair(7, 3),
-            Pair(0, 4), Pair(2, 4), Pair(4, 4), Pair(6, 4)
-        )
-        val downRightPattern = listOf(
-            Pair(0, 0), Pair(-1, 1), Pair(1, 1), Pair(-6, 2), Pair(-4, 2), Pair(-2, 2), Pair(0, 2), Pair(2, 2),
-            Pair(-5, 3), Pair(-3, 3), Pair(-1, 3), Pair(1, 3), Pair(3, 3), Pair(-4, 4), Pair(-2, 4), Pair(0, 4),
-            Pair(2, 4), Pair(-3, 5), Pair(-1, 5), Pair(1, 5), Pair(0, 6)
-        )
-        return when(direction) {
-            HexDirection.RIGHT -> rightPattern
-            HexDirection.LEFT -> rightPattern.map { Pair(-it.first, it.second) }
-            HexDirection.DOWN_RIGHT -> downRightPattern
-            HexDirection.DOWN_LEFT -> downRightPattern.reversed().map { Pair(-it.first, it.second) }
-            HexDirection.UP_RIGHT -> downRightPattern.map { Pair(it.first, -it.second) }
-            HexDirection.UP_LEFT -> downRightPattern.reversed().map { Pair(-it.first, -it.second) }
-        }
-    }
-    
     private fun addSegment(
-        segment: ArrayList<ArrayList<Field>>,
-        pattern: List<Pair<Int, Int>>,
-        segmentStart: Coordinates
+            segment: ArrayList<ArrayList<Field>>,
+            pattern: List<Pair<Int, Int>>,
+            segmentStart: Coordinates,
     ) {
-        val segmentPattern = buildPattern(HexDirection.RIGHT)
+        val segmentPattern = Pattern.RIGHT.pattern
         for(i in pattern.indices) {
             val offsetCoordinate = Coordinates(
-                segmentStart.x + pattern[i].first,
-                segmentStart.y + pattern[i].second
+                    segmentStart.x + pattern[i].first,
+                    segmentStart.y + pattern[i].second
             ).fromDoubledHex()
             val currentPositionInSegment =
-                Coordinates(segmentPattern[i].first, segmentPattern[i].second).fromDoubledHex()
+                    Coordinates(segmentPattern[i].first, segmentPattern[i].second).fromDoubledHex()
             if(segment.size <= currentPositionInSegment.x) {
                 segment.add(ArrayList())
             }
@@ -82,24 +62,24 @@ open class Board(
     }
     
     private fun initSegment(
-        seed: Int = Random.nextInt(),
-        lastSegment: Int,
-        segmentStart: Coordinates,
-        direction: HexDirection,
-        passengers: Int = NUMBER_OF_PASSENGERS,
-        blocked: Int = Random.nextInt(MIN_ISLANDS, MAX_ISLANDS),
-        special: Int = Random.nextInt(MIN_SPECIAL, MAX_SPECIAL),
-        end: Boolean
+            seed: Int = Random.nextInt(),
+            lastSegment: Int,
+            segmentStart: Coordinates,
+            direction: HexDirection,
+            passengers: Int = NUMBER_OF_PASSENGERS,
+            blocked: Int = Random.nextInt(MIN_ISLANDS, MAX_ISLANDS),
+            special: Int = Random.nextInt(MIN_SPECIAL, MAX_SPECIAL),
+            end: Boolean,
     ) {
         val segment: ArrayList<ArrayList<Field>> = ArrayList()
-        val pattern = buildPattern(direction)
+        val pattern = Pattern.match(direction).pattern
         addSegment(segment, pattern, segmentStart)
     }
     
     companion object {
         fun initBoard(
-            width: Int = Constants.NUMBER_OF_SEGMENTS * (Constants.SEGMENT_FIELDS_WIDTH + 1),
-            height: Int = Constants.NUMBER_OF_SEGMENTS * Constants.SEGMENT_FIELDS_HEIGHT
+                width: Int = Constants.NUMBER_OF_SEGMENTS * (Constants.SEGMENT_FIELDS_WIDTH + 1),
+                height: Int = Constants.NUMBER_OF_SEGMENTS * Constants.SEGMENT_FIELDS_HEIGHT,
         ): List<MutableList<Field>> {
             return List(width) { i ->
                 MutableList(height) { j ->
