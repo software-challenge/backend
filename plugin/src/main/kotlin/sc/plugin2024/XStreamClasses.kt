6972a1234f4c5d3e6db6d027337afa4bb8a4212b
maxbl <stu222782@mail.uni-kiel.de>
2023-07-08 11:04:27 +0200

    feat: dd segment converter in XStream serialization
    A new converter `GetSegmentsOfBoard` was added to the `XStreamClasses` to handle the serialization and deserialization of board segments.

--- plugin/src/main/kotlin/sc/plugin2024/XStreamClasses.kt
+++ plugin/src/main/kotlin/sc/plugin2024/XStreamClasses.kt
@@ -1,14 +1,23 @@
 package sc.plugin2024
 
+import com.thoughtworks.xstream.XStream
 import sc.api.plugins.Coordinates
 import sc.api.plugins.Team
 import sc.networking.XStreamProvider
+import sc.plugin2024.util.GetSegmentsOfBoard
 
 class XStreamClasses: XStreamProvider {
     
     override val classesToRegister =
             listOf(
-                    Board::class.java, Coordinates::class.java, GameState::class.java,
-                    Move::class.java, Team::class.java)
+                    Board::class.java,
+                    Coordinates::class.java,
+                    GameState::class.java,
+                    Move::class.java,
+                    Team::class.java
+            )
     
+    override fun setup(xStream: XStream) {
+        xStream.registerConverter(GetSegmentsOfBoard())
+    }
 }
---
+++ plugin/src/main/kotlin/sc/plugin2024/util/GetSegmentsOfBoard.kt
@@ -0,0 +1,37 @@
+package sc.plugin2024.util
+
+import com.thoughtworks.xstream.converters.Converter
+import com.thoughtworks.xstream.converters.MarshallingContext
+import com.thoughtworks.xstream.converters.UnmarshallingContext
+import com.thoughtworks.xstream.io.HierarchicalStreamReader
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter
+import sc.plugin2024.Segment
+
+class GetSegmentsOfBoard: Converter {
+    override fun canConvert(clazz: Class<*>?): Boolean {
+        return clazz == ArrayList::class.java
+    }
+    
+    override fun marshal(value: Any?, writer: HierarchicalStreamWriter?, context: MarshallingContext?) {
+        @Suppress("Unchecked_cast") val list = value as ArrayList<Segment>
+        
+        // only get the last three elements
+        val sublist = if(list.size <= 3) list
+        else list.subList(list.size - 3, list.size)
+        
+        sublist.forEach {
+            context?.convertAnother(it)
+        }
+    }
+    
+    override fun unmarshal(reader: HierarchicalStreamReader?, context: UnmarshallingContext?): Any {
+        val list = ArrayList<Segment>()
+        while(reader?.hasMoreChildren() == true) {
+            reader.moveDown()
+            val segment = context?.convertAnother(list, Segment::class.java) as Segment
+            list.add(segment)
+            reader.moveUp()
+        }
+        return list
+    }
+}
