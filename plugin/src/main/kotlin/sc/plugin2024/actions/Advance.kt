a57c2cdb5ae871ec1ad6e53278e29909056d1f38
maxbl <stu222782@mail.uni-kiel.de>
2023-07-13 10:15:56 +0200

    refactor(Advance): refactor ship movement and obstacle handling

--- plugin/src/main/kotlin/sc/plugin2024/actions/Advance.kt
+++ plugin/src/main/kotlin/sc/plugin2024/actions/Advance.kt
@@ -24,49 +24,41 @@ data class Advance(
     
     @Throws(InvalidMoveException::class)
     override fun perform(state: GameState, ship: Ship) {
-        validateShipMovement(ship)
+        if(ship.movement == 0) {
+            throw InvalidMoveException(AdvanceException.NO_MOVEMENT_POINTS)
+        }
         val start: CubeCoordinates = ship.position
         val direction = ship.direction
-        validateDistance()
-        val nextFields: LinkedList<Field> = LinkedList<Field>()
+        if(distance == 0 || distance > 6 || distance < -1) {
+            throw InvalidMoveException(AdvanceException.INVALID_DISTANCE)
+        }
+        val nextFields: LinkedList<CubeCoordinates> = LinkedList<CubeCoordinates>()
         
         when {
-            distance == -1 && state.board[start.cubeToDoubledHex()] === FieldType.SANDBANK -> handleMoveFromSandbank(start, ship, state, direction, moveBackward = true)
-            state.board[start.cubeToDoubledHex()] === FieldType.SANDBANK && distance == 1 -> handleMoveFromSandbank(start, ship, state, direction)
+            distance == -1 && state.board[start] === FieldType.SANDBANK -> handleMoveFromSandbank(start, ship, state, direction, moveBackward = true)
+            state.board[start] === FieldType.SANDBANK && distance == 1 -> handleMoveFromSandbank(start, ship, state, direction)
             else -> handleOtherMoves(ship, start, direction, state, nextFields)
         }
     }
     
-    private fun checkDestinationForShip(target: Field, state: GameState) {
-        val otherShip = state.otherTeam.pieces.first()
+    private fun checkDestinationForShip(target: CubeCoordinates, state: GameState) {
+        val otherShip = state.otherShip
         if(target == otherShip.position) {
             throw InvalidMoveException(AdvanceException.SHIP_ALREADY_IN_TARGET)
         }
     }
     
-    private fun validateDistance() {
-        if(distance == 0 || distance > 6 || distance < -1) {
-            throw InvalidMoveException(AdvanceException.INVALID_DISTANCE)
-        }
-    }
-    
-    private fun validateShipMovement(ship: Ship) {
-        if(ship.movement == 0) {
-            throw InvalidMoveException(AdvanceException.NO_MOVEMENT_POINTS)
-        }
-    }
-    
-    private fun handleMoveFromSandbank(start: Field, ship: Ship, state: GameState, direction: CubeDirection, moveBackward: Boolean = false) {
-        var next: FieldType? = state.board.getFieldInDirection(direction, start)
-        if (moveBackward) {
-            next = if(next == null || next === FieldType.BLOCKED || !next.isEmpty) {
+    private fun handleMoveFromSandbank(start: CubeCoordinates, ship: Ship, state: GameState, direction: CubeDirection, moveBackward: Boolean = false) {
+        var next: CubeCoordinates = start + direction.vector
+        val nextFieldType: FieldType = state.board[next] ?: throw InvalidMoveException(AdvanceException.FIELD_NOT_EXIST)
+        if(moveBackward) {
+            next = if(nextFieldType === FieldType.BLOCKED || !nextFieldType.isEmpty) {
                 throw InvalidMoveException(AdvanceException.BACKWARD_MOVE_NOT_POSSIBLE)
             } else {
                 next
             }
         } else {
-            next = next ?: throw InvalidMoveException(AdvanceException.FIELD_NOT_EXIST)
-            if (!next.isEmpty) {
+            if(!nextFieldType.isEmpty) {
                 throw InvalidMoveException(AdvanceException.FIELD_IS_BLOCKED)
             }
         }
@@ -75,48 +67,43 @@ data class Advance(
         ship.position = next
     }
     
-    private fun handleOtherMoves(ship: Ship, start: Field, direction: CubeDirection, state: GameState, nextFields: LinkedList<Field>) {
+    private fun handleOtherMoves(ship: Ship, start: CubeCoordinates, direction: CubeDirection, state: GameState, nextFields: LinkedList<CubeCoordinates>) {
         nextFields.add(start)
         for(i in 0 until distance) {
-            val next: Field? = state.board.getFieldInDirection(direction, nextFields[i])
-            next?.let { nextFields.add(it) } ?: throw InvalidMoveException(AdvanceException.FIELD_NOT_EXIST)
-            checkDestinationForShip(next, state)
-            handleObstacles(ship, state, nextFields, i)
+            val neighbour: CubeCoordinates = nextFields[i] + direction.vector
+            if(state.board[neighbour] != null) {
+                nextFields.add(neighbour)
+                checkDestinationForShip(neighbour, state)
+                handleObstacles(ship, state, nextFields, i)
+            } else {
+                throw InvalidMoveException(AdvanceException.FIELD_NOT_EXIST)
+            }
         }
-        val target: Field = nextFields[distance]
+        val target: CubeCoordinates = nextFields[distance]
         ship.position = target
         return
     }
     
-    private fun handleObstacles(ship: Ship, state: GameState, nextFields: LinkedList<Field>, i: Int) {
-        val checkField: Field = nextFields[i + 1]
-        if(!checkField.isPassable() || state.otherTeam.pieces.first().position == checkField && i != distance - 1) {
+    private fun handleObstacles(ship: Ship, state: GameState, nextFields: LinkedList<CubeCoordinates>, i: Int) {
+        val checkField: CubeCoordinates = nextFields[i + 1]
+        val checkedFieldType: FieldType? = state.board[checkField]
+        if(checkedFieldType == null || !checkedFieldType.isEmpty || state.otherShip.position == checkField && i != distance - 1) {
             throw InvalidMoveException(AdvanceException.FIELD_IS_BLOCKED)
         }
         
-        when(checkField.type) {
-            FieldType.SANDBANK -> handleSandbankObstacle(ship, checkField, state, i)
-            FieldType.LOG -> handleLogObstacle(ship)
+        when(state.board[checkField]) {
+            FieldType.SANDBANK -> {
+                ship.speed = 1
+                ship.movement = 0
+                endsTurn = true
+                if(i != distance - 1) {
+                    throw InvalidMoveException(AdvanceException.MOVE_END_ON_SANDBANK)
+                }
+                return
+            }
+
             else -> ship.movement -= 1
         }
-    }
-    
-    private fun handleSandbankObstacle(ship: Ship, checkField: Field, state: GameState, i: Int) {
-        ship.speed = 1
-        ship.movement = 0
-        endsTurn = true
-        if(i != distance - 1) {
-            throw InvalidMoveException(AdvanceException.MOVE_END_ON_SANDBANK)
-        }
-        return
-    }
-    
-    private fun handleLogObstacle(ship: Ship) {
-        if(ship.movement <= 1) {
-            throw InvalidMoveException(AdvanceException.NOT_ENOUGH_MOVEMENT_POINTS_TO_CROSS_LOG)
-        }
-        ship.movement -= 2
-        ship.speed = 1.coerceAtLeast(ship.speed - 1)
     }
     
     override fun toString(): String = "Gehe $distance vor"
