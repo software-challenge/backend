5e3d509b10471a8fb8e88a83370abfa1873d6556
xeruf <27jf@pm.me>
2025-02-07 12:45:48 +0100

    build: improve server push automation

--- helpers/push_to_server.sh
+++ helpers/push_to_server.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
+#!/bin/sh -ex
 
 if test -z "$server"; then
     # server is not set or ""
     echo "please call push_staging.sh or push_production.sh"
     exit 1
 fi
+
+if test -S .ssh-socket~
+then ssh -S .ssh-socket~ -O exit $server && sleep 1 || rm .ssh-socket~
+fi
+
 ssh -M -S .ssh-socket~ -fnNT -L 5000:localhost:5000 $server
 ssh -S .ssh-socket~ -O check $server || exit 1
-docker login localhost:5000 || exit 1
+echo testpassword | docker login --username testuser --password-stdin localhost:5000 || exit 1
 
 ./gradlew clean dockerImage
 
@@ -16,8 +21,6 @@ docker push localhost:5000/swc_game-server
 
 ssh -S .ssh-socket~ -O exit $server
 
-# this is currently done in webapp
-#scp docker-compose-production.yml $server:./docker-compose.yml
 ssh $server 'sudo docker pull localhost:5000/swc_game-server' && \
 # NOTE This requires the other services (client-controller and gameserver) in the compose file also already deployed
 ssh $server 'sudo docker service update --image localhost:5000/swc_game-server:latest --with-registry-auth contest_gameserver'
