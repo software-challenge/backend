e715994a8575dfaae89bca8e3c88c7308efcf2bb
xeruf <27jf@pm.me>
2025-06-24 10:49:35 +0200

    feat(plugin26): add fish size to field state

--- plugin2026/src/main/kotlin/sc/plugin2026/FieldState.kt
+++ plugin2026/src/main/kotlin/sc/plugin2026/FieldState.kt
@@ -2,43 +2,35 @@ package sc.plugin2026
 
 import sc.api.plugins.Team
 
-// TODO include fish size - maybe a sealed class
-
-enum class FieldState {
-    ONE,
-    TWO,
-    OBSTRUCTED,
-    EMPTY;
+enum class FieldState(val size: Int) {
+    ONE_S(1),
+    ONE_M(2),
+    ONE_L(3),
+    TWO_S(1),
+    TWO_M(2),
+    TWO_L(3),
+    OBSTRUCTED(0),
+    EMPTY(0);
     
-    override fun toString() = when(this) {
-        OBSTRUCTED -> "Krake"
-        ONE -> "Eins"
-        TWO -> "Zwei"
-        else -> "empty"
-    }
+    override fun toString() =
+        when(this) {
+            OBSTRUCTED -> "Krake"
+            EMPTY -> "Leer"
+            else -> team?.color.toString() + size.toString()
+        }
     
-    fun asLetter() = when(this) {
-        OBSTRUCTED -> 'O'
-        ONE -> 'R'
-        TWO -> 'B'
-        else -> ' '
-    }
+    fun asLetters() =
+        when(this) {
+            OBSTRUCTED -> "X "
+            EMPTY -> "  "
+            else -> team?.letter.toString() + size.toString()
+        }
     
     val team: Team?
         get() = when(this) {
-            ONE -> Team.ONE
-            TWO -> Team.TWO
-            OBSTRUCTED -> null
-            EMPTY -> null
+            ONE_S, ONE_M, ONE_L -> Team.ONE
+            TWO_S, TWO_M, TWO_L -> Team.TWO
+            OBSTRUCTED, EMPTY -> null
         }
     
-    companion object {
-        @JvmStatic
-        fun from(team: Team): FieldState {
-            return when(team) {
-                Team.ONE -> ONE
-                Team.TWO -> TWO
-            }
-        }
-    }
 }
