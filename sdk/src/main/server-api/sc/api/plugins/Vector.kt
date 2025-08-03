8a7f878f8b74e998987cb128240d7cac60c38d21
xeruf <27jf@pm.me>
2025-08-03 23:46:37 +0200

    refactor(sdk/Vector): move generic methods to IVector interface

--- sdk/src/main/server-api/sc/api/plugins/Vector.kt
+++ sdk/src/main/server-api/sc/api/plugins/Vector.kt
@@ -2,11 +2,17 @@ package sc.api.plugins
 
 import kotlin.math.abs
 import kotlin.math.sqrt
-import kotlin.random.Random
 
 interface IVector {
     val dx: Int
     val dy: Int
+    
+    /** Verändert die Länge des Vektors um den gegebenen Faktor, ohne seine Richtung zu ändern. */
+    operator fun times(scalar: Int): Vector =
+        Vector(scalar * dx, scalar * dy)
+    
+    /** Konvertiert den Vektor zu entsprechenden [Coordinates]. */
+    operator fun unaryPlus(): Coordinates = Coordinates(dx, dy)
 }
 
 /**
@@ -14,33 +20,27 @@ interface IVector {
  * @property dx die Differenz in x-Richtung
  * @property dy die Differenz in y-Richtung
  */
-data class Vector(override val dx: Int, override val dy: Int): IVector, Comparable<IVector> {
+data class Vector(override val dx: Int, override val dy: Int): IVector, Comparable<Vector> {
     
     /** Die Fläche des Rechtecks, dessen Diagonale der Vector ist. */
     val area: Int
         get() = abs(dx * dy)
     
+    /** Länge des Vektors auf einem rechteckigen Spielfeld. */
     val length: Double
         get() = sqrt(comparableLength.toDouble())
     
-    /** Verändert die Länge des Vektors um den gegebenen Faktor, ohne seine Richtung zu ändern. */
-    operator fun times(scalar: Int): Vector =
-            Vector(scalar * dx, scalar * dy)
-    
+    /** Ob dieser Vektor auf einem doubled Hex Feld eine gerade Linie beschreibt. */
     val straightHex: Boolean
         get() = abs(dx) == abs(dy) || (dx % 2 == 0 && dy == 0)
     
-    private val IVector.comparableLength: Int
+    private val comparableLength: Int
         get() = dx * dx + dy * dy
     
     /**
      * Vergleicht die Länge dieses Vektors mit einem anderen.
-     * @return groesser Null, wenn dieser laenger ist
+     * @return groesser als 0, wenn dieser Vektor laenger ist
      */
-    override operator fun compareTo(other: IVector): Int =
-            comparableLength - other.comparableLength
-    
-    /** Konvertiert den Vektor zu entsprechenden [Coordinates]. */
-    operator fun unaryPlus(): Coordinates = Coordinates(dx, dy)
-    
+    override operator fun compareTo(other: Vector): Int =
+        comparableLength - other.comparableLength
 }
