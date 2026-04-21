29e59d17cea1d0d9f6f58684909a556de2386dd7
SturmEnte <jonas@sturmente.de>
2026-04-21 11:39:10 +0200

    feat(vier-gewinnt): Adjusted GameRuleLogic

--- games/vier-gewinnt/src/main/kotlin/sc/plugin2098/util/GameRuleLogic.kt
+++ games/vier-gewinnt/src/main/kotlin/sc/plugin2098/util/GameRuleLogic.kt
@@ -9,12 +9,13 @@ import sc.plugin2098.Move
 import sc.plugin2098.PiranhaMoveMistake
 import sc.shared.IMoveMistake
 import sc.shared.MoveMistake
+import sc.api.plugins.Vector
 
 /** Regellogik und Hilfsfunktionen für das Piranhas-Spiel. */
 object GameRuleLogic {
     /** Anzahl der Fische in der Bewegungsachse des Zuges.
      * @return wie viele Felder weit der Zug sein sollte */
-    @JvmStatic
+    /*@JvmStatic
     fun movementDistance(board: Board, move: Move): Int {
         var count = 1
         var pos = move.from
@@ -34,48 +35,39 @@ object GameRuleLogic {
             }
         }
         return count
-    }
+    }*/
     
     /** Berechnet die Zielkoordinaten des Zuges. */
-    @JvmStatic
+    /*@JvmStatic
     fun targetCoordinates(board: Board, move: Move): Coordinates =
-        move.from + move.direction * movementDistance(board, move)
+        move.from + move.direction * movementDistance(board, move)*/
     
     /** Prüft, ob ein Zug gültig ist.
      * @return null wenn der Zug valide ist, sonst ein entsprechender [IMoveMistake]. */
     @JvmStatic
     fun checkMove(board: Board, move: Move): IMoveMistake? {
-        val distance = movementDistance(board, move)
-        var pos = move.from
-        
-        val team = board[move.from].team ?: return MoveMistake.START_EMPTY
-        val opponent = team.opponent()
-        
-        var moved = 1
-        while(moved < distance) {
-            pos += move.direction
-            val field = board.getOrNull(pos) ?: return MoveMistake.DESTINATION_OUT_OF_BOUNDS
-            if(field.team == opponent) {
-                return PiranhaMoveMistake.JUMP_OVER_OPPONENT
-            }
-            moved++
+
+        // Überprüfen, ob die Koordinaten in auf dem Spielfeld sind
+        if (move.position.x < 0 || move.position.x >= Connect4Constants.BOARD_WIDTH ||
+                move.position.y < 0 || move.position.y >= Connect4Constants.BOARD_HEIGHT)
+            return MoveMistake.DESTINATION_OUT_OF_BOUNDS
+
+
+        // Überprüfen ob das Feld nicht leer ist
+        if(!board[move.position].isEmpty)
+            return MoveMistake.DESTINATION_BLOCKED
+
+        // Überprüfen ob das Feld unter dem Ziel nicht unterhalb des Spielfelds ist und nicht von einem Plätchen belegt ist
+        // TODO: Überprüfen, ob der untere Rand tatsächlich y=0 ist
+        val fieldBelow = move.position + Vector(0, -1)
+        if(move.position.y != 0 && board[fieldBelow].isEmpty) {
+            return MoveMistake.DESTINATION_IN_AIR
         }
-        pos += move.direction
-        val state = board.getOrNull(pos)
-        return when(state) {
-            null -> MoveMistake.DESTINATION_OUT_OF_BOUNDS
-            FieldState.SQUID -> MoveMistake.DESTINATION_BLOCKED
-            else -> {
-                if(state.team == team) {
-                    MoveMistake.DESTINATION_BLOCKED_BY_SELF
-                } else {
-                    null
-                }
-            }
-        }
+
+        return null
     }
     
-    /** Valide Züge des Fisches auf dem Startfeld [pos]. */
+    /*/** Valide Züge des Fisches auf dem Startfeld(???) [pos]. */
     @JvmStatic
     fun possibleMovesFor(board: Board, pos: Coordinates): Collection<Move> {
         val moves: MutableList<Move> = ArrayList()
@@ -86,47 +78,48 @@ object GameRuleLogic {
             }
         }
         return moves
-    }
+    }*/
     
-    /** Sequenz aller gültigen Züge des Fisches auf dem Startfeld [pos]. */
-    fun possibleMovesSequence(board: Board, pos: Coordinates): Sequence<Move> =
-        Direction.values().asSequence()
-            .map { direction -> Move(pos, direction)}
-            .filter { move -> checkMove(board, move) == null }
+//    /** Sequenz aller gültigen Züge des Fisches auf dem Startfeld [pos]. */
+//    fun possibleMovesSequence(board: Board, pos: Coordinates): Sequence<Move> =
+//        Direction.values().asSequence()
+//            .map { direction -> Move(pos, direction)}
+//            .filter { move -> checkMove(board, move) == null }
     
-    /** @return die [Coordinates] aus [parentSet], die Nachbarn von [pos] sind */
-    private fun selectNeighbors(pos: Coordinates, parentSet: Collection<Coordinates>): Collection<Coordinates> {
-        val returnSet = ArrayList<Coordinates>(8)
-        for(i in -1..1) {
-            for(j in -1..1) {
-                val x = pos.x + i
-                val y = pos.y + j
-                if(x < 0 || x >= PiranhaConstants.BOARD_HEIGHT ||
-                   y < 0 || y >= PiranhaConstants.BOARD_WIDTH ||
-                   (i == 0 && j == 0)) continue
-                
-                val coord = Coordinates(x, y)
-                if(parentSet.contains(coord)) {
-                    returnSet.add(coord)
-                }
-            }
-        }
-        return returnSet
-    }
-    
-    /** Startet mit einem einzelnen Fisch in [swarm] und den verbleibenden [looseFishes],
-     * ruft sich rekursiv mit hinzugefügten Nachbarn auf, um den gesamten Schwarm zu finden. */
-    private fun getSwarm(looseFishes: Collection<Coordinates>, swarm: List<Coordinates>): List<Coordinates> {
-        val swarmNeighbors =
-            swarm.flatMap { selectNeighbors(it, looseFishes) }
-        
-        // only search on if any neighbors were added
-        if(swarmNeighbors.isNotEmpty()) {
-            return getSwarm(looseFishes - swarmNeighbors, swarm + swarmNeighbors)
-        }
-        return swarm
-    }
-    
+//    /** @return die [Coordinates] aus [parentSet], die Nachbarn von [pos] sind */
+//    private fun selectNeighbors(pos: Coordinates, parentSet: Collection<Coordinates>): Collection<Coordinates> {
+//        val returnSet = ArrayList<Coordinates>(8)
+//        for(i in -1..1) {
+//            for(j in -1..1) {
+//                val x = pos.x + i
+//                val y = pos.y + j
+//                if(x < 0 || x >= PiranhaConstants.BOARD_HEIGHT ||
+//                   y < 0 || y >= PiranhaConstants.BOARD_WIDTH ||
+//                   (i == 0 && j == 0)) continue
+//
+//                val coord = Coordinates(x, y)
+//                if(parentSet.contains(coord)) {
+//                    returnSet.add(coord)
+//                }
+//            }
+//        }
+//        return returnSet
+//    }
+//
+//    /** Startet mit einem einzelnen Fisch in [swarm] und den verbleibenden [looseFishes],
+//     * ruft sich rekursiv mit hinzugefügten Nachbarn auf, um den gesamten Schwarm zu finden. */
+//    private fun getSwarm(looseFishes: Collection<Coordinates>, swarm: List<Coordinates>): List<Coordinates> {
+//        val swarmNeighbors =
+//            swarm.flatMap { selectNeighbors(it, looseFishes) }
+//
+//        // only search on if any neighbors were added
+//        if(swarmNeighbors.isNotEmpty()) {
+//            return getSwarm(looseFishes - swarmNeighbors, swarm + swarmNeighbors)
+//        }
+//        return swarm
+//    }
+
+    /*
     /** Findet den schwersten Schwarm innerhalb einer Menge gewichteter Positionen. */
     @JvmStatic
     fun greatestSwarm(fieldsToCheck: Map<Coordinates, Int>): Map<Coordinates, Int>? {
@@ -169,5 +162,5 @@ object GameRuleLogic {
         val fieldsWithFish = board.fieldsForTeam(team)
         val greatestSwarm = greatestSwarm(fieldsWithFish)
         return greatestSwarm?.size == fieldsWithFish.size
-    }
+    }*/
 }
